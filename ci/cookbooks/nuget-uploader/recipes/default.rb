1319b9a906993650bbc22ade946f4507cf533f4d
Alex Gyoshev <alex@gyoshev.net>
2014-11-19 17:16:51 +0200

    Update windows cookbook, add nuget-uploader cookbook

---
+++ ci/cookbooks/nuget-uploader/recipes/default.rb
@@ -0,0 +1,8 @@
+case node["platform"]
+when "windows"
+    windows_zipfile 'c:\nuget-uploader' do
+        source '\\\\telerik.com\resources\AppDirector\Repositories\Uploader-SDK\Uploader.Sdk.57.zip'
+        action :unzip
+        not_if {::File.exists?('c:\nuget-uploader\MetadataTool\Telerik.Metadata.Tool.exe')}
+    end
+end
