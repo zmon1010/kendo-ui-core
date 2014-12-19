62d1f538039a6069c8dd7f9e0e70a317eec50cfe
Petyo Ivanov <underlog@gmail.com>
2014-12-19 13:32:27 +0200

    Update nuget uploader version

--- ci/cookbooks/nuget-uploader/recipes/default.rb
+++ ci/cookbooks/nuget-uploader/recipes/default.rb
@@ -1,7 +1,7 @@
 case node["platform"]
 when "windows"
     windows_zipfile 'c:\nuget-uploader' do
-        source '\\\\telerik.com\resources\AppDirector\Repositories\Uploader-SDK\Uploader.Sdk.57.zip'
+        source '\\\\telerik.com\resources\AppDirector\Repositories\Uploader-SDK\Uploader.Sdk.59.zip'
         action :unzip
         not_if {::File.exists?('c:\nuget-uploader\MetadataTool\Telerik.Metadata.Tool.exe')}
     end
