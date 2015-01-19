86fb8dedf1e0c25bad172f63fab954cadb19b56f
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2015-01-19 17:47:01 +0200

    Make data processing aspnet50 buildable

---
+++ wrappers/mvc-6/src/Kendo.Mvc/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,13 @@
+namespace Kendo.Mvc.Extensions
+{
+    using System.Linq.Expressions;
+    using Microsoft.AspNet.Mvc.Rendering.Expressions;
+
+    public static class ExpressionExtensions
+    {
+        public static string MemberWithoutInstance(this LambdaExpression expression)
+        {
+            return ExpressionHelper.GetExpressionText(expression);
+        }
+    }
+}
