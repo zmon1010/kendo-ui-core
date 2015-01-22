0ad97421879bec992e0f13f052e028dc73fb35ff
Nikolay Rusev <nikolay.rusev@telerik.com>
2015-01-22 16:40:02 +0200

    Add TreeList JSP demos for column reordering and column menu

---
+++ wrappers/java/spring-demos/src/main/java/com/kendoui/spring/controllers/treelist/ColumnMenuController.java
@@ -0,0 +1,33 @@
+package com.kendoui.spring.controllers.treelist;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kendoui.spring.models.DetailedEmployeeDirectory;
+import com.kendoui.spring.models.EmployeeDirectoryDao;
+
+
+@Controller("treelist-column-menu-controller")
+@RequestMapping(value="/treelist/")
+public class ColumnMenuController {
+    
+    @Autowired 
+    private EmployeeDirectoryDao employeeDirectory;
+    
+    @RequestMapping(value = "/column-menu", method = RequestMethod.GET)
+    public String index() {
+        return "treelist/column-menu";
+    }
+        
+    @RequestMapping(value = "/column-menu/read", method = RequestMethod.POST)
+    public @ResponseBody List<DetailedEmployeeDirectory> read(@RequestBody Map<String, Object> model) {
+        return employeeDirectory.getByEmployeeId((Integer)model.get("id"));
+    }   
+}
