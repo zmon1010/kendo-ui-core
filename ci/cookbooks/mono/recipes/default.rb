c888dae578450e1b27c0dcf95c4c59744c9632de
Petyo Ivanov <underlog@gmail.com>
2015-01-24 14:37:28 +0200

    Update Mono to 3.* Xamarin

--- ci/cookbooks/mono/recipes/default.rb
+++ ci/cookbooks/mono/recipes/default.rb
@@ -1,10 +1,24 @@
-package "mono-devel"
+apt_repository "mono" do
+    uri "http://download.mono-project.com/repo/debian"
+    distribution "wheezy"
+    components ["main"]
+    keyserver "hkp://keyserver.ubuntu.com:80/"
+    key "3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+end
+
+# needed for 12.04
+apt_repository "mono-tiffcompat" do
+    uri "http://download.mono-project.com/repo/debian"
+    distribution "wheezy-libtiff-compat"
+    components ["main"]
+    keyserver "hkp://keyserver.ubuntu.com:80/"
+    key "3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+end
+
+package "mono-complete"
 package "mono-xsp4"
 package "mono-xbuild"
-
-bash "setup moz certificates" do
-    code "mozroots --import --sync --machine"
-end
+package "nuget"
 
 remote_directory '/usr/lib/mono/4.0' do
     source 'assemblies'
