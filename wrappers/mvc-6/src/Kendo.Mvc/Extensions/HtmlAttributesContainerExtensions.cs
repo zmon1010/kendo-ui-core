1c92c74f4d7bd066d59da9190b008751dd8ccaa5
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2015-01-27 18:46:31 +0200

    Fix line endings

---
+++ wrappers/mvc-6/src/Kendo.Mvc/Extensions/HtmlAttributesContainerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Kendo.Mvc.UI;
+using System;
+
+namespace Kendo.Mvc.Extensions
+{
+    public static class HtmlAttributesContainerExtensions
+    {
+        public static void ThrowIfClassIsPresent(this IHtmlAttributesContainer container, string @class, string message)
+        {
+            object value;
+
+            if (container.HtmlAttributes.TryGetValue("class", out value))
+            {
+                if (value != null)
+                {
+                    var classes = value.ToString().Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                    if (Array.IndexOf(classes, @class) > -1)
+                    {
+                        throw new NotSupportedException(message.FormatWith(@class));
+                    }
+                }
+            }
+        }
+    }
+}
