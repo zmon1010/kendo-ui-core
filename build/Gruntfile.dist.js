b177062dc7a16deda5555452dd7fe16e3239bd57
Petyo Ivanov <underlog@gmail.com>
2015-02-16 16:43:54 +0200

    Fix nuget packages generation

--- build/Gruntfile.dist.js
+++ build/Gruntfile.dist.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     cwd: "styles",
-                    src: ["**/*.less", "**/*.woff", "**/*.ttf", "**/*.png", "**/*.gif", "**/*.css", "**/*.svg", "**/LICENSE" ],
+                    src: ["**/*.less", "**/*.woff", "**/*.ttf", "**/*.png", "**/*.gif", "**/*.css", "**/*.svg", "**/LICENSE", "**/LICENSE.txt" ],
                     dest: '<%= kendo.options.stylesDestDir %>/',
                 }]
             }
