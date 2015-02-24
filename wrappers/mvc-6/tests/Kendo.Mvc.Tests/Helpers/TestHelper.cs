133be911788a517ca439ce066e62aa88ba52425d
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2015-02-24 17:05:58 +0200

    Fix viewcontext mock

--- wrappers/mvc-6/tests/Kendo.Mvc.Tests/Helpers/TestHelper.cs
+++ wrappers/mvc-6/tests/Kendo.Mvc.Tests/Helpers/TestHelper.cs
@@ -9,6 +9,7 @@ using System.IO;
 using Xunit;
 using Microsoft.AspNet.Http;
 using Kendo.Mvc.UI;
+using Kendo.Mvc.Rendering;
 
 namespace Kendo.Mvc.Tests
 {
@@ -16,22 +17,44 @@ namespace Kendo.Mvc.Tests
     {
         public static ViewContext CreateViewContext()
         {
-            var activator = new Mock<IViewComponentActivator>();
-            activator.Setup(a => a.Activate(It.IsAny<Object>(), It.IsAny<ViewContext>()))
-                     .Callback((Object w, ViewContext v) => ((WidgetBase)w).HtmlHelper = Mock.Of<ITestableHtmlHelper>());
-
-            var httpContext = new Mock<HttpContext>();
-            httpContext.Setup(m => m.RequestServices.GetService(typeof(IViewComponentActivator)))
-                   .Returns(activator.Object);
-
+            var httpContext = new DefaultHttpContext();
             var urlHelper = new Mock<IUrlHelper>();
-            httpContext.Setup(m => m.RequestServices.GetService(typeof(IUrlHelper)))
-                  .Returns(urlHelper.Object);
+            var htmlHelper = new Mock<ITestableHtmlHelper>();
+            var urlGenerator = new Mock<IUrlGenerator>();
+            var kendoHtmlGenerator = new Mock<IKendoHtmlGenerator>();
+            var provider = new EmptyModelMetadataProvider();
 
-            var actionContext = new ActionContext(httpContext.Object, new RouteData(), new ActionDescriptor());
-            var viewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary());
+            var serviceProvider = new Mock<IServiceProvider>();
+            serviceProvider
+                .Setup(s => s.GetService(typeof(IModelMetadataProvider)))
+                .Returns(provider);
+
+            serviceProvider
+                .Setup(s => s.GetService(typeof(IUrlHelper)))
+                .Returns(urlHelper.Object);
+
+            serviceProvider
+                .Setup(s => s.GetService(typeof(IUrlGenerator)))
+                .Returns(urlGenerator.Object);
+
+            serviceProvider
+                .Setup(s => s.GetService(typeof(IHtmlHelper)))
+                .Returns(htmlHelper.Object);
+
+            serviceProvider
+                .Setup(s => s.GetService(typeof(IKendoHtmlGenerator)))
+                .Returns(kendoHtmlGenerator.Object);
+
+            serviceProvider
+                .Setup(s => s.GetService(typeof(IViewComponentActivator)))
+                .Returns(new DefaultViewComponentActivator());
+
+            httpContext.RequestServices = serviceProvider.Object;
+
+            var actionContext = new ActionContext(httpContext, new RouteData(), new ActionDescriptor());
+            var viewData = new ViewDataDictionary(provider, new ModelStateDictionary());
             var viewContext = new ViewContext(actionContext, Mock.Of<IView>(), viewData, TextWriter.Null);
-
+            
             return viewContext;
         }
     }
