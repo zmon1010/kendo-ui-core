af5b7e352f750dbeb4c59eb4f9b84f3dcc29ef45
Alex Gyoshev <alex@gyoshev.net>
2015-03-04 18:16:42 +0200

    Adjust MVC project to generate Trial assemblies

--- wrappers/mvc/src/Kendo.Mvc/Properties/AssemblyInfo.cs
+++ wrappers/mvc/src/Kendo.Mvc/Properties/AssemblyInfo.cs
@@ -5,7 +5,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
 
-[assembly: AssemblyTitle("Kendo.Mvc.Web")]
 [assembly: AssemblyProduct("Kendo.Mvc.Web")]
 [assembly: AssemblyDescription("Kendo Web Extensions for ASP.NET MVC")]
 [assembly: NeutralResourcesLanguage("en", UltimateResourceFallbackLocation.MainAssembly)]
