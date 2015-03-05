19dbdd48d6c2a0c58934e2daaa9ab61930cccd47
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2015-03-05 17:56:33 +0200

    Add grid test page some dummy binding

--- wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Grid/IndexController.cs
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Grid/IndexController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using System.Linq;
+using Kendo.Mvc.Examples.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNet.Mvc;
 
 namespace Kendo.Mvc.Examples.Controllers
 {
@@ -8,5 +12,15 @@ namespace Kendo.Mvc.Examples.Controllers
         {
             return View();
         }
-    }
+
+		public IActionResult Products([DataSourceRequest] DataSourceRequest request)
+		{
+			var data = Enumerable.Range(1, 200).Select(i => new Product {
+				ProductID = i,
+				ProductName = "Product" + i,
+				UnitPrice = i*3.14m
+			});
+			return Json(data.ToDataSourceResult(request));
+		}
+	}
 }
---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Kendo.Mvc.Examples.Models
+{
+    public class Product
+    {
+		public int ProductID { get; set; }
+		public decimal UnitPrice { get; set; }
+		public string ProductName { get; set; }
+	}
+}
