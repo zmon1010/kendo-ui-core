6a9a7a0f540cf804a97822771704817f870fe1d0
Iliana Nikolova <Iliana.Nikolova@telerik.com>
2015-03-12 12:10:59 +0200

    Update demo styling

--- demos/mvc/Controllers/ResponsiveController.cs
+++ demos/mvc/Controllers/ResponsiveController.cs
@@ -10,26 +10,31 @@ namespace Kendo.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.PageClassName = "responsive-index";
             return View();
         }
     
         public ActionResult Grid()
         {
+            ViewBag.PageClassName = "responsive-grid";
             return View();
         }
 
         public ActionResult Scheduler()
         {
+            ViewBag.PageClassName = "responsive-scheduler";
             return View();
         }
 
         public ActionResult Chart()
         {
+            ViewBag.PageClassName = "responsive-chart";
             return View();
         }
 
         public ActionResult ResponsivePanel()
         {
+            ViewBag.PageClassName = "responsive-panel";
             return View();
         }
     }
