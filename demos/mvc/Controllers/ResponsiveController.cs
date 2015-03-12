79cc9caf26fb4dc073d7d27b9e0cd769584942d3
Iliana Nikolova <Iliana.Nikolova@telerik.com>
2015-03-12 17:30:34 +0200

    Refactoring controllers, deleting code, yay

--- demos/mvc/Controllers/ResponsiveController.cs
+++ demos/mvc/Controllers/ResponsiveController.cs
@@ -10,31 +10,26 @@ namespace Kendo.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.PageClassName = "responsive-index";
             return View();
         }
     
         public ActionResult Grid()
         {
-            ViewBag.PageClassName = "responsive-grid";
             return View();
         }
 
         public ActionResult Scheduler()
         {
-            ViewBag.PageClassName = "responsive-scheduler";
             return View();
         }
 
         public ActionResult Chart()
         {
-            ViewBag.PageClassName = "responsive-chart";
             return View();
         }
 
         public ActionResult ResponsivePanel()
         {
-            ViewBag.PageClassName = "responsive-panel";
             return View();
         }
     }
