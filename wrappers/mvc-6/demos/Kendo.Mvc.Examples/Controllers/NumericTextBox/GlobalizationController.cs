f3b8cc326d79ddea142d631f28b7b7239e8fe815
Nikolay Rusev <nikolay.rusev@telerik.com>
2015-03-17 14:36:43 +0200

    Add more NumericTextBox demos

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/NumericTextBox/GlobalizationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class NumericTextBoxController : Controller
+    {
+        public ActionResult Globalization()
+        {
+            return View();
+        }
+    }
+}
