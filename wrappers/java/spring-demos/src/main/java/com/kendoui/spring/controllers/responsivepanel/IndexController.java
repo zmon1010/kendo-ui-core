e2135bed5843ec02a7adf7c80bbc8b59f34aecfa
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2015-03-18 19:01:49 +0200

    Rename responsive panel demo to responsive-panel

--- wrappers/java/spring-demos/src/main/java/com/kendoui/spring/controllers/responsivepanel/IndexController.java
+++ wrappers/java/spring-demos/src/main/java/com/kendoui/spring/controllers/responsivepanel/IndexController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller("responsivepanel-home-controller")
-@RequestMapping(value="/responsivepanel/")
+@RequestMapping(value="/responsive-panel/")
 public class IndexController {
     
     @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
     public String index() {
-        return "responsivepanel/index";
+        return "responsive-panel/index";
     }    
 }
