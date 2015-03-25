6ca8e5a19da4c36bdb318fc787643810653569ae
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2015-03-25 14:58:17 +0200

    Add local binding grid demo

--- wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Grid/IndexController.cs
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Grid/IndexController.cs
@@ -1,22 +1,56 @@
-﻿using Kendo.Mvc.Examples.Models;
+﻿using System.Collections.Generic;
+using Kendo.Mvc.Examples.Models;
 using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
+using System.Linq;
 using Microsoft.AspNet.Mvc;
 
 namespace Kendo.Mvc.Examples.Controllers
 {
-	public class GridController : Controller
+	public partial class GridController : Controller
     {
-        public IActionResult Index()
+		private ProductService productService;
+
+		public GridController()
+		{
+			productService = new ProductService(new SampleEntitiesDataContext());
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			productService.Dispose();
+
+			base.Dispose(disposing);
+		}
+
+		public IActionResult Index()
         {
             return View();
         }
 
 		public IActionResult Customers_Read([DataSourceRequest] DataSourceRequest request)
 		{
-			var db = new SampleEntitiesDataContext();
+			return Json(GetCustomers().ToDataSourceResult(request));
+		}
 
-			return Json(db.Customers.ToDataSourceResult(request));
+		private static IEnumerable<CustomerViewModel> GetCustomers()
+		{
+			var northwind = new SampleEntitiesDataContext();
+			return northwind.Customers.Select(customer => new CustomerViewModel
+			{
+				CustomerID = customer.CustomerID,
+				CompanyName = customer.CompanyName,
+				ContactName = customer.ContactName,
+				ContactTitle = customer.ContactTitle,
+				Address = customer.Address,
+				City = customer.City,
+				Region = customer.Region,
+				PostalCode = customer.PostalCode,
+				Country = customer.Country,
+				Phone = customer.Phone,
+				Fax = customer.Fax,
+				Bool = customer.Bool
+			});
 		}
 	}
 }
