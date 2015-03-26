7666ea477f7edf2d7b3597427bec649bc05c3903
Nikolay Rusev <nikolay.rusev@telerik.com>
2015-03-26 13:56:08 +0200

    Add Mobile ListView Local Virtualization example for MVC. Closes #3033

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Mobile_ListView/Local_VirtualizationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.Examples.Models;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class Mobile_ListViewController : Controller
+    {       
+        public ActionResult Local_Virtualization()
+        {
+            return View(productService.Read());
+        }        
+    }
+}
