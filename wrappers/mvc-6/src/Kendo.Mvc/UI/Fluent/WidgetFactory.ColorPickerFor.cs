539e67f7c4f4f9f5cab821f2776f7d882358262e
Raisolution <d.madjarov@gmail.com>
2015-03-26 16:14:17 +0200

    Add ColorPickerFor builder

---
+++ wrappers/mvc-6/src/Kendo.Mvc/UI/Fluent/WidgetFactory.ColorPickerFor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Kendo.Mvc.UI.Fluent
+{
+	public partial class WidgetFactory<TModel>
+	{
+		/// <summary>
+		/// Creates a new <see cref="ColorPicker"/>.
+		/// </summary>
+		/// <example>
+		/// <code lang="CS">
+		///  &lt;%= Html.Kendo().ColorPickerFor(m=>m.Property) %&gt;
+		/// </code>
+		/// </example>
+		public virtual ColorPickerBuilder ColorPickerFor(Expression<Func<TModel, String>> expression)
+		{
+			var metadata = GetModelMetadata(expression);
+			//var rules = HtmlHelper.GetClientValidationRules(metadata, expression.Name);
+
+			var widget = ColorPicker()
+				.Expression(GetExpressionName(expression))
+				.Value(metadata.Model as String);
+
+			return widget;
+		}
+	}
+}
