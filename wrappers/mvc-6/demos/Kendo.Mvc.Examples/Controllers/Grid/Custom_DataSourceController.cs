bf6ff299d245235b4bf4b7224ebb7c65114a1c4a
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2015-03-30 13:55:04 +0300

    Add custom datasource grid demo

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Grid/Custom_DataSourceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+	public partial class GridController : Controller
+    {
+        public ActionResult Custom_DataSource()
+        {
+            return View();
+        }
+    }
+}
