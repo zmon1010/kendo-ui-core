42693ce4fb806b7fc81a31a2a2c85fb216a8e01f
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2015-03-30 18:12:28 +0300

    Add grid custom client validation demo

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Models/CustomValidationProductViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Microsoft.AspNet.Mvc.ModelBinding;
+using System.Text.RegularExpressions;
+using System;
+
+namespace Kendo.Mvc.Examples.Models
+{
+	public class CustomValidationProductViewModel
+	{
+		public int ProductID { get; set; }
+
+		[Required]
+		[CustomProductNameValidation(ErrorMessage = "ProductName should start with capital letter")]
+		public string ProductName { get; set; }
+
+		[DataType(DataType.Currency)]
+		public decimal UnitPrice { get; set; }
+	}
+
+	public class CustomProductNameValidationAttribute : ValidationAttribute, IClientModelValidator
+	{		
+		public override bool IsValid(object value)
+		{
+			var productName = (string)value;
+			if (!string.IsNullOrEmpty(productName))
+			{
+				return Regex.IsMatch(productName, "^[A-Z]");
+			}
+			return true;
+		}
+
+		public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ClientModelValidationContext context)
+		{
+			yield return new ModelClientValidationRule("productnamevalidation", ErrorMessage);
+		}
+	}
+}
