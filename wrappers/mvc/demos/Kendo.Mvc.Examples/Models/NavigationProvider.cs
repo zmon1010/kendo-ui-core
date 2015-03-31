7e940ff49c34f74e15cb625468dc8ad776b5a6fd
Petyo Ivanov <underlog@gmail.com>
2015-03-31 16:55:46 +0300

    Fix mobile demos in MVC

--- wrappers/mvc/demos/Kendo.Mvc.Examples/Models/NavigationProvider.cs
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Models/NavigationProvider.cs
@@ -13,8 +13,52 @@ namespace Kendo.Mvc.Examples.Models
 
         public static IEnumerable<NavigationWidget> SuiteWidgets()
         {
+            var TITLES = new Dictionary<String, String>() {
+                { "application", "Application"},
+                { "buttongroup", "ButtonGroup"},
+                { "actionsheet", "ActionSheet"},
+                { "collapsible", "Collapsilble"},
+                { "drawer", "Drawer"},
+                { "mobile-button", "Button"},
+                { "mobile-forms", "Forms"},
+                { "mobile-layout", "Layout"},
+                { "mobile-listview", "ListView"},
+                { "mobile-tabstrip", "TabStrip"},
+                { "mobile-view", "View"},
+                { "modalview", "ModalView"},
+                { "navbar", "NavBar"},
+                { "popover", "PopOver"},
+                { "scroller", "Scroller"},
+                { "scrollview", "ScrollView"},
+                { "splitview", "SplitView"},
+                { "switch", "Switch"},
+                { "touchevents", "Touch Events" }
+            };
+
+
+
             var navigationJson = IOFile.ReadAllText(HttpContext.Current.Server.MapPath("~/Content/nav.json"));
-            return Serializer.Deserialize<IEnumerable<NavigationWidget>>(navigationJson);
+
+            var mobileNavJson = IOFile.ReadAllText(HttpContext.Current.Server.MapPath("~/Content/mobile-nav.json"));
+
+            var mobileItems = Serializer.Deserialize<IEnumerable<NavigationExample>>(mobileNavJson);
+
+            var widgets = Serializer.Deserialize<IEnumerable<NavigationWidget>>(navigationJson).ToList();
+            foreach (var item in mobileItems)
+            {
+                var widgetName = item.Url.Split('/')[0];
+                var widget = widgets.Find(w => w.Name == widgetName);
+
+                if (widget == null)
+                {
+                    widget = new NavigationWidget { Name = widgetName, Text = TITLES[widgetName], Items = new List<NavigationExample>() };
+                    widgets.Add(widget);
+                }
+
+                widget.Items.Add(item);
+            }
+
+            return widgets;
         }
     }
 }
