6bd0541ac34d16cf3ad4b382020682c1aff0ec34
Nikolay Rusev <nikolay.rusev@telerik.com>
2015-04-01 15:25:43 +0300

    Add TreeList to WidgetFactory

---
+++ wrappers/mvc-6/src/Kendo.Mvc/UI/Fluent/WidgetFactory.TreeList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Kendo.Mvc.UI.Fluent
+{
+    public partial class WidgetFactory<TModel>
+    {
+		/// <summary>
+		/// Creates a <see cref="TreeList"/>
+		/// </summary>
+		/// <example>
+		/// <code lang="CS">
+		/// @(Html.Kendo().TreeList()
+		///             .Name("TreeList")
+		/// )
+		/// </code>
+		/// </example>
+		public virtual TreeListBuilder<T> TreeList<T>() where T : class
+		{
+			return new TreeListBuilder<T>(new TreeList<T>(HtmlHelper.ViewContext));
+		}
+	}
+}
