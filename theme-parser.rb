aa5cef5417a086d0c486783f9130b3c64a5fabd1
Alex Gyoshev <alex@gyoshev.net>
2015-04-01 18:03:13 +0300

    Refactor, use functions based on color diffs, generate contrast diffs

--- theme-parser.rb
+++ theme-parser.rb
@@ -3,6 +3,7 @@
 require 'erb'
 require 'less'
 require 'color'
+require 'color/rgb/contrast'
 
 def read(filename)
     File.readlines(filename).map { |line| line.gsub(/\r?\n/, '') }
@@ -25,7 +26,7 @@ def parse(lines)
         vars.each { |var, value|
             match = /^@(.*)$/.match(value)
 
-            if match
+            if match and vars[match[1]] =~ /\S+/ then
                 matches = true
                 vars[var] = vars[match[1]]
             end
@@ -101,7 +102,7 @@ BASE_VAR_NAMES = BASE_VARS.values.first.keys
 
 OLD_VAR_NAMES = OLD_VARS.values.first.keys
 
-themes = BASE_VARS.keys
+THEMES = BASE_VARS.keys
 theme_dir = File.dirname(files.first)
 theme_template = File.read(File.join(theme_dir, "theme-template.less"))
 
@@ -109,67 +110,96 @@ rewritten = OLD_VAR_NAMES.select { |x| !(BASE_VAR_NAMES.include? x) }
 
 BASE_ARRAYS = {}
 
-themes.each do |theme|
-    BASE_VARS[theme].each do |variable, value|
-        BASE_ARRAYS[variable] = [] unless BASE_ARRAYS[variable]
-
-        BASE_ARRAYS[variable].push(value)
-    end
-end
-
-actions = {}
-
-def match_var old_values
-    BASE_ARRAYS.key old_values
-end
-
-
-def similar a, b
-    a == b
-end
-
 def transform_color color
     color.gsub!(/([a-fA-F]|[0-9])/, '\1\1') if color.length == 4
 
     color
 end
 
+def exact_match old_values
+    BASE_ARRAYS.key old_values
+end
+
+DIFF = {}
+
+def color_diff a, b
+    key = [a,b]
+    unless DIFF[key] then
+        begin
+            a = Color::RGB.from_html(a)
+            b = Color::RGB.from_html(b)
+
+            DIFF[key] = a.contrast(b)
+        rescue
+            DIFF[key] = 1.0
+        end
+    end
+
+    DIFF[key]
+end
+
+def similarity a, b
+    a.each_with_index.map { |x,i| color_diff x, b[i] } .reduce(:+)
+end
+
+def all_colors array
+    array.index { |v| v =~ /^#([a-fA-F]|[0-9])+$/ }
+end
+
 def transform old_values
-    color = /^#([a-fA-F]|[0-9])+$/
-
-    return if old_values.index { |v| not (v =~ color) }
-
-    old_values = old_values.map { |v| transform_color v }
+    return unless all_colors old_values
 
     # find function that transforms a variable from BASE_ARRAYS to old_values
-    BASE_ARRAYS.each do |variable, values|
+    best_fits = BASE_ARRAYS.map do |variable, values|
         next unless values.join(' ') =~ /^(#([a-fA-F]|[0-9])+\s*)+$/
 
         functions = function_map values
 
-        matches = functions.find { |f, c| similar(c, old_values) }
-
-        return matches[0].gsub(/VARIABLE/, variable) if matches
+        functions
+            .map { |function, colors| [function, similarity(colors, old_values), variable] }
+            .min { |a, b| a[1] <=> b[1] }
     end
 
-    "lighten(@accent, 20%)"
+    function, delta, variable = best_fits.compact.min { |a, b| a[1] <=> b[1] }
+
+    return function.gsub(/VARIABLE/, variable) if delta < 2
+
+    "UNKNOWN: #{old_values.join(' ')}"
 end
 
-rewritten.each do |variable|
-    old_values = []
+THEMES.each do |theme|
+    BASE_VARS[theme].each do |variable, value|
+        BASE_ARRAYS[variable] = [] unless BASE_ARRAYS[variable]
 
-    themes.each do |theme|
-        old_values.push OLD_VARS[theme][variable]
+        BASE_ARRAYS[variable].push(transform_color value)
+    end
+end
+
+def determine_actions(variables)
+    actions = {}
+
+    variables.each do |variable|
+        old_values = []
+
+        THEMES.each do |theme|
+            old_values.push OLD_VARS[theme][variable]
+        end
+
+        old_values.map!(&method(:transform_color)) if all_colors old_values
+
+        # determine action for variables
+        if same_values? old_values
+            actions[variable] = old_values[0]
+        elsif exact_match old_values
+            actions[variable] = "@#{exact_match old_values}"
+        elsif all_colors old_values
+            actions[variable] = transform old_values
+        else
+            actions[variable] = old_values[0]
+        end
     end
 
-    # determine action for variables
-    if same_values? old_values
-        actions[variable] = old_values[0]
-    elsif match_var old_values
-        actions[variable] = "@#{match_var old_values}"
-    else
-        actions[variable] = transform old_values
-    end
+    actions
 end
 
 def apply_actions variables, actions
@@ -184,7 +214,7 @@ end
 ####
 # write new type
 
-type_content = apply_actions(rewritten, actions) + "\n" + theme_template
+type_content = apply_actions(rewritten, determine_actions(rewritten)) + "\n" + theme_template
 
 File.open(File.join(theme_dir, "type-#{type}.less"), "w") do |f|
   f.write type_content.gsub(/\r?\n/, "\n")
