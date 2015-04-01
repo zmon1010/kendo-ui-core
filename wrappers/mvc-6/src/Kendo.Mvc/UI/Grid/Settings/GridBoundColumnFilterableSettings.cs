42f7356508d51291f96c7a37464ea8f4bf661dc4
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2015-04-01 16:57:12 +0300

    Add multi-checkbox filtering and copy to excel grid demos

--- wrappers/mvc-6/src/Kendo.Mvc/UI/Grid/Settings/GridBoundColumnFilterableSettings.cs
+++ wrappers/mvc-6/src/Kendo.Mvc/UI/Grid/Settings/GridBoundColumnFilterableSettings.cs
@@ -11,32 +11,56 @@ namespace Kendo.Mvc.UI
 		{
 			Enabled = true;
 			FilterUIHandler = new ClientHandlerDescriptor();
+			ItemTemplate = new ClientHandlerDescriptor();
 			CellSettings = new GridColumnFilterableCellSettings(modelMetadataProvider);
+
+			DataSource = new DataSource(modelMetadataProvider);
+			DataSource.Transport.SerializeEmptyPrefix = false;
+			DataSource.Schema.Data = "";
+			DataSource.Schema.Total = "";
+			DataSource.Schema.Errors = "";
 		}
 
 		public GridFilterUIRole FilterUIRole { get; set; }
         public ClientHandlerDescriptor FilterUIHandler { get; set; }
-        public GridColumnFilterableCellSettings CellSettings { get; }
+		public bool Multi { get; set; }
+		public ClientHandlerDescriptor ItemTemplate { get; set; }
+		public bool CheckAll { get; set; }
+		public GridColumnFilterableCellSettings CellSettings { get; }
+		public DataSource DataSource { get; set; }
 
-        protected override void Serialize(IDictionary<string, object> json)
-        {            
-            base.Serialize(json);
+		protected override void Serialize(IDictionary<string, object> json)
+		{
+			base.Serialize(json);
 
-            if (FilterUIHandler.HasValue())
-            {
-                json["ui"] = FilterUIHandler;                
-            }
-            else if (FilterUIRole != GridFilterUIRole.Default)
-            {
-                json["ui"] = Enum.GetName(typeof(GridFilterUIRole), FilterUIRole).ToLowerInvariant();                
-            }
+			if (FilterUIHandler.HasValue())
+			{
+				json["ui"] = FilterUIHandler;
+			}
+			else if (FilterUIRole != GridFilterUIRole.Default)
+			{
+				json["ui"] = Enum.GetName(typeof(GridFilterUIRole), FilterUIRole).ToLowerInvariant();
+			}
 
-           var cellSettings = CellSettings.ToJson();
-           if (cellSettings.Any())
-            {
-                json["cell"] = cellSettings;
-            }
-            
-        }
+			var cellSettings = CellSettings.ToJson();
+			if (cellSettings.Any())
+			{
+				json["cell"] = cellSettings;
+			}
+
+			if (Multi)
+			{
+				json["multi"] = Multi;
+			}
+			if (ItemTemplate.HasValue())
+			{
+				json["itemTemplate"] = ItemTemplate;
+			}
+			if (CheckAll == false)
+			{
+				json["checkAll"] = CheckAll;
+			}
+
+		}
     }
 }
