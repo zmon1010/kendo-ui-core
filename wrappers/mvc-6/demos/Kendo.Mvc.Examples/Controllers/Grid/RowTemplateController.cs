72d1603586654f7e085ae73544648a302f1153d3
Rosen Konstantinov <rosen.konstantinov@telerik.com>
2015-04-03 15:52:09 +0300

    Add row template grid demo

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Grid/RowTemplateController.cs
@@ -0,0 +1,19 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNet.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class GridController : Controller
+    {
+        public ActionResult RowTemplate()
+        {
+            return View();
+        }
+
+        public ActionResult RowTemplate_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(GetEmployees().ToDataSourceResult(request));
+        }
+    }
+}
