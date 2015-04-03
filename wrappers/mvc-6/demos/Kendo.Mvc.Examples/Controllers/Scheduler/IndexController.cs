36cc56eddfa2e9b903f27b3a641397cd820791d2
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2015-04-03 14:51:45 +0300

    Update Scheduler Basic Usage demo.

--- wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Scheduler/IndexController.cs
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Scheduler/IndexController.cs
@@ -1,12 +1,73 @@
-﻿using Microsoft.AspNet.Mvc;
-
-namespace Kendo.Mvc.Examples.Controllers
+﻿namespace Kendo.Mvc.Examples.Controllers
 {
-    public class SchedulerController : Controller
+    using Kendo.Mvc.Examples.Models.Scheduler;
+    using Kendo.Mvc.UI;
+    using Kendo.Mvc.Extensions;
+    using Microsoft.AspNet.Mvc;
+
+    public partial class SchedulerController : Controller
     {
+        private SchedulerTaskService taskService;
+        private SchedulerMeetingService meetingService;
+
+
+        public SchedulerController()
+        {
+            this.taskService = new SchedulerTaskService();
+            this.meetingService = new SchedulerMeetingService();
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public virtual JsonResult Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(taskService.GetAll().ToDataSourceResult(request));
+        }
+
+        public virtual JsonResult Destroy([DataSourceRequest] DataSourceRequest request, TaskViewModel task)
+        {
+            if (ModelState.IsValid)
+            {
+                taskService.Delete(task, ModelState);
+            }
+
+            return Json(new[] { task }.ToDataSourceResult(request, ModelState));
+        }
+
+        public virtual JsonResult Create([DataSourceRequest] DataSourceRequest request, TaskViewModel task)
+        {
+            if (ModelState.IsValid)
+            {
+                taskService.Insert(task, ModelState);
+            }
+
+            return Json(new[] { task }.ToDataSourceResult(request, ModelState));
+        }
+
+        public virtual JsonResult Update([DataSourceRequest] DataSourceRequest request, TaskViewModel task)
+        {
+            //example custom validation:
+            if (task.Start.Hour < 8 || task.Start.Hour > 22)
+            {
+                ModelState.AddModelError("start", "Start date must be in working hours (8h - 22h)");
+            }
+
+            if (ModelState.IsValid)
+            {
+                taskService.Update(task, ModelState);
+            }
+
+            return Json(new[] { task }.ToDataSourceResult(request, ModelState));
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            taskService.Dispose();
+            meetingService.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
