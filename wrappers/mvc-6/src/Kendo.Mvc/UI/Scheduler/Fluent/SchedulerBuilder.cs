79eb55cb0a249785fa2f41099c9908f3e1ed08db
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2015-04-07 16:53:31 +0300

    Add Scheduler workWeekStart/end and startTime/endTime missing builder overloads

--- wrappers/mvc-6/src/Kendo.Mvc/UI/Scheduler/Fluent/SchedulerBuilder.cs
+++ wrappers/mvc-6/src/Kendo.Mvc/UI/Scheduler/Fluent/SchedulerBuilder.cs
@@ -14,6 +14,108 @@ namespace Kendo.Mvc.UI.Fluent
         }
 
         /// <summary>
+        /// The start time of the week and day views. The scheduler will display events starting after the startTime.
+        /// </summary>
+        /// <param name="hours">The hours</param>
+        /// <param name="minutes">The minutes</param>
+        /// <param name="seconds">The seconds</param>
+        /// <example>
+        /// <code lang="Razor">
+        /// @(Html.Kendo().Scheduler&lt;Kendo.Mvc.Examples.Models.Scheduler.Screening&gt;()
+        ///     .Name(&quot;scheduler&quot;)
+        ///     .Date(new DateTime(2013, 6, 13))
+        ///     .StartTime(10, 0, 0)
+        ///     .BindTo(Model)
+        /// )
+        /// </code>
+        /// </example>
+
+        public SchedulerBuilder<T> StartTime(int hours, int minutes, int seconds)
+        {
+            var today = DateTime.Today;
+
+            Container.StartTime = new DateTime(today.Year, today.Month, today.Day, hours, minutes, seconds);
+
+            return this;
+        }
+
+        /// <summary>
+        /// The end time of the week and day views. The scheduler will display events ending before the endTime.
+        /// </summary>
+        /// <param name="hours">The hours</param>
+        /// <param name="minutes">The minutes</param>
+        /// <param name="seconds">The seconds</param>
+        /// <example>
+        /// <code lang="Razor">
+        /// @(Html.Kendo().Scheduler&lt;Kendo.Mvc.Examples.Models.Scheduler.Screening&gt;()
+        ///     .Name(&quot;scheduler&quot;)
+        ///     .Date(new DateTime(2013, 6, 13))
+        ///     .EndTime(10,0,0)
+        ///     .BindTo(Model)
+        /// )
+        /// </code>
+        /// </example>
+        public SchedulerBuilder<T> EndTime(int hours, int minutes, int seconds)
+        {
+            var today = DateTime.Today;
+
+            Container.EndTime = new DateTime(today.Year, today.Month, today.Day, hours, minutes, seconds);
+
+            return this;
+        }
+
+
+        /// <summary>
+        /// Sets the start of the work day when the  "Show business hours" button is clicked.
+        /// </summary>
+        /// <param name="hours">The hours</param>
+        /// <param name="minutes">The minutes</param>
+        /// <param name="seconds">The seconds</param>
+        /// <example>
+        /// <code lang="Razor">
+        /// @(Html.Kendo().Scheduler&lt;Kendo.Mvc.Examples.Models.Scheduler.Screening&gt;()
+        ///     .Name(&quot;scheduler&quot;)
+        ///     .Date(new DateTime(2013, 6, 13))
+        ///     .WorkDayStart(10, 0, 0)
+        ///     .BindTo(Model)
+        /// )
+        /// </code>
+        /// </example>
+        public SchedulerBuilder<T> WorkDayStart(int hours, int minutes, int seconds)
+        {
+            var today = DateTime.Today;
+
+            Component.WorkDayStart = new DateTime(today.Year, today.Month, today.Day, hours, minutes, seconds);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the end of the work day when the  "Show business hours" button is clicked.
+        /// </summary>
+        /// <param name="hours">The hours</param>
+        /// <param name="minutes">The minutes</param>
+        /// <param name="seconds">The seconds</param>
+        /// <example>
+        /// <code lang="Razor">
+        /// @(Html.Kendo().Scheduler&lt;Kendo.Mvc.Examples.Models.Scheduler.Screening&gt;()
+        ///     .Name(&quot;scheduler&quot;)
+        ///     .Date(new DateTime(2013, 6, 13))
+        ///     .WorkDayEnd(16,0,0)
+        ///     .BindTo(Model)
+        /// )
+        /// </code>
+        /// </example>
+        public SchedulerBuilder<T> WorkDayEnd(int hours, int minutes, int seconds)
+        {
+            var today = DateTime.Today;
+
+            Component.WorkDayEnd = new DateTime(today.Year, today.Month, today.Day, hours, minutes, seconds);
+
+            return this;
+        }
+
+        /// <summary>
         /// Binds the scheduler to a list of objects
         /// </summary>
         /// <param name="dataSource">The data source.</param>
