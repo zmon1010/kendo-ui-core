98d09938902035e13c76fbddb4a7c4e8b3272fc7
Raisolution <d.madjarov@gmail.com>
2015-04-27 18:32:25 +0300

    Add LinearGauge demos

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/LinearGauge/IndexController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class LinearGaugeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/LinearGauge/Multiple_PointersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class LinearGaugeController : Controller
+    {
+        public IActionResult Multiple_Pointers()
+        {
+            return View();
+        }
+    }
+}
