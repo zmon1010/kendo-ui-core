18e7cd3949fc658d7eaa61c2b53b5ac04c5c33ac
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2015-04-28 15:52:22 +0300

    Add Chart widget factory

---
+++ wrappers/mvc-6/src/Kendo.Mvc/UI/Fluent/WidgetFactory.Chart.cs
@@ -0,0 +1,10 @@
+﻿namespace Kendo.Mvc.UI.Fluent
+{
+    public partial class WidgetFactory<TModel>
+    {
+        public virtual ChartBuilder Chart()
+        {
+            return new ChartBuilder(new Chart(HtmlHelper.ViewContext));
+        }
+    }
+}
