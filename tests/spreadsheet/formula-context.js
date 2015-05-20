d04885d4b462a9e8f440515ae94684f0066a732c
Petyo Ivanov <underlog@gmail.com>
2015-05-20 18:51:30 +0300

    Make the simplest range ref test work

--- tests/spreadsheet/formula-context.js
+++ tests/spreadsheet/formula-context.js
@@ -1,7 +1,8 @@
 (function() {
     var context, sheet;
-
-    var makeCellRef = kendo.spreadsheet.calc.Runtime.makeCellRef;
+    var calc = kendo.spreadsheet.calc;
+    var makeCellRef = calc.Runtime.makeCellRef;
+    var makeRangeRef = calc.Runtime.makeRangeRef;
 
     function FormulaContext(sheets) {
         this.sheets = sheets;
@@ -9,17 +10,49 @@
 
     FormulaContext.prototype = {
         getRefCells: function(ref) {
-            var sheet = this.sheet(ref);
-            var formula = sheet.range(ref.row-1, ref.col-1).formula() || null;
-            var value = sheet.range(ref.row-1, ref.col-1).value();
+            if (ref instanceof calc.Runtime.CellRef) {
+                var sheet = this.sheet(ref);
+                var formula = sheet.range(ref.row-1, ref.col-1).formula() || null;
+                var value = sheet.range(ref.row-1, ref.col-1).value();
 
-            if (formula != null || value != null) {
-                return [ this.state(formula, value, ref) ];
-                return [{ formula: formula, value: value, row: ref.row, col: ref.col, sheet: sheet.name() }];
-            } else {
-                return [];
+                if (formula != null || value != null) {
+                    return [{
+                        formula: formula,
+                        value: value,
+                        row: ref.row,
+                        col: ref.col,
+                        sheet: ref.sheet
+                    }];
+                } else {
+                    return [];
+                }
             }
+            if (ref instanceof calc.Runtime.RangeRef) {
 
+                var sheet = this.sheet(ref);
+
+                var startCellIndex = sheet._grid.index(ref.topLeft.row-1, ref.topLeft.col-1);
+
+                var endCellIndex = sheet._grid.index(ref.bottomRight.row-1, ref.bottomRight.col-1);
+
+                var formulas = sheet._formulas.iterator(startCellIndex, endCellIndex);
+                var values = sheet._values.iterator(startCellIndex, endCellIndex);
+
+                var states = [];
+
+                for (var col = ref.topLeft.col; col <= ref.bottomRight.col; ++col) {
+                    for (var row = ref.topLeft.row; row <= ref.bottomRight.row; ++row) {
+                        var index = sheet._grid.index(row-1, col-1);
+                        var formula = formulas.at(index) || null;
+                        var value = values.at(index);
+                        if (formula != null || value != null) {
+                            states.push({ formula: formula, value: value, row: row, col: col, sheet: ref.sheet });
+                        }
+                    }
+                }
+
+                return states;
+            }
         },
 
         sheet: function(cellRef) {
@@ -27,13 +60,7 @@
         },
 
         state: function(formula, value, ref) {
-            return {
-                formula: formula,
-                value: value,
-                row: ref.row,
-                col: ref.col,
-                sheet: ref.sheet
-            };
+            return ;
         }
     };
 
@@ -42,6 +69,8 @@
             sheet = new kendo.spreadsheet.Sheet(10, 10, 10, 10);
             sheet.name("Sheet1");
             sheet.range(0, 0, 1, 1).value(1000);
+            sheet.range(0, 1, 1, 1).value(2000);
+            sheet.range(1, 1, 1, 1).value(3000);
 
             var sheets = {
                 "Sheet1": sheet
@@ -71,4 +100,16 @@
 
         equal(states.length, 0);
     });
+
+    test("gets the correct cell state as a reference", function() {
+        var a1 = makeCellRef(1, 1).setSheet("Sheet1");
+        var b2 = makeCellRef(2, 2).setSheet("Sheet1");
+
+        var range = makeRangeRef(a1, b2);
+        range.setSheet("Sheet1");
+
+        var states = context.getRefCells(range);
+        equal(states.length, 3);
+    });
+
 })();
