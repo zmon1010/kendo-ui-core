dab57c338b6bc4d660e35c55954e5b6294321bec
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2015-05-28 14:23:21 +0300

    Update mono recipes

--- ci/cookbooks/mono-fcgi/recipes/workstation.rb
+++ ci/cookbooks/mono-fcgi/recipes/workstation.rb
@@ -2,6 +2,8 @@
 package "mono-fastcgi-server4"
 
 template '/etc/init/mono-fcgi.conf' do
+    owner 'www-data'
+    group 'www-data'
     source 'workstation.conf.erb'
     variables({
         :kendo_dir => node['kendo_dir'] || File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', '..'))
