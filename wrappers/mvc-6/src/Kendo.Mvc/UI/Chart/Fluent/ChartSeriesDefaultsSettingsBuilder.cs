31d4c0f00f8474edb65b97dee5b88858e47916ad
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2015-05-29 16:04:56 +0300

    Add series default types for all types (semi-manual)

--- wrappers/mvc-6/src/Kendo.Mvc/UI/Chart/Fluent/ChartSeriesDefaultsSettingsBuilder.cs
+++ wrappers/mvc-6/src/Kendo.Mvc/UI/Chart/Fluent/ChartSeriesDefaultsSettingsBuilder.cs
@@ -21,12 +21,246 @@ namespace Kendo.Mvc.UI.Fluent
         }
 
         /// <summary>
-        /// The border of the series.
+        /// The Area series default settings.
         /// </summary>
-        /// <param name="configurator">The configurator for the border setting.</param>
+        /// <param name="configurator">The configurator for the Area series settings.</param>
         public ChartSeriesBuilder Area()
         {
             return new ChartSeriesBuilder(Container.Area);
+        }
+
+        /// <summary>
+        /// The Bar series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the Bar series settings.</param>
+        public ChartSeriesBuilder Bar()
+        {
+            return new ChartSeriesBuilder(Container.Bar);
+        }
+
+        /// <summary>
+        /// The Column series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the Column series settings.</param>
+        public ChartSeriesBuilder Column()
+        {
+            return new ChartSeriesBuilder(Container.Column);
+        }
+
+        /// <summary>
+        /// The Line series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the Line series settings.</param>
+        public ChartSeriesBuilder Line()
+        {
+            return new ChartSeriesBuilder(Container.Line);
+        }
+
+        /// <summary>
+        /// The VerticalArea series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the VerticalArea series settings.</param>
+        public ChartSeriesBuilder VerticalArea()
+        {
+            return new ChartSeriesBuilder(Container.VerticalArea);
+        }
+
+        /// <summary>
+        /// The VerticalLine series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the VerticalLine series settings.</param>
+        public ChartSeriesBuilder VerticalLine()
+        {
+            return new ChartSeriesBuilder(Container.VerticalLine);
+        }
+
+        /// <summary>
+        /// The RadarArea series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the RadarArea series settings.</param>
+        public ChartSeriesBuilder RadarArea()
+        {
+            return new ChartSeriesBuilder(Container.RadarArea);
+        }
+
+        /// <summary>
+        /// The RadarColumn series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the RadarColumn series settings.</param>
+        public ChartSeriesBuilder RadarColumn()
+        {
+            return new ChartSeriesBuilder(Container.RadarColumn);
+        }
+
+        /// <summary>
+        /// The RadarLine series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the RadarLine series settings.</param>
+        public ChartSeriesBuilder RadarLine()
+        {
+            return new ChartSeriesBuilder(Container.RadarLine);
+        }
+
+        /// <summary>
+        /// The BoxPlot series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the BoxPlot series settings.</param>
+        public ChartSeriesBuilder BoxPlot()
+        {
+            return new ChartSeriesBuilder(Container.BoxPlot);
+        }
+
+        /// <summary>
+        /// The Bubble series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the Bubble series settings.</param>
+        public ChartSeriesBuilder Bubble()
+        {
+            return new ChartSeriesBuilder(Container.Bubble);
+        }
+
+        /// <summary>
+        /// The Bullet series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the Bullet series settings.</param>
+        public ChartSeriesBuilder Bullet()
+        {
+            return new ChartSeriesBuilder(Container.Bullet);
+        }
+
+        /// <summary>
+        /// The VerticalBullet series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the VerticalBullet series settings.</param>
+        public ChartSeriesBuilder VerticalBullet()
+        {
+            return new ChartSeriesBuilder(Container.VerticalBullet);
+        }
+
+        /// <summary>
+        /// The Candlestick series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the Candlestick series settings.</param>
+        public ChartSeriesBuilder Candlestick()
+        {
+            return new ChartSeriesBuilder(Container.Candlestick);
+        }
+
+        /// <summary>
+        /// The OHLC series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the OHLC series settings.</param>
+        public ChartSeriesBuilder OHLC()
+        {
+            return new ChartSeriesBuilder(Container.OHLC);
+        }
+
+        /// <summary>
+        /// The Funnel series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the Funnel series settings.</param>
+        public ChartSeriesBuilder Funnel()
+        {
+            return new ChartSeriesBuilder(Container.Funnel);
+        }
+
+        /// <summary>
+        /// The RangeColumn series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the RangeColumn series settings.</param>
+        public ChartSeriesBuilder RangeColumn()
+        {
+            return new ChartSeriesBuilder(Container.RangeColumn);
+        }
+
+        /// <summary>
+        /// The RangeBar series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the RangeBar series settings.</param>
+        public ChartSeriesBuilder RangeBar()
+        {
+            return new ChartSeriesBuilder(Container.RangeBar);
+        }
+
+        /// <summary>
+        /// The Pie series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the Pie series settings.</param>
+        public ChartSeriesBuilder Pie()
+        {
+            return new ChartSeriesBuilder(Container.Pie);
+        }
+
+        /// <summary>
+        /// The Donut series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the Donut series settings.</param>
+        public ChartSeriesBuilder Donut()
+        {
+            return new ChartSeriesBuilder(Container.Donut);
+        }
+
+        /// <summary>
+        /// The Scatter series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the Scatter series settings.</param>
+        public ChartSeriesBuilder Scatter()
+        {
+            return new ChartSeriesBuilder(Container.Scatter);
+        }
+
+        /// <summary>
+        /// The ScatterLine series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the ScatterLine series settings.</param>
+        public ChartSeriesBuilder ScatterLine()
+        {
+            return new ChartSeriesBuilder(Container.ScatterLine);
+        }
+
+        /// <summary>
+        /// The PolarArea series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the PolarArea series settings.</param>
+        public ChartSeriesBuilder PolarArea()
+        {
+            return new ChartSeriesBuilder(Container.PolarArea);
+        }
+
+        /// <summary>
+        /// The PolarLine series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the PolarLine series settings.</param>
+        public ChartSeriesBuilder PolarLine()
+        {
+            return new ChartSeriesBuilder(Container.PolarLine);
+        }
+
+        /// <summary>
+        /// The PolarScatter series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the PolarScatter series settings.</param>
+        public ChartSeriesBuilder PolarScatter()
+        {
+            return new ChartSeriesBuilder(Container.PolarScatter);
+        }
+
+        /// <summary>
+        /// The Waterfall series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the Waterfall series settings.</param>
+        public ChartSeriesBuilder Waterfall()
+        {
+            return new ChartSeriesBuilder(Container.Waterfall);
+        }
+
+        /// <summary>
+        /// The HorizontalWaterfall series default settings.
+        /// </summary>
+        /// <param name="configurator">The configurator for the HorizontalWaterfall series settings.</param>
+        public ChartSeriesBuilder HorizontalWaterfall()
+        {
+            return new ChartSeriesBuilder(Container.HorizontalWaterfall);
         }
     }
 }
--- wrappers/mvc-6/src/Kendo.Mvc/UI/Chart/Settings/ChartSeriesDefaultsSettings.cs
+++ wrappers/mvc-6/src/Kendo.Mvc/UI/Chart/Settings/ChartSeriesDefaultsSettings.cs
@@ -13,6 +13,59 @@ namespace Kendo.Mvc.UI
     {
         public ChartSeries Area { get; } = new ChartSeries();
 
+        public ChartSeries Bar { get; } = new ChartSeries();
+
+        public ChartSeries Column { get; } = new ChartSeries();
+
+        public ChartSeries Line { get; } = new ChartSeries();
+
+        public ChartSeries VerticalArea { get; } = new ChartSeries();
+
+        public ChartSeries VerticalLine { get; } = new ChartSeries();
+
+        public ChartSeries RadarArea { get; } = new ChartSeries();
+
+        public ChartSeries RadarColumn { get; } = new ChartSeries();
+
+        public ChartSeries RadarLine { get; } = new ChartSeries();
+
+        public ChartSeries BoxPlot { get; } = new ChartSeries();
+
+        public ChartSeries Bubble { get; } = new ChartSeries();
+
+        public ChartSeries Bullet { get; } = new ChartSeries();
+
+        public ChartSeries VerticalBullet { get; } = new ChartSeries();
+
+        public ChartSeries Candlestick { get; } = new ChartSeries();
+
+        public ChartSeries OHLC { get; } = new ChartSeries();
+
+        public ChartSeries Funnel { get; } = new ChartSeries();
+
+        public ChartSeries RangeColumn { get; } = new ChartSeries();
+
+        public ChartSeries RangeBar { get; } = new ChartSeries();
+
+        public ChartSeries Pie { get; } = new ChartSeries();
+
+        public ChartSeries Donut { get; } = new ChartSeries();
+
+        public ChartSeries Scatter { get; } = new ChartSeries();
+
+        public ChartSeries ScatterLine { get; } = new ChartSeries();
+
+        public ChartSeries PolarArea { get; } = new ChartSeries();
+
+        public ChartSeries PolarLine { get; } = new ChartSeries();
+
+        public ChartSeries PolarScatter { get; } = new ChartSeries();
+
+        public ChartSeries Waterfall { get; } = new ChartSeries();
+
+        public ChartSeries HorizontalWaterfall { get; } = new ChartSeries();
+
+
         public Dictionary<string, object> Serialize()
         {
             var settings = new Dictionary<string, object>();
@@ -21,6 +74,162 @@ namespace Kendo.Mvc.UI
             if (area.Any())
             {
                 settings["area"] = area;
+            }
+
+            var bar = Bar.Serialize();
+            if (bar.Any())
+            {
+                settings["bar"] = bar;
+            }
+
+            var column = Column.Serialize();
+            if (column.Any())
+            {
+                settings["column"] = column;
+            }
+
+            var line = Line.Serialize();
+            if (line.Any())
+            {
+                settings["line"] = line;
+            }
+
+            var verticalArea = VerticalArea.Serialize();
+            if (verticalArea.Any())
+            {
+                settings["verticalArea"] = verticalArea;
+            }
+
+            var verticalLine = VerticalLine.Serialize();
+            if (verticalLine.Any())
+            {
+                settings["verticalLine"] = verticalLine;
+            }
+
+            var radarArea = RadarArea.Serialize();
+            if (radarArea.Any())
+            {
+                settings["radarArea"] = radarArea;
+            }
+
+            var radarColumn = RadarColumn.Serialize();
+            if (radarColumn.Any())
+            {
+                settings["radarColumn"] = radarColumn;
+            }
+
+            var radarLine = RadarLine.Serialize();
+            if (radarLine.Any())
+            {
+                settings["radarLine"] = radarLine;
+            }
+
+            var boxPlot = BoxPlot.Serialize();
+            if (boxPlot.Any())
+            {
+                settings["boxPlot"] = boxPlot;
+            }
+
+            var bubble = Bubble.Serialize();
+            if (bubble.Any())
+            {
+                settings["bubble"] = bubble;
+            }
+
+            var bullet = Bullet.Serialize();
+            if (bullet.Any())
+            {
+                settings["bullet"] = bullet;
+            }
+
+            var verticalBullet = VerticalBullet.Serialize();
+            if (verticalBullet.Any())
+            {
+                settings["verticalBullet"] = verticalBullet;
+            }
+
+            var candlestick = Candlestick.Serialize();
+            if (candlestick.Any())
+            {
+                settings["candlestick"] = candlestick;
+            }
+
+            var ohlc = OHLC.Serialize();
+            if (ohlc.Any())
+            {
+                settings["ohlc"] = ohlc;
+            }
+
+            var funnel = Funnel.Serialize();
+            if (funnel.Any())
+            {
+                settings["funnel"] = funnel;
+            }
+
+            var rangeColumn = RangeColumn.Serialize();
+            if (rangeColumn.Any())
+            {
+                settings["rangeColumn"] = rangeColumn;
+            }
+
+            var rangeBar = RangeBar.Serialize();
+            if (rangeBar.Any())
+            {
+                settings["rangeBar"] = rangeBar;
+            }
+
+            var pie = Pie.Serialize();
+            if (pie.Any())
+            {
+                settings["pie"] = pie;
+            }
+
+            var donut = Donut.Serialize();
+            if (donut.Any())
+            {
+                settings["donut"] = donut;
+            }
+
+            var scatter = Scatter.Serialize();
+            if (scatter.Any())
+            {
+                settings["scatter"] = scatter;
+            }
+
+            var scatterLine = ScatterLine.Serialize();
+            if (scatterLine.Any())
+            {
+                settings["scatterLine"] = scatterLine;
+            }
+
+            var polarArea = PolarArea.Serialize();
+            if (polarArea.Any())
+            {
+                settings["polarArea"] = polarArea;
+            }
+
+            var polarLine = PolarLine.Serialize();
+            if (polarLine.Any())
+            {
+                settings["polarLine"] = polarLine;
+            }
+
+            var polarScatter = PolarScatter.Serialize();
+            if (polarScatter.Any())
+            {
+                settings["polarScatter"] = polarScatter;
+            }
+
+            var waterfall = Waterfall.Serialize();
+            if (waterfall.Any())
+            {
+                settings["waterfall"] = waterfall;
+            }
+
+            var horizontalWaterfall = HorizontalWaterfall.Serialize();
+            if (horizontalWaterfall.Any())
+            {
+                settings["horizontalWaterfall"] = horizontalWaterfall;
             }
 
             return settings;
