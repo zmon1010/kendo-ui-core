27741606e0cdcf4b7e42f230028dd51046054f03
Georgi Krustev <ggkrustev@yahoo.com>
2015-06-09 18:13:41 +0300

    Add JSP MultiSelect demo for TagMode

---
+++ wrappers/java/spring-demos/src/main/java/com/kendoui/spring/controllers/multiselect/TagModeController.java
@@ -0,0 +1,42 @@
+
+package com.kendoui.spring.controllers.multiselect;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.kendoui.spring.models.DropDownListItem;
+
+@Controller("multiselect-tagmode-controller")
+@RequestMapping(value="/multiselect/")
+public class IndexController {
+
+    @RequestMapping(value = {"/tag-mode"}, method = RequestMethod.GET)
+    public String index(Model model) {
+
+        model.addAttribute("attendees", new String[] {
+            "Steven White",
+            "Nancy King",
+            "Anne King",
+            "Nancy Davolio",
+            "Robert Davolio",
+            "Michael Leverling",
+            "Andrew Callahan",
+            "Michael Suyama",
+            "Anne King",
+            "Laura Peacock",
+            "Robert Fuller",
+            "Janet White",
+            "Nancy Leverling",
+            "Robert Buchanan",
+            "Andrew Fuller",
+            "Anne Davolio",
+            "Andrew Suyama",
+            "Nige Buchanan",
+            "Laura Fuller"
+        });
+
+        return "multiselect/tag-mode";
+    }
+}
---
+++ wrappers/java/spring-demos/src/main/webapp/WEB-INF/views/multiselect/tag-mode.jsp
@@ -0,0 +1,27 @@
+<%@page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@page import="com.kendoui.spring.models.DropDownListItem"%>
+<%@taglib prefix="kendo" uri="http://www.kendoui.com/jsp/tags"%>
+<%@taglib prefix="demo" tagdir="/WEB-INF/tags"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<demo:header />
+	<%
+	String[] values = {"Anne King", "Andrew Fuller"};
+	%>
+    <div class="demo-section">
+        <h4>'Single' tag mode</h4>
+	    <kendo:multiSelect name="required" placeholder="Select attendees..." value="<%= values %>"
+            autoClose="false" tagMode="single">
+	        <kendo:dataSource data="${attendees}"></kendo:dataSource>
+        </kendo:multiSelect>
+        <h4>'Multiple' tag mode</h4>
+	    <kendo:multiSelect name="optional" autoClose="false" placeholder="Select attendees...">
+	        <kendo:dataSource data="${attendees}"></kendo:dataSource>
+	    </kendo:multiSelect>
+	</div>
+    <style>
+        .demo-section * + h4 {
+            margin-top: 2em;
+        }
+    </style>
+<demo:footer />
