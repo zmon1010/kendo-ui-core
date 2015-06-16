38cb78c19edefcb05f0906ea00b4dfa668be6dbf
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2015-06-16 10:00:11 +0300

    Upload based on size and in parallel

--- build/upload-docs.sh
+++ build/upload-docs.sh
@@ -7,23 +7,27 @@ HOST='172.17.49.82'
 PORT='33'
 USER='kendodocumentation'
 PASS='qGMQIUxq57'
-SOURCE_FOLDER="docs/_site/"
-DIST_FOLDER="docs/_site_sync"
+DOCS_SITE="docs/_site"
+MIRROR_OPTIONS="--reverse --delete --ignore-time --verbose"
 
 # Generate docs
-(cd docs && bundle --without development --path ~/gems && bundle exec jekyll build)
+# (cd docs && bundle --without development --path ~/gems && bundle exec jekyll build)
 
 # Clean-up generated wrappers
 rm -rf docs/api/wrappers/*
 
-# Sync first to avoid transferring duplicates
-rsync -av --delete $SOURCE_FOLDER $DIST_FOLDER
-
 lftp -e "
 open -p $PORT $HOST
 user $USER $PASS
-lcd $DIST_FOLDER
-mirror --reverse --delete --parallel=3 --verbose . kendouidocsweb1
-mirror --reverse --delete --parallel=3 --verbose . kendouidocsweb2
+lcd $DOCS_SITE
+mirror $MIRROR_OPTIONS . kendouidocsweb1
 bye
-"
+" &
+lftp -e "
+open -p $PORT $HOST
+user $USER $PASS
+lcd $DOCS_SITE
+mirror $MIRROR_OPTIONS . kendouidocsweb2
+bye
+" &
+wait
