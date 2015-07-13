b63e242cee97e6876c0fe934ac6d29b8f74da40c
Alex Gyoshev <alex@gyoshev.net>
2015-07-13 18:06:07 +0300

    Add undo/redo events, refactor tests

--- src/util/undoredostack.js
+++ src/util/undoredostack.js
@@ -3,37 +3,37 @@
 })(function(){
 
 (function(kendo) {
-    var UndoRedoStack = kendo.Class.extend({
-        init: function() {
+    var UndoRedoStack = kendo.Observable.extend({
+        init: function(options) {
+            kendo.Observable.fn.init.call(this, options);
             this.clear();
         },
-
+        events: [ "undo", "redo" ],
         push: function (command) {
             this.stack = this.stack.slice(0, this.currentCommandIndex + 1);
             this.currentCommandIndex = this.stack.push(command) - 1;
         },
-
         undo: function () {
             if (this.canUndo()) {
-                this.stack[this.currentCommandIndex--].undo();
+                var command = this.stack[this.currentCommandIndex--];
+                command.undo();
+                this.trigger("undo", { command: command });
             }
         },
-
         redo: function () {
             if (this.canRedo()) {
-                this.stack[++this.currentCommandIndex].redo();
+                var command = this.stack[++this.currentCommandIndex];
+                command.redo();
+                this.trigger("redo", { command: command });
             }
         },
-
         clear: function() {
             this.stack = [];
             this.currentCommandIndex = -1;
         },
-
         canUndo: function () {
             return this.currentCommandIndex >= 0;
         },
-
         canRedo: function () {
             return this.currentCommandIndex != this.stack.length - 1;
         }
--- tests/util/undoredostack.js
+++ tests/util/undoredostack.js
@@ -8,6 +8,13 @@ module("UndoRedoStack", {
     }
 });
 
+function command() {
+    return {
+        undo: spy(),
+        redo: spy()
+    };
+}
+
 test("stack is initially empty", function() {
     ok(!undoRedoStack.canUndo());
     ok(!undoRedoStack.canRedo());
@@ -21,71 +28,94 @@ test("canUndo returns true after command is pushed in stack", function() {
 });
 
 test("canRedo returns true after undo", function() {
-    undoRedoStack.push({ undo: function() {} });
+    undoRedoStack.push(command());
     undoRedoStack.undo();
 
     ok(undoRedoStack.canRedo());
 });
 
 test("canUndo returns false when at the bottom of the stack", function() {
-    undoRedoStack.push({ undo: function() {} });
+    undoRedoStack.push(command());
     undoRedoStack.undo();
 
     ok(!undoRedoStack.canUndo());
 });
 
 test("canRedo returns false when a new command is pushed", function() {
-    undoRedoStack.push({ undo: function() {} });
+    undoRedoStack.push(command());
     undoRedoStack.undo();
-    undoRedoStack.push({ undo: function() {} });
+    undoRedoStack.push(command());
 
     ok(!undoRedoStack.canRedo());
     ok(undoRedoStack.canUndo());
 });
 
 test("undo delegates undo to current command", function() {
-    var called = false;
+    var c = command();
 
-    undoRedoStack.push({ undo: function() { called = true; } });
+    undoRedoStack.push(c);
     undoRedoStack.undo();
 
-    ok(called);
+    equal(c.undo.calls, 1);
 });
 
 test("redo delegates to exec to current command", function() {
-    var called = false;
+    var c = command();
 
-    undoRedoStack.push({ undo: function() { }, redo: function() { called = true; } });
+    undoRedoStack.push(c);
     undoRedoStack.undo();
     undoRedoStack.redo();
 
-    ok(called);
+    equal(c.redo.calls, 1);
     ok(!undoRedoStack.canRedo());
     ok(undoRedoStack.canUndo());
 });
 
 test("redo does not delegate to exec when at top of stack", function() {
-    var called = false;
+    var c = command();
 
-    undoRedoStack.push({ undo: function() { }, redo: function() { called = true; } });
+    undoRedoStack.push(c);
     undoRedoStack.redo();
 
-    ok(!called);
+    ok(!c.redo.calls);
 });
 
 test("canUndo is true after undoing the second command", function() {
-    undoRedoStack.push({ undo: function() { } });
-    undoRedoStack.push({ undo: function() { } });
+    undoRedoStack.push(command());
+    undoRedoStack.push(command());
     undoRedoStack.undo();
 
     ok(undoRedoStack.canUndo());
 });
 
 test("clear clears stack", function() {
-    undoRedoStack.push({ undo: function() { } });
+    undoRedoStack.push(command());
     undoRedoStack.clear();
 
     ok(!undoRedoStack.canUndo());
+});
+
+test("undo triggers undo event", function() {
+    var handler = spy();
+    var c = command();
+    undoRedoStack.bind("undo", handler);
+    undoRedoStack.push(c);
+    undoRedoStack.undo();
+
+    equal(handler.calls, 1);
+    deepEqual(handler.args[0][0].command, c);
+});
+
+test("redo triggers redo event", function() {
+    var handler = spy();
+    var c = command();
+    undoRedoStack.bind("redo", handler);
+    undoRedoStack.push(c);
+    undoRedoStack.undo();
+    undoRedoStack.redo();
+
+    equal(handler.calls, 1);
+    deepEqual(handler.args[0][0].command, c);
 });
 
 }());
