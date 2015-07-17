fe1a3b32f1e4a69cba4e9c6836cc411e8eb5abcc
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2015-07-17 17:15:31 +0300

    Add Angular 1.4 test

---
+++ playground/angular-new-router/components/directory/directory.js
@@ -0,0 +1,41 @@
+angular.module('app.directory', ["ngNewRouter", "kendo.directives"])
+  .controller('DirectoryController',[ '$routeParams', DirectoryController]);
+
+function DirectoryController($routeParams) {
+    this.mainGridOptions = {
+	height: 800,
+        dataSource: {
+            type: "odata",
+            transport: {
+                read: "http://demos.telerik.com/kendo-ui/service/Northwind.svc/Employees"
+            },
+            pageSize: 5,
+            serverPaging: true,
+            serverSorting: true
+        },
+        autoBind: true,
+        sortable: true,
+        pageable: true,
+        columns: [{
+            field: "FirstName",
+            title: "First Name",
+            width: "120px"
+        },{
+            field: "LastName",
+            title: "Last Name",
+            width: "120px"
+        },{
+            field: "Country",
+            width: "120px"
+        },{
+            field: "City",
+            width: "120px"
+        },{
+            field: "Title"
+        }]
+    };
+
+    this.monthSelectorOptions = {
+      format: "MMM yyyy"
+    };
+}
