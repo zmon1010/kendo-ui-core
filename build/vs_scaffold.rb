2abc0ffab0413861c4e05f572c391bed04c57ca3
Raisolution <d.madjarov@gmail.com>
2015-08-26 13:34:57 +0300

    Update build to include Scaffolder version

--- build/vs_scaffold.rb
+++ build/vs_scaffold.rb
@@ -4,10 +4,26 @@ VS_SCAFFOLD_SRC = FileList[VS_SCAFFOLD_SRC_ROOT + '**/*.*']
 
 VS_SCAFFOLD_PROJECT = VS_SCAFFOLD_SRC_ROOT + 'KendoScaffolder.sln'
 VS_SCAFFOLD_OUTPUT = VS_SCAFFOLD_SRC_ROOT + 'KendoScaffolderExtension/bin/Release/KendoScaffolderExtension.vsix'
+VS_SCAFFOLD_MANIFEST = VS_SCAFFOLD_SRC_ROOT + 'KendoScaffolder/KendoScaffolderExtension/source.extension.vsixmanifest'
+VS_SCAFFOLD_MANIFEST_TEMPLATE = VS_SCAFFOLD_MANIFEST + '.template'
 
 CLEAN.include(VS_SCAFFOLD_OUTPUT)
 
 namespace :vs_scaffold do
+    desc 'Build the Scaffold manifest'
+    task VS_SCAFFOLD_MANIFEST do
+        ensure_path VS_SCAFFOLD_MANIFEST
+
+        File.open(VS_SCAFFOLD_MANIFEST_TEMPLATE, 'r:bom|utf-8') do |source|
+            contents = source.read
+
+            File.open(VS_SCAFFOLD_MANIFEST, "w") do |file|
+                contents.sub!("$SCAFFOLD_VERSION", VERSION)
+                file.write(contents)
+            end
+        end
+    end
+
     file VS_SCAFFOLD_OUTPUT => VS_SCAFFOLD_SRC do |t|
         options = '/p:Configuration=Release /p:VisualStudioVersion=12.0'
 
@@ -15,7 +31,7 @@ namespace :vs_scaffold do
     end
 
     desc 'Builds the VS Scaffolder extension'
-    task :build => VS_SCAFFOLD_OUTPUT
+    task :build => [VS_SCAFFOLD_MANIFEST, VS_SCAFFOLD_OUTPUT]
 
     file "plugins/KendoScaffolder/KendoScaffolderExtension.vsix" => "dist/binaries/scaffolding/KendoScaffolderExtension.vsix" do |t|
        cp t.prerequisites.first, t.name
