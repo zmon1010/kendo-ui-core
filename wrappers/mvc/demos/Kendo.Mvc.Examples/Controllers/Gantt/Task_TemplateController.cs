2424109504bc51896d97bf42c5882099ab752c60
Dimtar Terziev <dimitar.terziev@telerik.com>
2015-09-10 17:37:42 +0300

    Add Task Template demo for the MVC Gantt wrapper.

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Gantt/Task_TemplateController.cs
@@ -0,0 +1,19 @@
+﻿using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Kendo.Mvc.Extensions;
+using System.Web.Mvc;
+using Kendo.Mvc.Examples.Models.Gantt;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class GanttController : Controller
+    {
+        public ActionResult Task_Template()
+        {
+            return View();
+        }
+    }
+}
