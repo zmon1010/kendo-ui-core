7894b7b6b1e874ec8ca84d11fd3b47d4d8055bda
Nikolay Rusev <nikolay.rusev@telerik.com>
2015-09-15 15:55:45 +0300

    Sign Telerik.Web.Spreadsheet assembly

--- dpl/Telerik.Web.Spreadsheet/Properties/AssemblyInfo.cs
+++ dpl/Telerik.Web.Spreadsheet/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -34,3 +36,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: CLSCompliant(true)]
+[assembly: SecurityRules(SecurityRuleSet.Level1)]
+[assembly: AllowPartiallyTrustedCallers]
