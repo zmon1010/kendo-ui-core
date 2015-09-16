e1a0422c2175a34f17abd6cc476c1f62b5255946
Alex Gyoshev <alex@gyoshev.net>
2015-09-16 11:05:29 +0300

    Do not add data-command=undefined

--- src/spreadsheet/toolbar.js
+++ src/spreadsheet/toolbar.js
@@ -348,12 +348,14 @@
 
     var PopupTool = kendo.toolbar.Item.extend({
         init: function(options, toolbar) {
-            this.element = $("<a href='#' data-command='" + options.command + "' class='k-button k-button-icon'>" +
+            this.element = $("<a href='#' class='k-button k-button-icon'>" +
                                 "<span class='" + options.spriteCssClass + "'>" +
                                 "</span><span class='k-font-icon k-icon k-i-arrow-s'></span>" +
                             "</a>");
 
-            this.element.on("click", this.open.bind(this));
+            this.element
+                .on("click", this.open.bind(this))
+                .attr("data-command", options.command);
 
             this.options = options;
             this.toolbar = toolbar;
