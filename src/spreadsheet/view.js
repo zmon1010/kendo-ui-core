8710d79ee5da1d7c93ac7c389824b3af93d6c227
Georgi Krustev <ggkrustev@yahoo.com>
2015-09-17 17:56:29 +0300

    Remove setTimeout used to select clipboard content.
    It seems that it was added to ensure that the clipboard is correctly updated in IE after copying text with right click (context menu).
    As this is no needed anymore, we will remove it. This will fix the 'Discontiguous selection is not supported'

--- src/spreadsheet/view.js
+++ src/spreadsheet/view.js
@@ -35,13 +35,7 @@
         var range = document.createRange();
         range.selectNodeContents(el);
 
-        setTimeout(function () {
-            sel.removeAllRanges(); // XXX: should we actually do it above too?
-            // if we don't removeAllRanges here, we sometimes end up
-            // with "discontinuous selection not supported" exception.
-
-            sel.addRange(range);
-        });
+        sel.addRange(range);
     }
 
     function cellBefore(table, row) {
