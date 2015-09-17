b0503e435b676b8a0ac4d11e26e1f28bc3945a92
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2015-09-17 14:26:22 +0300

    Render 'all' as value for the pager
    See #4838

--- wrappers/mvc/src/Kendo.Mvc/UI/Grid/Html/GridPagerPageSizeSection.cs
+++ wrappers/mvc/src/Kendo.Mvc/UI/Grid/Html/GridPagerPageSizeSection.cs
@@ -15,12 +15,14 @@ namespace Kendo.Mvc.UI.Html
             foreach (var pageSize in section.PageSizes)
             {
                 var pageSizeText = pageSize.ToString().ToLowerInvariant();
+                var pageSizeValue = pageSizeText;
+
                 if (pageSizeText == "all")
                 {
                     pageSizeText = section.Messages.AllPages;
                 }
 
-                new HtmlElement("option").Text(pageSizeText).AppendTo(select);
+                new HtmlElement("option").Attribute("value", pageSizeValue).Text(pageSizeText).AppendTo(select);
             }
 
             new LiteralNode(section.Messages.ItemsPerPage).AppendTo(span);
