9a954777b2ea467c2f1972c2eb39024f49f4392c
Nikolay Rusev <nikolay.rusev@telerik.com>
2015-09-28 13:58:45 +0300

    Add Server import/export MVC example for Spradsheet

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Spreadsheet/Server_Side_Import_ExportController.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Web.Spreadsheet;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class SpreadsheetController : Controller
+    {
+        public ActionResult Server_Side_Import_Export()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            var workbook = Workbook.Load(file.InputStream, Path.GetExtension(file.FileName));
+            return Content(workbook.ToJson(), Telerik.Web.Spreadsheet.MimeTypes.JSON);
+        }
+
+        [HttpPost]
+        public ActionResult Download(string data, string extension)
+        {
+            var workbook = Workbook.FromJson(data);
+            using (var stream = new MemoryStream())
+            {
+                workbook.Save(stream, extension);
+
+                var mimeType = Telerik.Web.Spreadsheet.MimeTypes.ByExtension[extension];
+                return File(stream.ToArray(), mimeType, "Exported" + extension);
+            }
+        }
+    }
+}
