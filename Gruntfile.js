9ce9cf6434071b698e28743678a3936e6d8e170d
Will Meldon <wdmeldon@gmail.com>
2015-10-08 10:48:13 -0400

    Run gulp node for more predictable behavior.

--- Gruntfile.js
+++ Gruntfile.js
@@ -277,7 +277,7 @@ module.exports = function(grunt) {
                 stderr: false
             },
             gulpStyles: {
-                command: 'node_modules/gulp/bin/gulp.js styles'
+                command: 'node node_modules/gulp/bin/gulp.js styles'
             }
         },
 
