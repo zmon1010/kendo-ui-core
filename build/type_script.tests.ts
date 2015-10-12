b3d2eb2c638c34619ef1e64d8d3a1e35d6e86f35
Alex Gyoshev <alex@gyoshev.net>
2015-10-12 10:06:04 +0300

    Add typescript test for window content

--- build/type_script.tests.ts
+++ build/type_script.tests.ts
@@ -8,10 +8,20 @@ var is = {
     }
 }
 
+// TreeView
 $(() => {
     var treeview = <kendo.ui.TreeView>$("#treeview").data("kendoTreeView");
 
     is.string(treeview.text("#foo"));
 
     treeview.text("#foo", "bar");
+});
+
+// Window
+$(() => {
+    var window = <kendo.ui.Window>$("#window").data("kendoWindow");
+
+    var dom = $("<em>Foo</em>");
+
+    window.content(dom);
 });
