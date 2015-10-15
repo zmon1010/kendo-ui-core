8ed697c39e4201d1d0bb656453919e96b83d2c6e
Alex Gyoshev <alex@gyoshev.net>
2015-10-15 11:16:18 +0300

    Refactor, set proper exit code

--- build/check-docs.sh
+++ build/check-docs.sh
@@ -7,30 +7,31 @@ if [ $# -eq 0 ]; then
 fi
 
 # find broken links
-declare not_found=$(
-    wget --spider -e robots=off --wait 1 -r -p $1 2>&1 \
-        | grep -B 2 '404' \
-        | grep http \
-        | awk '{ print $3 }'
-)
+wget --spider -e robots=off --wait 1 -r -p $1 2>&1 \
+    | grep -B 2 '404' | grep http | awk '{ print $3 }' \
+    | {
+        # list pages that refer to broken links
+        has_broken_links=0
 
-declare statuscode=0
+        while read -r line; do
+            # remove hostname
+            line=$(echo "$line" | sed -r "s#[^/]*//[^/]*/kendo-ui##")
 
-# list pages that refer to broken links
-printf '%s\n' "$not_found" \
-    | while read -r line; do
-        # remove hostname
-        line=$(echo "$line" | sed -r "s#[^/]*//[^/]*/kendo-ui##")
+            if [ $line -n ]; then
+                continue
+            fi
 
-        statuscode=1
+            has_broken_links=1
 
-        echo -e "\e[91mBroken link: $line\e[39m"
-        echo "Found in:"
+            # report to console
+            echo -e "\e[91mBroken link: $line\e[39m"
+            echo "Found in:"
 
-        find docs -name "*.md" -print0 \
-            | xargs -0 grep -Hn --color=always $line
+            find docs -name "*.md" -print0 \
+                | xargs -0 grep -Hn --color=always $line
 
-        echo ""
-    done
+            echo ""
+        done
 
-exit $statuscode
+        exit $has_broken
+    }
