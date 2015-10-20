00f2f7df4e9766909c256492d388a12a51a9a16f
Alex Gyoshev <alex@gyoshev.net>
2015-10-20 17:45:20 +0300

    Use linkchecker for checking docs links + recipe
    Solves the need of searching manually for the originating links

--- build/check-docs.sh
+++ build/check-docs.sh
@@ -2,36 +2,9 @@
 
 if [ $# -eq 0 ]; then
     echo "  Usage: $0 <documentation-starting-page>"
-    echo "Example: $0 http://kendobuild.telerik.com/docs/introduction"
+    echo "Example: $0 http://kendobuild.telerik.com/kendo-ui/introduction"
     exit 1
 fi
 
 # find broken links
-wget --spider -e robots=off --wait 1 -r -p $1 2>&1 \
-    | grep -B 2 '404' | grep http | awk '{ print $3 }' \
-    | {
-        # list pages that refer to broken links
-        has_broken_links=0
-
-        while read -r line; do
-            # remove hostname
-            line=$(echo "$line" | sed -r "s#[^/]*//[^/]*/kendo-ui##")
-
-            if [ -z $line ]; then
-                continue
-            fi
-
-            has_broken_links=1
-
-            # report to console
-            echo -e "\e[91mBroken link: $line\e[39m"
-            echo "Found in:"
-
-            find docs -name "*.md" -print0 \
-                | xargs -0 grep -Hn --color=always $line
-
-            echo ""
-        done
-
-        exit $has_broken_links
-    }
+linkchecker --no-status --config=build/check-docs.ini -P1 $1
