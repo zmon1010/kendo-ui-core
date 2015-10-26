ca856e30cd65e8451714b4fa21077726f4c0230e
Petyo Ivanov <underlog@gmail.com>
2015-10-26 11:55:15 +0200

    Fix the named modules in the minified files

--- build/gulp/uglify.js
+++ build/gulp/uglify.js
@@ -4,6 +4,7 @@ var lazypipe = require('lazypipe');
 var uglify = require('gulp-uglify');
 var logger = require('gulp-logger');
 var rename = require('gulp-rename');
+var replace = require('gulp-replace');
 
 var compress = {
     unsafe       : true,
@@ -15,8 +16,13 @@ var compress = {
 var mangle = {
     except: [ "define" ]
 };
+function renameModules(match) {
+    return match.replace(/['"]([\w\.\/]+)?['"]/g, '"$1.min"');
+}
+
 
 module.exports = lazypipe()
     .pipe(logger, { after: 'uglify complete', extname: '.min.js', showChange: true })
     .pipe(uglify, { compress, mangle, preserveComments: "license" })
+    .pipe(replace, /define\(.+?\]/g, renameModules)
     .pipe(rename, { suffix: ".min" });
