10e0c68cd2038faef57145be03f4fea8e19a4b9b
Boyan Dimitrov <boyan85@gmail.com>
2015-11-20 17:20:41 +0200

    Update Color Picker API offline wrapper demo.

--- wrappers/php/colorpicker/api.php
+++ wrappers/php/colorpicker/api.php
@@ -5,40 +5,9 @@ require_once '../lib/Kendo/Autoload.php';
 
 ?>
 
-<div class="configuration-horizontal">
-    <div class="config-section">
-    <span class="configHead">Values</span>
-    <ul class="options">
-        <li>
-            <input id="value" value="#ff0000"  class="k-textbox" style="width: 100px; margin: 0;" />
-            <button id="set" class="k-button">Set value</button>
-        </li>
-        <li><button id="get" class="k-button">Get value</button></li>
-    </ul>
-    </div>
-    <div class="config-section">
-    <span class="configHead">Enable/Disable</span>
-    <ul class="options">
-        <li>
-            <button id="enable" class="k-button">Enable</button> 
-            <button id="disable" class="k-button">Disable</button>
-        </li>
-    </ul>
-    </div>
-    <div class="config-section">
-    <span class="configHead">Open/Close</span>
-    <ul class="options">
-        <li>
-            <button id="open" class="k-button">Open</button> 
-            <button id="close" class="k-button">Close</button>
-        </li>
-    </ul>
-    </div>
-</div>
-
-<div class="demo-section">
+<div class="demo-section k-content">
     <label for="colorpicker">ColorPicker:</label>
-<?php
+    <?php
     $colorPicker = new \Kendo\UI\ColorPicker('colorpicker');
 
     $colorPicker
@@ -47,6 +16,38 @@ require_once '../lib/Kendo/Autoload.php';
 
     echo $colorPicker->render();
 ?>
+
+</div>
+
+<div class="box">
+    <div class="box-col">
+    <h4>Values</h4>
+    <ul class="options">
+        <li>
+            <input id="value" value="#ff0000"  class="k-textbox" style="width: 100px; margin: 0;" />
+            <button id="set" class="k-button">Set value</button>
+        </li>
+        <li><button id="get" class="k-button">Get value</button></li>
+    </ul>
+    </div>
+    <div class="box-col">
+    <h4>Enable/Disable</h4>
+    <ul class="options">
+        <li>
+            <button id="enable" class="k-button">Enable</button> 
+            <button id="disable" class="k-button">Disable</button>
+        </li>
+    </ul>
+    </div>
+    <div class="box-col">
+    <h4>Open/Close</h4>
+    <ul class="options">
+        <li>
+            <button id="open" class="k-button">Open</button> 
+            <button id="close" class="k-button">Close</button>
+        </li>
+    </ul>
+    </div>
 </div>
 
 <script>
@@ -86,16 +87,8 @@ require_once '../lib/Kendo/Autoload.php';
 </script>
 
 <style>
-    .demo-section {
-        width: 660px;
-        padding: 30px;
-        text-align: center;
-    }
     .k-button {
         min-width: 80px;
-    }
-    .configuration-horizontal .options li {
-        padding: 3px 0;
     }
 </style>
 
