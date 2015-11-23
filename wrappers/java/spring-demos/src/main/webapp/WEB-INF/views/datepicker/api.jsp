e55b8ee53b7e1b8f36123ca916c1ecce6b80da6e
Boyan Dimitrov <boyan85@gmail.com>
2015-11-23 17:25:56 +0200

    Update DatePicker API offline wrapper demo.

--- wrappers/java/spring-demos/src/main/webapp/WEB-INF/views/datepicker/api.jsp
+++ wrappers/java/spring-demos/src/main/webapp/WEB-INF/views/datepicker/api.jsp
@@ -4,27 +4,42 @@
 
 <demo:header />
 
-<div class="configuration k-widget k-header" style="width: 220px">
-<span class="configHead">API Functions</span>
-<ul class="options">
-    <li>
-       <button id="get" class="k-button">Get value</button>
-   </li>
-   <li>
-       <input id="value" value="10/10/2000" style="float:none" />
-            <button id="set" class="k-button">Set value</button>
-        </li>
-         <li>
-             <button id="enable" class="k-button">Enable</button> or <button id="disable" class="k-button">Disable</button> or <button id="readonly" class="k-button">Readonly</button>
-         </li>
-         <li>
-             <button id="open" class="k-button">Open</button> or <button id="close" class="k-button">Close</button> the calendar
-         </li>
-     </ul>
+<div class="box wide">
+   <div class="box-col">
+         <h4>Set / Get Value</h4>
+         <ul class="options">
+             <li>
+                 <input id="value" value="10/10/2000" style="float:none" />
+ 			      <button id="set" class="k-button">Set value</button>
+       	  </li>
+       	<li style="text-align: right;">
+           <button id="get" class="k-button">Get value</button>
+       </li>
+   	</ul>
+	</div>
+    <div class="box-col">
+        <h4>Open / Close</h4>
+        <ul class="options">
+            <li>
+                <button id="open" class="k-button">Open</button>
+                <button id="close" class="k-button">Close</button>
+            </li>
+        </ul>
+    </div>
+    <div class="box-col">
+        <h4>Enable / Disable</h4>
+        <ul class="options">
+            <li>
+                <button id="enable" class="k-button">Enable</button>
+                <button id="disable" class="k-button">Disable</button>
+                <button id="readonly" class="k-button">Readonly</button>
+            </li>
+        </ul>
+    </div>
 </div>
 
-<div class="demo-section">
-    <kendo:datePicker name="datepicker"></kendo:datePicker>
+ <div class="demo-section k-content">
+    <kendo:datePicker name="datepicker" style="width: 100%;"></kendo:datePicker>
 </div>
 
 <script>
@@ -67,9 +82,12 @@ $(document).ready(function() {
 });
 </script>
 <style>
-	.demo-section{
-		width:155px;
-	}
-</style>
+   .box .k-textbox {
+          width: 80px;
+    }
+    .box .k-button {
+        min-width: 80px;
+    }
+ </style>
 
 <demo:footer />
--- wrappers/php/datepicker/api.php
+++ wrappers/php/datepicker/api.php
@@ -3,12 +3,48 @@
 require_once '../lib/Kendo/Autoload.php';
 require_once '../include/header.php';
 ?>
+
+<div class="box wide">
+   <div class="box-col">
+       <h4>Set / Get Value</h4>
+       <ul class="options">
+           <li>
+               <input id="value" value="10/10/2000" style="float:none" />
+     <button id="set" class="k-button">Set value</button>
+           </li>
+           <li style="text-align: right;">
+               <button id="get" class="k-button">Get value</button>
+           </li>
+       </ul>
+   </div>
+   <div class="box-col">
+       <h4>Open / Close</h4>
+       <ul class="options">
+           <li>
+               <button id="open" class="k-button">Open</button>
+               <button id="close" class="k-button">Close</button>
+           </li>
+       </ul>
+   </div>
+   <div class="box-col">
+       <h4>Enable / Disable</h4>
+       <ul class="options">
+           <li>
+               <button id="enable" class="k-button">Enable</button>
+               <button id="disable" class="k-button">Disable</button>
+               <button id="readonly" class="k-button">Readonly</button>
+           </li>
+       </ul>
+   </div>
+</div>
+
+<div class="demo-section k-content">
 <?php
 $datePicker = new \Kendo\UI\DatePicker('datepicker');
-
+$datePicker->attr('style', 'width: 100%');
 echo $datePicker->render();
 ?>
-
+</div>
 <script>
     $(document).ready(function() {
         var datepicker = $("#datepicker").data("kendoDatePicker");
@@ -48,22 +84,13 @@ echo $datePicker->render();
         });
     });
 </script>
-<div class="configuration k-widget k-header" style="width: 220px">
-    <span class="configHead">API Functions</span>
-    <ul class="options">
-        <li>
-           <button id="get" class="k-button">Get value</button>
-       </li>
-       <li>
-           <input id="value" value="10/10/2000" style="float:none" />
-           <button id="set" class="k-button">Set value</button>
-       </li>
-        <li>
-            <button id="enable" class="k-button">Enable</button> or <button id="disable" class="k-button">Disable</button> or <button id="readonly" class="k-button">Readonly</button>
-        </li>
-        <li>
-            <button id="open" class="k-button">Open</button> or <button id="close" class="k-button">Close</button> the calendar
-        </li>
-    </ul>
-</div>
+<style>
+  .box .k-textbox {
+      width: 80px;
+  }
+  .box .k-button {
+      min-width: 80px;
+  }
+</style>
+
 <?php require_once '../include/footer.php'; ?>
