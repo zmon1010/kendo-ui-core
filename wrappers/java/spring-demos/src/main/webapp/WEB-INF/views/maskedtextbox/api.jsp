0030f2e560d85f916e1177a4a7212d0d74565932
Boyan Dimitrov <boyan85@gmail.com>
2015-11-26 17:53:56 +0200

    Update MaskedTextBox API offline wrapper demo.

--- wrappers/java/spring-demos/src/main/webapp/WEB-INF/views/maskedtextbox/api.jsp
+++ wrappers/java/spring-demos/src/main/webapp/WEB-INF/views/maskedtextbox/api.jsp
@@ -4,24 +4,37 @@
 
 <demo:header />
 
-<div class="configuration" style="width: 220px">
-    <span class="configHead">API Functions</span>
-    <ul class="options">
-        <li>
-           <button id="get" class="k-button">Get value</button>
-       </li>
-       <li>
-           <button id="set" class="k-button">Set value</button>
-           <input class="k-textbox" id="value" value="555 123 4567" style="float:none" />
-       </li>
-        <li>
-            <button id="enable" class="k-button">Enable</button> or <button id="disable" class="k-button">Disable</button> or <button id="readonly" class="k-button">Readonly</button>
+ <div class="demo-section k-content">
+    <h4>Enter phone number</h4>
+    <kendo:maskedTextBox name="maskedtextbox" mask="(999) 000-0000" style="width: 100%;"></kendo:maskedTextBox>
+ </div>
+
+<div class="box wide">
+     <div class="box-col">
+     <h4>Set / Get Value</h4>
+     <ul class="options">
+         <li>
+            <button id="get" class="k-button">Get value</button>
         </li>
-    </ul>
-</div>           
-<div class="demo-section">
-    <kendo:maskedTextBox name="maskedtextbox" mask="(999) 000-0000"></kendo:maskedTextBox>
-</div>
+        <li>
+            <button id="set" class="k-button">Set value</button>
+            <input class="k-textbox" id="value" value="555 123 4567" style="float:none" />
+        </li>
+     </ul>
+     </div>
+     <div class="box-col">
+     <h4>Enable / Disable</h4>
+     <ul class="options">
+         <li>
+             <button id="enable" class="k-button">Enable</button>
+             <button id="disable" class="k-button">Disable</button>
+         </li>
+         <li>
+             <button id="readonly" class="k-button">Readonly</button>
+         </li>
+     </ul>
+     </div>
+ </div>          
 
 <script>
     $(document).ready(function() {
@@ -51,11 +64,6 @@
     });
 </script>           
  <style>
-    .demo-section{
-        width:150px;
-        margin-right: 400px;
-    }
-
     #value {
         width: 135px;
         margin-left: 3px;
--- wrappers/php/maskedtextbox/api.php
+++ wrappers/php/maskedtextbox/api.php
@@ -5,13 +5,42 @@ require_once '../lib/Kendo/Autoload.php';
 $maskedtextbox = new \Kendo\UI\MaskedTextBox('maskedtextbox');
 
 $maskedtextbox->mask("(999) 000-0000");
+$maskedtextbox->attr('style', 'width: 100%');
 
 ?>
-<div class="demo-section">
+<div class="demo-section k-content">
+    <h4>Enter phone number</h4>
 <?php
 echo $maskedtextbox->render();
 ?>
-</div>
+ </div>
+
+<div class="box wide">
+    <div class="box-col">
+    <h4>Set / Get Value</h4>
+    <ul class="options">
+        <li>
+           <button id="get" class="k-button">Get value</button>
+       </li>
+       <li>
+           <button id="set" class="k-button">Set value</button>
+           <input class="k-textbox" id="value" value="555 123 4567" style="float:none" />
+       </li>
+    </ul>
+    </div>
+    <div class="box-col">
+    <h4>Enable / Disable</h4>
+    <ul class="options">
+        <li>
+            <button id="enable" class="k-button">Enable</button>
+            <button id="disable" class="k-button">Disable</button>
+        </li>
+        <li>
+            <button id="readonly" class="k-button">Readonly</button>
+        </li>
+    </ul>
+    </div>
+</div>          
 
 <script>
     $(document).ready(function() {
@@ -40,30 +69,10 @@ echo $maskedtextbox->render();
         });
     });
 </script>
-<div class="configuration" style="width: 220px">
-    <span class="configHead">API Functions</span>
-    <ul class="options">
-        <li>
-           <button id="get" class="k-button">Get value</button>
-       </li>
-       <li>
-           <button id="set" class="k-button">Set value</button>
-           <input class="k-textbox" id="value" value="555 123 4567" style="float:none" />
-       </li>
-        <li>
-            <button id="enable" class="k-button">Enable</button> or <button id="disable" class="k-button">Disable</button> or <button id="readonly" class="k-button">Readonly</button>
-        </li>
-    </ul>
-</div>
 <style>
-    .demo-section{
-        width:150px;
-        margin-right: 400px;
-    }
-
-    #value {
-        width: 135px;
-        margin-left: 3px;
-    }
+   #value {
+       width: 135px;
+       margin-left: 3px;
+   }
 </style>
 <?php require_once '../include/footer.php'; ?>
