9b035b42fd3a3b835ddef6be2719200df368b81a
KirilNN <kiril.nikolov@gmail.com>
2015-11-27 18:04:53 +0200

    Add Java Events demo for Spreadsheet related to #5110

---
+++ wrappers/java/spring-demos/src/main/java/com/kendoui/spring/controllers/spreadsheet/EventsController.java
@@ -0,0 +1,15 @@
+package com.kendoui.spring.controllers.spreadsheet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller("spreadsheet-events-controller")
+@RequestMapping(value="/spreadsheet/")
+public class EventsController {
+    @RequestMapping(value = {"/events"}, method = RequestMethod.GET)
+    public String index() {
+        return "spreadsheet/events";
+    }
+}
+
