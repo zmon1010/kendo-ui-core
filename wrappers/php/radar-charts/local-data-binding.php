e8698ee9639ca311b421976096e8d150efc19e72
Boyan Dimitrov <boyan85@gmail.com>
2015-12-29 16:43:08 +0200

    Updata Radat Charts Local data binding offline wrapper demo.

--- wrappers/php/radar-charts/local-data-binding.php
+++ wrappers/php/radar-charts/local-data-binding.php
@@ -25,7 +25,10 @@ $chart->title(array('text' => 'Protein quality, Apple raw'))
       ->addValueAxisItem($valueAxis)
       ->tooltip(array('visible' => true))
       ->legend(array('visible' => false));
-
+?>
+<div class="demo-section k-content wide">
+<?php
 echo $chart->render();
 ?>
+</div>
 <?php require_once '../include/footer.php'; ?>
