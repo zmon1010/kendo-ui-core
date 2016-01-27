a8c192b8c86e263d26ef029fdbe31845f54ce3b5
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2016-01-27 15:56:51 +0200

    Make the recipe aware of current absolute path

--- ci/cookbooks/kendo_docker/recipes/default.rb
+++ ci/cookbooks/kendo_docker/recipes/default.rb
@@ -12,31 +12,33 @@ require 'version'
 #================ VARIABLES ==================
 new_line = "\n"
 tab = "\t"
-mvc_demos_path = "root/kendo/wrappers/mvc-6/demos/Kendo.Mvc.Examples/"
+kendoDirName = "/kendo/"
+root = File.absolute_path(File.dirname(__FILE__)).split(kendoDirName)[0] + kendoDirName
+mvc_demos_path = root + "wrappers/mvc-6/demos/Kendo.Mvc.Examples/"
 docker_image_name = "telerik/kendo_offline_demos"
 
 #================ INSTALL DOCKER ==================
-ruby_block 'fstab' do
-    block do
-        line_for_insert = new_line + "\#Docker interface with custom IP" + new_line +
-	"auto docker0" + new_line + 
-	"iface docker0 inet static" + new_line +
-        	tab + "address 10.1.0.1" + new_line +
-	        tab + "netmask 255.255.255.0" + new_line +
-        	tab + "bridge_ports dummy0" + new_line +
-	        tab + "bridge_stp off" + new_line +
-	        tab + "bridge_fd 0" + new_line
-        file = Chef::Util::FileEdit.new('/etc/network/interfaces')
-        file.insert_line_if_no_match(/docker0/, line_for_insert)
-        file.write_file
-    end
-end
+#ruby_block 'fstab' do
+#    block do
+#        line_for_insert = new_line + "\#Docker interface with custom IP" + new_line +
+#	"auto docker0" + new_line +
+#	"iface docker0 inet static" + new_line +
+#        	tab + "address 10.1.0.1" + new_line +
+#	        tab + "netmask 255.255.255.0" + new_line +
+#        	tab + "bridge_ports dummy0" + new_line +
+#	        tab + "bridge_stp off" + new_line +
+#	        tab + "bridge_fd 0" + new_line
+#        file = Chef::Util::FileEdit.new('/etc/network/interfaces')
+#        file.insert_line_if_no_match(/docker0/, line_for_insert)
+#        file.write_file
+#    end
+#end
 
-docker_installation_script 'default' do
-  repo 'main'
-  script_url 'https://get.docker.com/'
-  action :create
-end
+#docker_installation_script 'default' do
+#  repo 'main'
+#  script_url 'https://get.docker.com/'
+#  action :create
+#end
 
 #================ RUN DOCKER IMAGE SECTION ==================
 #test docker image:
@@ -56,13 +58,13 @@ end
 #end
 
 #================ CREATE DOCKER FILE ==================
-directory mvc_demos_path do
-  owner 'root'
-  group 'root'
-  mode '0755'
-  action :create
-  recursive true
-end
+#directory mvc_demos_path do
+#  owner 'root'
+#  group 'root'
+#  mode '0755'
+#  action :create
+#  recursive true
+#end
 
 docker_contents = "FROM microsoft/aspnet:1.0.0-rc1-final-coreclr" + new_line +
 	new_line +
@@ -76,31 +78,31 @@ docker_contents = "FROM microsoft/aspnet:1.0.0-rc1-final-coreclr" + new_line +
 	"EXPOSE 5000" + new_line +
 	'ENTRYPOINT ["dnx", "-p", "project.json", "kestrel"]'
 
-#======FOR TEST PURPOSS ONLY
+#======FOR TEST PURPOSS ONLY - IMAGE WITH ENTRYPOINT ONLY
 	docker_contents = "FROM microsoft/aspnet:1.0.0-rc1-final-coreclr" + new_line + "ENTRYPOINT echo"
 #======
 
-file mvc_demos_path + 'Dockerfile' do
-  content docker_contents
-  owner 'root'
-  group 'root'
-  mode '0755'
-end
+#file mvc_demos_path + 'Dockerfile' do
+#  content docker_contents
+#  owner 'root'
+#  group 'root'
+#  mode '0755'
+#end
 
 #================ TODO: GET VERSION ==================
 #apply to projec.json
 #apply to image tag
 #VERSION IS HARDCODED FOR TEST PURPOSES:
-version = "2016.1.112"
+#version = "2016.1.112"
 
 #================ BUILD DOCKER IMAGE ==================
 
-docker_image docker_image_name do
-        tag version
-	force true
-	source mvc_demos_path + 'Dockerfile'
-        action :build
-end
+#docker_image docker_image_name do
+#       tag version
+#	force true
+#	source mvc_demos_path + 'Dockerfile'
+#       action :build
+#end
 
 #================ LOGIN TO DOCKER HUB ==================
 #=========DOES NOT WORK
@@ -112,10 +114,10 @@ end
 #end
 
 #======== WORKAROUND:
-execute 'docker-login' do
-  command 'docker login --email="kendouiteam@telerik.com" --username="telerik" --password="kendorullz"'
-  action :run
-end
+#execute 'docker-login' do
+#  command 'docker login --email="kendouiteam@telerik.com" --username="telerik" --password="kendorullz"'
+#  action :run
+#end
 
 #================ PUSH DOCKER IMAGE ==================
 #========DOES NOT WORK
@@ -132,14 +134,14 @@ end
 #end
 
 #=========DEBUG
-version = 
+#version =
 #=========
 
 #================ LOGOUT OF DOCKER HUB ==================
 #======== WORKAROUND:
-execute 'docker-logout' do
-  command 'docker logout'
-  action :run
-end
+#execute 'docker-logout' do
+#  command 'docker logout'
+#  action :run
+#end
 
 
