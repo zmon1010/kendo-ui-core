3bc3b9c46e5029d455eb2c194171ba6729516549
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2016-02-05 15:32:39 +0200

    Update ApiDiff tool to new dnx name

--- build/apidiff.bat
+++ build/apidiff.bat
@@ -9,7 +9,7 @@ msbuild /verbosity:quiet /p:Configuration=Debug mvc-6\tools\APIChange\ApiChange.
 if not exist "mvc-6\tools\APIChange\bin\Debug\MVC5" md mvc-6\tools\APIChange\bin\Debug\MVC5
 if not exist "mvc-6\tools\APIChange\bin\Debug\MVC6" md mvc-6\tools\APIChange\bin\Debug\MVC6
 copy /y mvc\src\Kendo.Mvc\bin\Debug\Kendo.Mvc.dll mvc-6\tools\APIChange\bin\Debug\MVC5
-copy /y mvc-6\src\artifacts\bin\Kendo.Mvc\Debug\aspnet50\Kendo.Mvc.dll mvc-6\tools\APIChange\bin\Debug\MVC6
+copy /y mvc-6\src\artifacts\bin\Kendo.Mvc\Debug\dnx451\Kendo.Mvc.dll mvc-6\tools\APIChange\bin\Debug\MVC6
 
 pushd mvc-6\tools\APIChange\bin\Debug
 ApiChange -diff -old MVC5\Kendo.Mvc.dll -new MVC6\Kendo.Mvc.dll > ..\..\..\..\apidiff.txt
