8a7c21899c0e5c177b1fda94afc4e0065411afc8
Stamo-Gochev <stamo.gochev@telerik.com>
2016-02-09 15:52:11 +0200

    Add missing TreeMapTestHelper class

---
+++ wrappers/mvc-6/tests/Kendo.Mvc.Tests/UI/TreeMap/TreeMapTestHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace Kendo.Mvc.UI.Tests
+{
+    using Moq;
+    using Mvc.Tests;
+    public class TreeMapTestHelper
+    {
+        public static TreeMap CreateTreeMap()
+        {
+            return new TreeMap(TestHelper.CreateViewContext())
+            {
+                Name = "#TreeMap"
+            };
+        }
+    }
+}
