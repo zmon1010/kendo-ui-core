8b9b6fcc3d8355d2cbcfbf9ea010c4c688eecf5c
Stamo-Gochev <stamo.gochev@telerik.com>
2016-02-12 16:01:41 +0200

    Add generation of chart series Fluent API in MVC6

--- wrappers/mvc-6/src/Kendo.Mvc/UI/Chart/Fluent/ChartSeriesFactory.Generated.cs
+++ wrappers/mvc-6/src/Kendo.Mvc/UI/Chart/Fluent/ChartSeriesFactory.Generated.cs
@@ -1,23 +1,86 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Kendo.Mvc.Extensions;
 
 namespace Kendo.Mvc.UI.Fluent
 {
     /// <summary>
-    /// Defines the fluent API for adding items to Kendo UI Chart
+    /// Defines the fluent API for configuring List<ChartSeries<T>>
     /// </summary>
-    public partial class ChartSeriesFactory<T>
-        where T : class 
+    public partial class ChartSeriesFactory<T> where T : class
     {
+        /// <summary>
+        /// Defines area series bound to inline data.
+        /// </summary>
+        /// <param name="data">
+        /// The list of data items to bind to
+        /// </param>
+        public virtual ChartSeriesBuilder<T> Area(IEnumerable data)
+        {
+            var item = new ChartSeries<T>()
+            {
+                Type = "area",
+                Data = data
+            };
 
-        public Chart<T> Chart { get; set; }
+            item.Chart = Chart;
+            Container.Add(item);
+
+            return new ChartSeriesBuilder<T>(item);
+        }
 
         /// <summary>
-        /// Adds an item to the collection
+        /// Defines area series bound to model member(s).
         /// </summary>
-        public virtual ChartSeriesBuilder<T> Add()
+        /// <param name="expression">
+        /// The expression used to extract the value from the model.
+        /// </param>
+        public virtual ChartSeriesBuilder<T>  Area<TValue>(
+            Expression<Func<T, TValue>> expression)
         {
-            var item = new ChartSeries<T>();
-            item.Chart = Chart;
+            return Area(expression.MemberWithoutInstance());
+        }
+
+        /// <summary>
+        /// Defines area series bound to model member(s).
+        /// </summary>
+        /// <param name="expression">
+        /// The expression used to extract the value from the model.
+        /// </param>
+        /// <param name="categoryExpression">
+        /// The expression used to extract the category from the model.
+        /// </param>
+        public virtual ChartSeriesBuilder<T>  Area<TValue, TCategory>(
+            Expression<Func<T, TValue>> expression,
+            Expression<Func<T, TCategory>> categoryExpression)
+        {
+            return Area(expression.MemberWithoutInstance(), categoryExpression.MemberWithoutInstance());
+        }
+
+        /// <summary>
+        /// Defines bound area series.
+        /// </summary>
+        /// <param name="memberName">
+        /// The name of the value member.
+        /// </param>
+        /// <param name="categoryMemberName">
+        /// The name of the category member. Optional.
+        /// </param>
+        public virtual ChartSeriesBuilder<T> Area(
+            string memberName,
+            string categoryMemberName = null)
+        {
+            var item = new ChartSeries<T>()
+            {
+                Chart = Chart,
+                Type = "area",
+                Name = memberName.AsTitle(),
+                Field = memberName,
+                CategoryField = categoryMemberName
+            };
+
             Container.Add(item);
 
             return new ChartSeriesBuilder<T>(item);
--- wrappers/mvc-6/src/Kendo.Mvc/UI/Chart/Fluent/ChartSeriesFactory.cs
+++ wrappers/mvc-6/src/Kendo.Mvc/UI/Chart/Fluent/ChartSeriesFactory.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq.Expressions;
-using Kendo.Mvc.Extensions;
 
 namespace Kendo.Mvc.UI.Fluent
 {
@@ -17,86 +14,14 @@ namespace Kendo.Mvc.UI.Fluent
             Container = container;
         }
 
+        public Chart<T> Chart { get; set; }
+
         protected List<ChartSeries<T>> Container
         {
             get;
             private set;
         }
 
-        /// <summary>
-        /// Defines area series bound to inline data.
-        /// </summary>
-        /// <param name="data">
-        /// The data to bind to
-        /// </param>
-        public virtual ChartSeriesBuilder<T> Area(IEnumerable data)
-        {
-            var item = new ChartSeries<T>()
-            {
-                Type = "area",
-                Data = data
-            };
-
-            item.Chart = Chart;
-            Container.Add(item);
-
-            return new ChartSeriesBuilder<T>(item);
-        }
-
-        /// <summary>
-        /// Defines bound area series.
-        /// </summary>
-        /// <param name="expression">
-        /// The expression used to extract the value from the chart model.
-        /// </param>
-        /// <param name="noteTextExpression">
-        /// The expression used to extract the note text from the chart model.
-        /// </param>
-        public virtual ChartSeriesBuilder<T> Area<TValue>(
-            Expression<Func<T, TValue>> expression)
-        {
-            return Area(expression.MemberWithoutInstance());
-        }
-
-        /// <summary>
-        /// Defines bound area series.
-        /// </summary>
-        /// <param name="expression">
-        /// The expression used to extract the value from the chart model.
-        /// </param>
-        /// <param name="noteTextExpression">
-        /// The expression used to extract the note text from the chart model.
-        /// </param>
-        public virtual ChartSeriesBuilder<T> Area<TValue, TCategory>(
-            Expression<Func<T, TValue>> expression,
-            Expression<Func<T, TCategory>> categoryExpression)
-        {
-            return Area(expression.MemberWithoutInstance(), categoryExpression?.MemberWithoutInstance());
-        }
-
-        /// <summary>
-        /// Defines bound area series.
-        /// </summary>
-        /// <param name="expression">
-        /// The expression used to extract the value from the chart model.
-        /// </param>
-        /// <param name="noteTextExpression">
-        /// The expression used to extract the note text from the chart model.
-        /// </param>
-        public virtual ChartSeriesBuilder<T> Area(string memberName, string categoryMemberName = null)
-        {
-            var item = new ChartSeries<T>()
-            {
-                Type = "area",
-                Name = memberName.AsTitle(),
-                Field = memberName,
-                CategoryField = categoryMemberName,
-                Chart = Chart
-            };
-            
-            Container.Add(item);
-
-            return new ChartSeriesBuilder<T>(item);
-        }
+        // Place custom settings here
     }
 }
