06335f48da5ac6b1172256f0d28f437f8c72c62f
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2016-02-15 18:32:38 +0200

    Add DropDownList demos

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/DropDownList/Cascading_DropDownListController.cs
@@ -0,0 +1,47 @@
+﻿using Kendo.Mvc.Examples.Models;
+using Microsoft.AspNet.Mvc;
+using System.Linq;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class DropDownListController : Controller
+    {
+        public ActionResult Cascading_DropDownList()
+        {
+            return View();
+        }
+
+        public JsonResult GetCascadeCategories()
+        {
+            var northwind = new SampleEntitiesDataContext();
+
+            return Json(northwind.Categories.Select(c => new { CategoryId = c.CategoryID, CategoryName = c.CategoryName }));
+        }
+
+        public JsonResult GetCascadeProducts(int? categories)
+        {
+            var northwind = new SampleEntitiesDataContext();
+            var products = northwind.Products.AsQueryable();
+
+            if (categories != null)
+            {
+                products = products.Where(p => p.CategoryID == categories);
+            }
+
+            return Json(products.Select(p => new { ProductID = p.ProductID, ProductName = p.ProductName }));
+        }
+
+        public JsonResult GetCascadeOrders(int? products)
+        {
+            var northwind = new SampleEntitiesDataContext();
+            var orders = northwind.OrderDetails.AsQueryable();
+
+            if (products != null)
+            {
+                orders = orders.Where(o => o.ProductID == products);
+            }
+
+            return Json(orders.Select(o => new { OrderID = o.OrderID, ShipCity = o.Order.ShipCity }));
+        }
+    }
+}
