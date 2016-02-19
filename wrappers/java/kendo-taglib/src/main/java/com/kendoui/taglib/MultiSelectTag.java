bedc5b5ffb3dbf44bcc30c8e88662a6b0853840f
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2016-02-19 17:29:35 +0200

    Generate java and php wrappers for ignoreCase with boolean type

--- wrappers/java/kendo-taglib/src/main/java/com/kendoui/taglib/MultiSelectTag.java
+++ wrappers/java/kendo-taglib/src/main/java/com/kendoui/taglib/MultiSelectTag.java
@@ -189,11 +189,11 @@ public class MultiSelectTag extends WidgetTag /* interfaces */implements DataBou
         setProperty("highlightFirst", value);
     }
 
-    public java.lang.String getIgnoreCase() {
-        return (java.lang.String)getProperty("ignoreCase");
+    public boolean getIgnoreCase() {
+        return (Boolean)getProperty("ignoreCase");
     }
 
-    public void setIgnoreCase(java.lang.String value) {
+    public void setIgnoreCase(boolean value) {
         setProperty("ignoreCase", value);
     }
 
