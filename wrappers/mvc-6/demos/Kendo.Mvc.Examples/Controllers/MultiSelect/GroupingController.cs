58d221c0657c0e8dda3e1f8f694f16cd2f83a17a
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2016-02-22 17:29:14 +0200

    Add MultiSelect MVC 6 demos

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/MultiSelect/GroupingController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.Examples.Models;
+using System.Linq;
+using Kendo.Mvc.UI;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class MultiSelectController : Controller
+    {
+        public ActionResult Grouping()
+        {
+            return View();
+        }
+
+        public ActionResult Customers_Read()
+        {
+            return Json(GetCustomers());
+        }
+
+        private static IEnumerable<CustomerViewModel> GetCustomers()
+        {
+            var northwind = new SampleEntitiesDataContext();
+            return northwind.Customers.Select(customer => new CustomerViewModel
+            {
+                CustomerID = customer.CustomerID,
+                CompanyName = customer.CompanyName,
+                ContactName = customer.ContactName,
+                ContactTitle = customer.ContactTitle,
+                Address = customer.Address,
+                City = customer.City,
+                Region = customer.Region,
+                PostalCode = customer.PostalCode,
+                Country = customer.Country,
+                Phone = customer.Phone,
+                Fax = customer.Fax,
+                Bool = customer.Bool
+            });
+        }
+    }
+}
