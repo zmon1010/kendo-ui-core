389e3da349dd6c60860b07edc7b8a8b6a63edac6
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2016-02-25 14:38:27 +0200

    Add enum generation from model yml files

--- build/codegen/lib/mvc-6/model-generator.rb
+++ build/codegen/lib/mvc-6/model-generator.rb
@@ -4,9 +4,9 @@ class ModelMember
     attr_reader :type, :name, :enum_type
 
     def initialize(config)
-        @type = [config[:type]]
+        @type = config[:type]
         @name = config[:name]
-        @enum_type = false
+        @enum_type = config[:enum_type]
     end
 end
 
@@ -27,14 +27,28 @@ module CodeGen::MVC6::Wrappers::ModelGenerator
     INTERFACE_TEMPLATE = ERB.new(File.read("build/codegen/lib/mvc-6/templates/model-interface.erb"), 0, '%<>')
     DESCRIPTOR_TEMPLATE = ERB.new(File.read("build/codegen/lib/mvc-6/templates/model-descriptor.erb"), 0, '%<>')
     DESCRIPTOR_FACTORY_TEMPLATE = ERB.new(File.read("build/codegen/lib/mvc-6/templates/model-descriptor-factory.erb"), 0, '%<>')
+    ENUM_TEMPLATE = ERB.new(File.read("build/codegen/lib/mvc-6/templates/option-enum.erb"), 0, '%<>')
 
     def write_models(component)
         @componentPath = component.path
         models = YAML.load(File.read("build/codegen/lib/mvc-6/models/#{component.name.downcase}.yml"))
 
+        write_modelEnums(models)
         write_interfaces(models)
         write_modelDescriptors(models)
         write_modelDescriptorFactories(models)
+    end
+
+    def write_modelEnums(models)
+        models.each do |model|
+            model[:members].each do |member|
+                if !member[:values].nil?
+                    ctx = EnumContext.new(member)
+                    filename = "#{@path}/#{@componentPath}/#{member[:enum_type]}.Generated.cs"     
+                    write_file(filename, ENUM_TEMPLATE.result(ctx.get_binding))
+                end
+            end
+        end
     end
 
     def write_interfaces(models)
