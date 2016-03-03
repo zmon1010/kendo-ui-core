64ce823c8fd32ef5565e0968ed7c7f3de8bdd769
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2016-03-03 15:29:35 +0200

    Set up documentation redirects and clean up staging

--- ci/cookbooks/nginx/recipes/default.rb
+++ ci/cookbooks/nginx/recipes/default.rb
@@ -1,9 +1,22 @@
 package "nginx"
 
-cookbook_file "/etc/nginx/nginx.conf" do
-    source "nginx.conf"
+cookbook_file '/etc/nginx/nginx.conf' do
+    source 'nginx.conf'
 end
 
-service "nginx" do
+file '/var/www/kendo-ui/redirects.conf' do
+end
+
+service 'nginx' do
+    action :restart
+end
+
+package 'incron'
+
+cookbook_file '/etc/incron.d/in-modify-nginx-redirects' do
+    source 'in-modify-nginx-redirects'
+end
+
+service 'incron' do
     action :restart
 end
