6647102e0a7b395369b2ac7ba863a801fa52460e
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2016-03-09 15:05:51 +0200

    Fix paths
    Third time's the charm

--- build/upload-docs-to-kendo-www.sh
+++ build/upload-docs-to-kendo-www.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#
-# Sample Usage
-# build/upload-docs.sh docs/_site
 
 SYNC='rsync -rvcz --delete --inplace'
 DEST='/usr/share/nginx/html/docs'
@@ -21,7 +18,7 @@ rm -rf docs/api/wrappers/*
 for host in "${HOSTS[@]}"
 do
     log "Uploading documentation to $host"
-    $SYNC _site/ $USER@$host:$DEST
+    $SYNC docs/_site/ $USER@$host:$DEST
 done
 
 log "Done"
