da207f9f6a56e84502ecbcb8aea6fa45e66a4a81
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2016-03-09 14:34:45 +0200

    Add new upload docs script

---
+++ build/upload-docs-to-kendo-www.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Sample Usage
+# build/upload-docs.sh docs/_site
+
+SYNC='rsync -rvcz --delete --inplace'
+DEST='/usr/share/nginx/html/docs'
+USER='nginx'
+declare -a HOSTS=("ordkendowww01.telerik.local" "ordkendowww02.telerik.local")
+
+function log {
+    echo "[$(date +%T)] $1"
+}
+
+log "Generating documentation"
+(cd docs && bundle --without development --path ~/gems && bundle exec jekyll build)
+
+# Clean-up generated wrappers
+rm -rf docs/api/wrappers/*
+
+for host in "${HOSTS[@]}"
+do
+    log "Uploading documentation to $host"
+    $SYNC $USER@$host:$DEST
+done
+
+log "Done"
