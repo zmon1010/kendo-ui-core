86ee562a52e0a82066ef56fb92e1e7dcfb19c365
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2016-03-15 17:11:56 +0200

    Update cleanup code

--- build/upload-docs-to-kendo-www.sh
+++ build/upload-docs-to-kendo-www.sh
@@ -13,7 +13,9 @@ log "Generating documentation"
 (cd docs && bundle --without development --path ~/gems && bundle exec jekyll build)
 
 # Clean-up generated wrappers
-rm -rf docs/api/wrappers/*
+rm -rf docs/api/aspnet-mvc
+rm -rf docs/api/jsp
+rm -rf docs/api/php
 
 for host in "${HOSTS[@]}"
 do
