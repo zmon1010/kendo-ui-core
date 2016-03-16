1053c494aea53760d35baefa793e1031307bc86d
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2016-03-16 17:23:48 +0200

    Add MVC 6 Tag Helpers generation

--- build/codegen/lib/mvc-6/main.rb
+++ build/codegen/lib/mvc-6/main.rb
@@ -3,6 +3,7 @@ module CodeGen::MVC6
 end
 
 module CodeGen::MVC6::Wrappers
+    IGNORED = YAML.load(File.read("build/codegen/lib/mvc-6/config/ignored.yml")).map(&:downcase)
 end
 
 require 'codegen/lib/mvc-6/component'
@@ -10,3 +11,4 @@ require 'codegen/lib/mvc-6/chart-generator'
 require 'codegen/lib/mvc-6/enum-generator'
 require 'codegen/lib/mvc-6/model-generator'
 require 'codegen/lib/mvc-6/generator'
+require 'codegen/lib/mvc-6/taghelper-generator'
