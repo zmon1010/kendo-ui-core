4e3f1766eecace8c8e84fc92f0b3d069357669ca
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2016-03-22 16:23:00 +0200

    Fix NumericTextBox Decimals generation

--- wrappers/mvc-6/src/Kendo.Mvc/UI/NumericTextBox/Fluent/NumericTextBoxBuilder.Generated.cs
+++ wrappers/mvc-6/src/Kendo.Mvc/UI/NumericTextBox/Fluent/NumericTextBoxBuilder.Generated.cs
@@ -20,6 +20,16 @@ namespace Kendo.Mvc.UI.Fluent
         }
 
         /// <summary>
+        /// Specifies the number precision applied to the widget value and when the NumericTextBox is focused. If not set, the precision defined by the current culture is used.Compare with the format property.
+        /// </summary>
+        /// <param name="value">The value for Decimals</param>
+        public NumericTextBoxBuilder<T> Decimals(int value)
+        {
+            Container.Decimals = value;
+            return this;
+        }
+
+        /// <summary>
         /// Specifies the text of the tooltip on the down arrow.
         /// </summary>
         /// <param name="value">The value for DownArrowText</param>
