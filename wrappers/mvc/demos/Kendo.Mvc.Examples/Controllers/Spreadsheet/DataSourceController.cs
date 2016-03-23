73d0cccd10e58349dc138f0507236a16917be7b5
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2016-03-23 15:36:03 +0200

    Add DataSource example to MVC Spreadsheet Demos

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Spreadsheet/DataSourceController.cs
@@ -0,0 +1,82 @@
+﻿using Kendo.Mvc.Examples.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class SpreadsheetController : Controller
+    {
+        private SpreadsheetProductService productService;
+
+        public SpreadsheetController()
+        {
+            productService = new SpreadsheetProductService(new SampleEntities());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            productService.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        public ActionResult DataSource()
+        {                            
+            return View();
+        }
+
+        public ActionResult Products_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(productService.Read().ToDataSourceResult(request));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Products_Update([DataSourceRequest] DataSourceRequest request,
+            [Bind(Prefix = "models")]IEnumerable<SpreadsheetProductViewModel> products)
+        {
+            if (products != null && ModelState.IsValid)
+            {
+                foreach (var product in products)
+                {
+                    productService.Update(product);
+                }
+            }
+
+            return Json(products.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Products_Create([DataSourceRequest] DataSourceRequest request,
+            [Bind(Prefix = "models")]IEnumerable<SpreadsheetProductViewModel> products)
+        {
+            var results = new List<SpreadsheetProductViewModel>();
+
+            if (products != null && ModelState.IsValid)
+            {
+                foreach (var product in products)
+                {
+                    productService.Create(product);
+
+                    results.Add(product);
+                }
+            }
+
+            return Json(results.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Products_Destroy([DataSourceRequest] DataSourceRequest request,
+            [Bind(Prefix = "models")]IEnumerable<SpreadsheetProductViewModel> products)
+        {
+            foreach (var product in products)
+            {
+                productService.Destroy(product);
+            }
+
+            return Json(products.ToDataSourceResult(request, ModelState));
+        }
+    }
+}
