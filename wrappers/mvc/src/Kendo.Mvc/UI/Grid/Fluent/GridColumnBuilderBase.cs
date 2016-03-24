210e07e4177a4cc0a790a2214c7531fc9bfd0f5f
Dimo Dimov <dimo.dimov@telerik.com>
2016-03-24 11:15:12 +0200

    Improve Grid column title info in MVC API 2

--- wrappers/mvc/src/Kendo.Mvc/UI/Grid/Fluent/GridColumnBuilderBase.cs
+++ wrappers/mvc/src/Kendo.Mvc/UI/Grid/Fluent/GridColumnBuilderBase.cs
@@ -35,7 +35,7 @@ namespace Kendo.Mvc.UI.Fluent
         }
 
         /// <summary>
-        /// Sets the title displayed in the header of the column. Any HTML entities or tags should be encoded.
+        /// Sets the title displayed in the header of the column. Any HTML entities or tags should be encoded, or use a HeaderTemplate instead.
         /// </summary>
         /// <param name="text">The text.</param>
         /// <example>
