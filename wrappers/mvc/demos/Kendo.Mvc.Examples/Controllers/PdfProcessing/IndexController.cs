237f6a84c680d9736fa476e466d41712f4e92934
Ianko Djemerenov <iankodj@gmail.com>
2016-03-25 14:20:58 +0200

    Added razor demo for PdfProcessing

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/PdfProcessing/IndexController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Telerik.Windows.Documents.Fixed.FormatProviders.Pdf;
+using Telerik.Windows.Documents.Fixed.FormatProviders.Pdf.Export;
+using Telerik.Windows.Documents.Fixed.Model;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public class PdfProcessingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Download_Document()
+        {
+            PdfFormatProvider formatProvider = new PdfFormatProvider();
+            formatProvider.ExportSettings.ImageQuality = ImageQuality.High;
+
+            byte[] renderedBytes = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                RadFixedDocument document = CreatePdfDocument.CreateDocument();
+                formatProvider.Export(document, ms);
+                renderedBytes = ms.ToArray();
+            }
+
+            return File(renderedBytes, "application/pdf", "PdfDocument.pdf");
+        }
+    }
+}
