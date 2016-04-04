0b3060c98ec173fb87b2fc330cbeb01d2ec4eef7
Stamo-Gochev <stamo.gochev@telerik.com>
2016-04-04 13:42:19 +0300

    Fix a PolarLine series demo

--- wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Polar_Charts/Smooth_Polar_LineController.cs
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Polar_Charts/Smooth_Polar_LineController.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Linq;
-using System.Web.Mvc;
-using Kendo.Mvc.Examples.Models;
+﻿using Kendo.Mvc.Examples.Models;
+using Microsoft.AspNet.Mvc;
 
 namespace Kendo.Mvc.Examples.Controllers
 {
     public partial class Polar_ChartsController : Controller
     {
-        public ActionResult Smooth_Polar_Line()
+        public IActionResult Smooth_Polar_Line()
         {
             return View(ChartDataRepository.SunPosition());
         }
