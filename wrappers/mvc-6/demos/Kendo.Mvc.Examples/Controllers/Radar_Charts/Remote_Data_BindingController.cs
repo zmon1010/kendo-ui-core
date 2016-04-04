5373cf050d12553e21bb6359fd73b8e3ae60ec64
Stamo-Gochev <stamo.gochev@telerik.com>
2016-04-04 14:48:05 +0300

    Add Chart demos for Radar series in MVC Core

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Radar_Charts/Remote_Data_BindingController.cs
@@ -0,0 +1,19 @@
+﻿using Kendo.Mvc.Examples.Models;
+using Microsoft.AspNet.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class Radar_ChartsController : Controller
+    {
+        public IActionResult Remote_Data_Binding()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult _BudgetReport()
+        {
+            return Json(ChartDataRepository.BudgetReport());
+        }
+    }
+}
---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Radar_Charts/Smooth_Radar_LineController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class Radar_ChartsController : Controller
+    {
+        public IActionResult Smooth_Radar_Line()
+        {
+            return View();
+        }
+    }
+}
