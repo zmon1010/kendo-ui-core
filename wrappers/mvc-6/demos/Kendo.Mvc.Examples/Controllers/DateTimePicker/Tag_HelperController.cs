57f19cba95c2c44b95460c7746602a82c57275e0
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2016-04-04 18:31:49 +0300

    Add Mvc Core Tag Helpers demos

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/DateTimePicker/Tag_HelperController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class DateTimePickerController : Controller
+    {
+        public IActionResult Tag_Helper()
+        {
+            return View();
+        }
+    }
+}
