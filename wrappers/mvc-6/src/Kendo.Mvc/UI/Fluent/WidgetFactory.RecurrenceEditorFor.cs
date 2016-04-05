ae1a6d013ce15cc2fbde25eac6646ee99b88d890
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2016-04-05 11:52:24 +0300

    Add RecurrenceEditorFor Mvc Core wrapper

---
+++ wrappers/mvc-6/src/Kendo.Mvc/UI/Fluent/WidgetFactory.RecurrenceEditorFor.cs
@@ -0,0 +1,30 @@
+﻿using Kendo.Mvc.Extensions;
+using System;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace Kendo.Mvc.UI.Fluent
+{
+    public partial class WidgetFactory<TModel>
+    {
+        /// <summary>
+        /// Creates a new <see cref="RecurrenceEditor"/>.
+        /// </summary>
+        /// <example>
+        /// <code lang="CS">
+        ///  &lt;%= Html.Kendo().RecurrenceEditorFor(m=>m.Property) %&gt;
+        /// </code>
+        /// </example>
+        public virtual RecurrenceEditorBuilder RecurrenceEditorFor<TValue>(Expression<Func<TModel, TValue>> expression)
+        {
+            var explorer = GetModelExplorer(expression);
+            var model = explorer.Model;
+
+            var widget = RecurrenceEditor()
+                    .Expression(GetExpressionName(expression))
+                    .Value(model != null && model.GetType().IsPredefinedType() ? Convert.ToString(model) : null);
+
+            return widget;
+        }
+    }
+}
