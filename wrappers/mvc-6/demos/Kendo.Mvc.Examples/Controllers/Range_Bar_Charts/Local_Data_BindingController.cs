f914303a25fa2064697cea75b19130d1d7e2cdf7
Stamo-Gochev <stamo.gochev@telerik.com>
2016-04-05 09:38:02 +0300

    Add Chart demos for Range series in MVC Core

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Range_Bar_Charts/Local_Data_BindingController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Kendo.Mvc.Examples.Models;
+using Microsoft.AspNet.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class Range_Bar_ChartsController : Controller
+    {
+        public IActionResult Local_Data_Binding()
+        {
+            var viewModel = new List<RangeBarChartsLocalDataViewModel>();
+            viewModel.Add(new RangeBarChartsLocalDataViewModel(0, 10, 0, 30, "Monday"));
+            viewModel.Add(new RangeBarChartsLocalDataViewModel(10, 30, 30, 45, "Tuesday"));
+            viewModel.Add(new RangeBarChartsLocalDataViewModel(30, 40, 45, 60, "Wednesday"));
+            viewModel.Add(new RangeBarChartsLocalDataViewModel(40, 60, 60, 80, "Thursday"));
+            viewModel.Add(new RangeBarChartsLocalDataViewModel(60, 100, 80, 100, "Friday"));
+
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Range_Bar_Charts/Remote_Data_BindingController.cs
@@ -0,0 +1,19 @@
+﻿using Kendo.Mvc.Examples.Models;
+using Microsoft.AspNet.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class Range_Bar_ChartsController : Controller
+    {
+        public IActionResult Remote_Data_Binding()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult _DownloadSpeeds()
+        {
+            return Json(ChartDataRepository.DownloadSpeeds());
+        }
+    }
+}
