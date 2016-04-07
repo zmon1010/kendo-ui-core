0ab3d983429d1e54aebc31e0e75f951da410d0fb
Stamo-Gochev <stamo.gochev@telerik.com>
2016-04-07 09:07:30 +0300

    Add Sparkline demos for MVC Core

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Sparklines/Remote_Data_BindingController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Kendo.Mvc.Examples.Models;
+using Microsoft.AspNet.Mvc;
+using System.Linq;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class SparklinesController : Controller
+    {
+        public IActionResult Remote_Data_Binding()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult _Weather(string station, int year, int month)
+        {
+            var db = new SampleEntitiesDataContext();
+            var weatherData = db.Weather
+                .Where(x => x.Station == station && x.Date.Year == year && x.Date.Month == month)
+                .Select(x => new
+                {
+                    ID = x.ID,
+                    Rain = x.Rain,             
+                    TMax = x.TMax,
+                    Wind = x.Wind
+                });
+            
+            return Json(weatherData);
+        }
+    }
+}
