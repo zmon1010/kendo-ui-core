07aa464b345df1d9e1e29ff770e923e2a183c869
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2016-04-08 14:32:44 +0300

    Add Spreadsheet Mvc Core demos

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Spreadsheet/Disabled_CellsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class SpreadsheetController : Controller
+    {
+        public ActionResult Disabled_Cells()
+        {
+            return View();
+        }
+    }
+}
