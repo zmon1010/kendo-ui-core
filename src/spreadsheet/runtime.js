zmon1010/kendo-ui-core

commit 4183ff5b95083e5ac87f23b85d62645c3967bc00

[spreadsheet] Fix argument for functions expecting strings
As it turns out, functions expecting a string argument should silently
convert numbers, booleans or the empty value, to string, e.g.:

    =LEN(10) --> 2
    =LEN(false) --> 5

This fixes an additional issue visible in the file attached to #5528
(#VALUE in column G).

src/spreadsheet/runtime.js
@@ -880,7 +880,7 @@
                 return "((typeof " + force() + " == 'number' || typeof $"+name+" == 'boolean') && (($"+name+" |= 0 ) > 0) ? true : ((err = 'NUM'), false))";
             }
             if (type == "string") {
-                return "(typeof " + force() + " == 'string')";
+                return "((typeof " + force() + " == 'string' || typeof $"+name+" == 'boolean' || typeof $"+name+" == 'number') ? ($"+name+" += '', true) : ($"+name+" === undefined ? ($"+name+" = '', true) : false))";
             }
             if (type == "boolean") {
                 return "(typeof " + force() + " == 'boolean')";

commit 3bd0ff96df7e7f7e898903f07a67fbe39d55da79

[spreadsheet] Fix default value for optional string args
Regression occurred in 4183ff5b95083e5ac87f23b85d62645c3967bc00 — we
can't treat undefined as empty string.

src/spreadsheet/runtime.js
@@ -880,7 +880,7 @@
                 return "((typeof " + force() + " == 'number' || typeof $"+name+" == 'boolean') && (($"+name+" |= 0 ) > 0) ? true : ((err = 'NUM'), false))";
             }
             if (type == "string") {
-                return "((typeof " + force() + " == 'string' || typeof $"+name+" == 'boolean' || typeof $"+name+" == 'number') ? ($"+name+" += '', true) : ($"+name+" === undefined ? ($"+name+" = '', true) : false))";
+                return "((typeof " + force() + " == 'string' || typeof $"+name+" == 'boolean' || typeof $"+name+" == 'number') ? ($"+name+" += '', true) : false)";
             }
             if (type == "boolean") {
                 return "(typeof " + force() + " == 'boolean')";