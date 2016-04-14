b07a4249d977cbaed0e670c14a8d647184b09bf2
Stamo-Gochev <stamo.gochev@telerik.com>
2016-04-14 11:33:33 +0300

    Add button tools to Editor in MVC Core

--- wrappers/mvc-6/src/Kendo.Mvc/UI/Editor/Fluent/EditorToolFactory.Generated.cs
+++ wrappers/mvc-6/src/Kendo.Mvc/UI/Editor/Fluent/EditorToolFactory.Generated.cs
@@ -24,6 +24,204 @@ namespace Kendo.Mvc.UI.Fluent
         }
 
         /// <summary>
+        /// Adds "cleanFormatting" tool.
+        /// </summary>
+        public virtual EditorToolFactory CleanFormatting()
+        {
+            AddTool("cleanFormatting");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "createLink" tool.
+        /// </summary>
+        public virtual EditorToolFactory CreateLink()
+        {
+            AddTool("createLink");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "indent" tool.
+        /// </summary>
+        public virtual EditorToolFactory Indent()
+        {
+            AddTool("indent");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "insertFile" tool.
+        /// </summary>
+        public virtual EditorToolFactory InsertFile()
+        {
+            AddTool("insertFile");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "insertImage" tool.
+        /// </summary>
+        public virtual EditorToolFactory InsertImage()
+        {
+            AddTool("insertImage");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "insertOrderedList" tool.
+        /// </summary>
+        public virtual EditorToolFactory InsertOrderedList()
+        {
+            AddTool("insertOrderedList");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "insertUnorderedList" tool.
+        /// </summary>
+        public virtual EditorToolFactory InsertUnorderedList()
+        {
+            AddTool("insertUnorderedList");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "italic" tool.
+        /// </summary>
+        public virtual EditorToolFactory Italic()
+        {
+            AddTool("italic");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "justifyCenter" tool.
+        /// </summary>
+        public virtual EditorToolFactory JustifyCenter()
+        {
+            AddTool("justifyCenter");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "justifyFull" tool.
+        /// </summary>
+        public virtual EditorToolFactory JustifyFull()
+        {
+            AddTool("justifyFull");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "justifyLeft" tool.
+        /// </summary>
+        public virtual EditorToolFactory JustifyLeft()
+        {
+            AddTool("justifyLeft");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "justifyRight" tool.
+        /// </summary>
+        public virtual EditorToolFactory JustifyRight()
+        {
+            AddTool("justifyRight");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "outdent" tool.
+        /// </summary>
+        public virtual EditorToolFactory Outdent()
+        {
+            AddTool("outdent");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "pdf" tool.
+        /// </summary>
+        public virtual EditorToolFactory Pdf()
+        {
+            AddTool("pdf");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "print" tool.
+        /// </summary>
+        public virtual EditorToolFactory Print()
+        {
+            AddTool("print");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "separator" tool.
+        /// </summary>
+        public virtual EditorToolFactory Separator()
+        {
+            AddTool("separator");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "strikethrough" tool.
+        /// </summary>
+        public virtual EditorToolFactory Strikethrough()
+        {
+            AddTool("strikethrough");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "subscript" tool.
+        /// </summary>
+        public virtual EditorToolFactory Subscript()
+        {
+            AddTool("subscript");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "superscript" tool.
+        /// </summary>
+        public virtual EditorToolFactory Superscript()
+        {
+            AddTool("superscript");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "underline" tool.
+        /// </summary>
+        public virtual EditorToolFactory Underline()
+        {
+            AddTool("underline");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "unlink" tool.
+        /// </summary>
+        public virtual EditorToolFactory Unlink()
+        {
+            AddTool("unlink");
+            return this;
+        }
+
+        /// <summary>
+        /// Adds "viewHtml" tool.
+        /// </summary>
+        public virtual EditorToolFactory ViewHtml()
+        {
+            AddTool("viewHtml");
+            return this;
+        }
+
+        /// <summary>
         /// Adds "backColor" tool.
         /// </summary>
         public virtual EditorToolFactory BackColor()
--- wrappers/mvc-6/tests/Kendo.Mvc.Tests/UI/Editor/Fluent/Tools/EditorToolFactoryTests.cs
+++ wrappers/mvc-6/tests/Kendo.Mvc.Tests/UI/Editor/Fluent/Tools/EditorToolFactoryTests.cs
@@ -32,6 +32,336 @@ namespace Kendo.Mvc.UI.Tests
         }
 
         [Fact]
+        public void Builder_should_create_CleanFormatting_tool()
+        {
+            builder.CleanFormatting();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("cleanFormatting");
+        }
+
+        [Fact]
+        public void CleanFormatting_should_return_builder()
+        {
+            builder.CleanFormatting().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_CreateLink_tool()
+        {
+            builder.CreateLink();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("createLink");
+        }
+
+        [Fact]
+        public void CreateLink_should_return_builder()
+        {
+            builder.CreateLink().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_Indent_tool()
+        {
+            builder.Indent();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("indent");
+        }
+
+        [Fact]
+        public void Indent_should_return_builder()
+        {
+            builder.Indent().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_InsertFile_tool()
+        {
+            builder.InsertFile();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("insertFile");
+        }
+
+        [Fact]
+        public void InsertFile_should_return_builder()
+        {
+            builder.InsertFile().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_InsertImage_tool()
+        {
+            builder.InsertImage();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("insertImage");
+        }
+
+        [Fact]
+        public void InsertImage_should_return_builder()
+        {
+            builder.InsertImage().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_InsertOrderedList_tool()
+        {
+            builder.InsertOrderedList();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("insertOrderedList");
+        }
+
+        [Fact]
+        public void InsertOrderedList_should_return_builder()
+        {
+            builder.InsertOrderedList().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_InsertUnorderedList_tool()
+        {
+            builder.InsertUnorderedList();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("insertUnorderedList");
+        }
+
+        [Fact]
+        public void InsertUnorderedList_should_return_builder()
+        {
+            builder.InsertUnorderedList().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_Italic_tool()
+        {
+            builder.Italic();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("italic");
+        }
+
+        [Fact]
+        public void Italic_should_return_builder()
+        {
+            builder.Italic().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_JustifyCenter_tool()
+        {
+            builder.JustifyCenter();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("justifyCenter");
+        }
+
+        [Fact]
+        public void JustifyCenter_should_return_builder()
+        {
+            builder.JustifyCenter().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_JustifyFull_tool()
+        {
+            builder.JustifyFull();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("justifyFull");
+        }
+
+        [Fact]
+        public void JustifyFull_should_return_builder()
+        {
+            builder.JustifyFull().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_JustifyLeft_tool()
+        {
+            builder.JustifyLeft();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("justifyLeft");
+        }
+
+        [Fact]
+        public void JustifyLeft_should_return_builder()
+        {
+            builder.JustifyLeft().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_JustifyRight_tool()
+        {
+            builder.JustifyRight();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("justifyRight");
+        }
+
+        [Fact]
+        public void JustifyRight_should_return_builder()
+        {
+            builder.JustifyRight().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_Outdent_tool()
+        {
+            builder.Outdent();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("outdent");
+        }
+
+        [Fact]
+        public void Outdent_should_return_builder()
+        {
+            builder.Outdent().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_Pdf_tool()
+        {
+            builder.Pdf();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("pdf");
+        }
+
+        [Fact]
+        public void Pdf_should_return_builder()
+        {
+            builder.Pdf().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_Print_tool()
+        {
+            builder.Print();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("print");
+        }
+
+        [Fact]
+        public void Print_should_return_builder()
+        {
+            builder.Print().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_Separator_tool()
+        {
+            builder.Separator();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("separator");
+        }
+
+        [Fact]
+        public void Separator_should_return_builder()
+        {
+            builder.Separator().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_Strikethrough_tool()
+        {
+            builder.Strikethrough();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("strikethrough");
+        }
+
+        [Fact]
+        public void Strikethrough_should_return_builder()
+        {
+            builder.Strikethrough().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_Subscript_tool()
+        {
+            builder.Subscript();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("subscript");
+        }
+
+        [Fact]
+        public void Subscript_should_return_builder()
+        {
+            builder.Subscript().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_Superscript_tool()
+        {
+            builder.Superscript();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("superscript");
+        }
+
+        [Fact]
+        public void Superscript_should_return_builder()
+        {
+            builder.Superscript().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_Underline_tool()
+        {
+            builder.Underline();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("underline");
+        }
+
+        [Fact]
+        public void Underline_should_return_builder()
+        {
+            builder.Underline().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_Unlink_tool()
+        {
+            builder.Unlink();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("unlink");
+        }
+
+        [Fact]
+        public void Unlink_should_return_builder()
+        {
+            builder.Unlink().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
+        public void Builder_should_create_ViewHtml_tool()
+        {
+            builder.ViewHtml();
+
+            items.Count.ShouldEqual(1);
+            items[0].Name.ShouldEqual("viewHtml");
+        }
+
+        [Fact]
+        public void ViewHtml_should_return_builder()
+        {
+            builder.ViewHtml().ShouldBeSameAs(builder);
+        }
+
+        [Fact]
         public void Builder_should_create_FontName_tool()
         {
             builder.FontName();
