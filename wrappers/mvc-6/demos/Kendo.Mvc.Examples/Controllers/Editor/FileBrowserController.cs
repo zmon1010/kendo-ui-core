a92ffe87f643e12311581f25452cce1886c6be05
Stamo-Gochev <stamo.gochev@telerik.com>
2016-04-15 12:12:19 +0300

    Add a demo for FileBrowser and ImageBrowser in MVC Core

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Editor/FileBrowserController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using Kendo.Mvc.UI;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public class FileBrowserController : EditorFileBrowserController
+    {
+        private const string contentFolderRoot = "shared/";
+        private const string folderName = "Images/";        
+        private static readonly string[] foldersToCopy = new[] { "shared/images/employees" };
+
+        [FromServices]
+        public override IHostingEnvironment Server { get; set; }
+        
+        /// <summary>
+        /// Gets the base paths from which content will be served.
+        /// </summary>
+        public override string ContentPath
+        {
+            get
+            {
+                return CreateUserFolder();
+            }
+        }
+
+        /// <summary>
+        /// Gets the valid file extensions by which served files will be filtered.
+        /// </summary>
+        public override string Filter
+        {
+            get
+            {
+                return "*.png";
+            }
+        }
+
+        private string CreateUserFolder()
+        {            
+            var virtualPath = Path.Combine(contentFolderRoot, "UserFiles", folderName);
+
+            var path = Server.MapPath(virtualPath);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                foreach (var sourceFolder in foldersToCopy)
+                {
+                    CopyFolder(Server.MapPath(sourceFolder), path);
+                }
+            }
+            return virtualPath;
+        }
+
+        private void CopyFolder(string source, string destination)
+        {
+            if (!Directory.Exists(destination))
+            {
+                Directory.CreateDirectory(destination);
+            }
+
+            foreach (var file in Directory.EnumerateFiles(source))
+            {
+                var dest = Path.Combine(destination, Path.GetFileName(file));
+                System.IO.File.Copy(file, dest);
+            }
+
+            foreach (var folder in Directory.EnumerateDirectories(source))
+            {
+                var dest = Path.Combine(destination, Path.GetFileName(folder));
+                CopyFolder(folder, dest);
+            }
+        }
+    }
+}
