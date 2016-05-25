98c60b8c9466704a822bdf67e56a3219b9252c2c
Stanimir Patarinski <spatarinski@gmail.com>
2016-05-25 14:04:44 +0300

    Add immutables.js file

---
+++ src/editor/immutables.js
@@ -0,0 +1,19 @@
+(function(f, define){
+    define([ "./tables" ], f);
+})(function(){
+
+(function($, undefined) {
+
+var kendo = window.kendo,
+    extend = $.extend,
+    proxy = $.proxy,
+    Editor = kendo.ui.editor,
+    dom = Editor.Dom,
+    EditorUtils = Editor.EditorUtils,
+    RangeUtils = Editor.RangeUtils,
+    Command = Editor.Command,
+    NS = ".kendoEditor";
+
+})(window.kendo.jQuery);
+
+}, typeof define == 'function' && define.amd ? define : function(a1, a2, a3){ (a3 || a2)(); }); 
