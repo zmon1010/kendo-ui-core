41d0b0bdc370d271c3e6f8428475af24b4bcf631
Petyo Ivanov <underlog@gmail.com>
2016-06-07 12:56:03 +0300

    Fix missing constant

--- build/npm.rb
+++ build/npm.rb
@@ -7,6 +7,19 @@ task :npm do
 
     npmgit = "git --work-tree=#{repo_path} --git-dir=#{repo_path}/.git"
 
+    SYNC_NPM = <<-SH
+        cd /usr/share/nginx/html;
+        if [ ! -d npm-kendo-ui.git ]; then
+            git clone --bare git@github.com:telerik/npm-kendo-ui.git;
+            cd npm-kendo-ui.git;
+            git config http.receivepack false;
+        fi
+
+        cd npm-kendo-ui.git;
+        git fetch;
+        git fetch --tags --prune origin;
+    SH
+
     sh <<-SHELL
     if [ ! -d #{repo_path} ]; then
         git clone #{repo_url} #{repo_path};
@@ -25,7 +38,7 @@ task :npm do
     #{npmgit} push --force origin master:master;
     #{npmgit} push --force origin "#{VERSION}";
 
-    ssh nginx@ordkendobower01.telerik.local '#{SYNC_BOWER}';
+    ssh nginx@ordkendobower01.telerik.local '#{SYNC_NPM}';
 
     SHELL
 
