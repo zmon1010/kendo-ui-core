978b5e67c840e13c46cf08db51b4e8a7bf83c7c7
zdravkov <zdravkov.pl@gmail.com>
2016-06-08 16:05:23 +0300

    Generate Gantt editable MVC core wrappers

--- wrappers/mvc-6/src/Kendo.Mvc/UI/Gantt/Fluent/GanttEditableSettingsBuilder.Generated.cs
+++ wrappers/mvc-6/src/Kendo.Mvc/UI/Gantt/Fluent/GanttEditableSettingsBuilder.Generated.cs
@@ -21,6 +21,86 @@ namespace Kendo.Mvc.UI.Fluent
         }
 
         /// <summary>
+        /// If set to false the user won't be able to create tasks.
+        /// </summary>
+        /// <param name="value">The value for Create</param>
+        public GanttEditableSettingsBuilder<TTaskModel, TDependenciesModel> Create(bool value)
+        {
+            Container.Create = value;
+            return this;
+        }
+
+        /// <summary>
+        /// If set to false the user won't be able to create dependencies.
+        /// </summary>
+        /// <param name="value">The value for DependencyCreate</param>
+        public GanttEditableSettingsBuilder<TTaskModel, TDependenciesModel> DependencyCreate(bool value)
+        {
+            Container.DependencyCreate = value;
+            return this;
+        }
+
+        /// <summary>
+        /// If set to false the user won't be able to delete dependencies.
+        /// </summary>
+        /// <param name="value">The value for DependencyDestroy</param>
+        public GanttEditableSettingsBuilder<TTaskModel, TDependenciesModel> DependencyDestroy(bool value)
+        {
+            Container.DependencyDestroy = value;
+            return this;
+        }
+
+        /// <summary>
+        /// If set to false the user won't be able to edit the percentComplete of the tasks.
+        /// </summary>
+        /// <param name="value">The value for DragPercentComplete</param>
+        public GanttEditableSettingsBuilder<TTaskModel, TDependenciesModel> DragPercentComplete(bool value)
+        {
+            Container.DragPercentComplete = value;
+            return this;
+        }
+
+        /// <summary>
+        /// If set to false the user won't be able to delete tasks.
+        /// </summary>
+        /// <param name="value">The value for Destroy</param>
+        public GanttEditableSettingsBuilder<TTaskModel, TDependenciesModel> Destroy(bool value)
+        {
+            Container.Destroy = value;
+            return this;
+        }
+
+        /// <summary>
+        /// If set to false the user won't be able to move tasks.
+        /// </summary>
+        /// <param name="value">The value for Move</param>
+        public GanttEditableSettingsBuilder<TTaskModel, TDependenciesModel> Move(bool value)
+        {
+            Container.Move = value;
+            return this;
+        }
+
+        /// <summary>
+        /// If set to false the user won't be able to reorder tasks in the task list.
+        /// </summary>
+        /// <param name="value">The value for Reorder</param>
+        public GanttEditableSettingsBuilder<TTaskModel, TDependenciesModel> Reorder(bool value)
+        {
+            Container.Reorder = value;
+            return this;
+        }
+
+        /// <summary>
+        /// If set to false the user won't be able to resize tasks.
+        /// </summary>
+        /// <param name="value">The value for Resize</param>
+        public GanttEditableSettingsBuilder<TTaskModel, TDependenciesModel> Resize(bool value)
+        {
+            Container.Resize = value;
+            return this;
+        }
+
+        /// <summary>
         /// The template which renders the editor.The template should contain elements whose name HTML attributes are set as the editable fields. This is how the Gantt will know
 		/// which field to update. The other option is to use MVVM bindings in order to bind HTML elements to data item fields.
         /// </summary>
@@ -39,6 +119,16 @@ namespace Kendo.Mvc.UI.Fluent
         public GanttEditableSettingsBuilder<TTaskModel, TDependenciesModel> TemplateId(string templateId)
         {
             Container.TemplateId = templateId;
+            return this;
+        }
+
+        /// <summary>
+        /// If set to false the user won't be able to update tasks.
+        /// </summary>
+        /// <param name="value">The value for Update</param>
+        public GanttEditableSettingsBuilder<TTaskModel, TDependenciesModel> Update(bool value)
+        {
+            Container.Update = value;
             return this;
         }
 
