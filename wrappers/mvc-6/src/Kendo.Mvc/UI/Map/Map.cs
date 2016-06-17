f93b22c05b2fae979e58c3e09277386344c6aeea
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2016-06-17 15:31:41 +0300

    Add Map to WidgetFactory

--- wrappers/mvc-6/src/Kendo.Mvc/UI/Map/Map.cs
+++ wrappers/mvc-6/src/Kendo.Mvc/UI/Map/Map.cs
@@ -1,7 +1,4 @@
-using Kendo.Mvc.Extensions;
-using Microsoft.AspNet.Mvc.Rendering;
-using System;
-using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.IO;
 
 namespace Kendo.Mvc.UI
