c6c04e1b7192e112367a8df125473e3aeaec0a3e
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2016-06-22 17:51:07 +0300

    Add the rest of the map demos

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Map/ExportController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class MapController : Controller
+    {
+        public ActionResult Export()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Export_Save(string contentType, string base64, string fileName)
+        {
+            var fileContents = Convert.FromBase64String(base64);
+
+            return File(fileContents, contentType, fileName);
+        }
+    }
+}
