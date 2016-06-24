0e2014072a917e3035214ccef4d52aaf01acc992
Mihai Bazon <mihai.bazon@gmail.com>
2016-06-24 11:22:22 +0300

    [spreadsheet] parse more number input formats
    Previously, we missed support for negative fancy inputs.
    
    Ref telerik/kendo-ui-core#1828

--- src/spreadsheet/calc.js
+++ src/spreadsheet/calc.js
@@ -1383,9 +1383,15 @@
         var comma = culture.numberFormat[","];
         var dot = culture.numberFormat["."];
         var currency = culture.numberFormat.currency.symbol;
-        var rx = new RegExp("^(\\" + currency + "\\s*)?(\\d+(\\" + comma + "\\d{3})*(\\" + dot + "\\d+)?)(\\s*\\" + currency + ")?$");
+        var rx = getNumberRegexp(currency, comma, dot);
+        var sign = 1;
+        if (/-/.test(input)) {
+            input = input.replace(/-/, "");
+            sign = -1;
+        }
         if ((m = rx.exec(input))) {
             var value = m[2]
+                .replace(/\s+/g, "")
                 .replace(new RegExp("\\" + comma, "g"), "")
                 .replace(dot, "."); // depending on culture, dot might be ','
             var format = "#";
@@ -1396,7 +1402,7 @@
                 format += "." + repeat("0", m[1] || m[5] ? 2 : m[4].length - 1);
             }
             if (m[1]) {
-                format = '"' + m[1] + '"' + format;
+                format = '"' + m[1].replace(/-/, "") + '"' + format;
             }
             if (m[5]) {
                 format = format + '"' + m[5] + '"';
@@ -1404,10 +1410,25 @@
             return {
                 type   : "number",
                 format : format == "#" ? null : format,
-                value  : parseFloat(value)
+                value  : sign * parseFloat(value)
             };
         }
     });
+
+    var NUMBER_FORMAT_RX = {};
+    function getNumberRegexp(currency, comma, dot) {
+        var id = currency + comma + dot;
+        var rx = NUMBER_FORMAT_RX[id];
+        if (!rx) {
+            rx = "^\\s*(CUR\\s*)?(\\d+(COM\\d{3})*(DOT\\d+)?)(\\s*CUR)?\\s*$";
+            rx = rx.replace(/CUR/g, "\\" + currency)
+                .   replace(/DOT/g, "\\" + dot)
+                .   replace(/COM/g, "\\" + comma);
+            rx = new RegExp(rx);
+            NUMBER_FORMAT_RX[id] = rx;
+        }
+        return rx;
+    }
 
     function repeat(str, len) {
         var out = "";
