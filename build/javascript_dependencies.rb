d7b83c7ba927bbe0accf1af5e49d7fe211a5763d
Desislava Mihaylova <dmihaylo@telerik.com>
2016-06-27 11:44:42 +0300

    Fix layout issues in script doc files

--- build/javascript_dependencies.rb
+++ build/javascript_dependencies.rb
@@ -55,91 +55,91 @@ task :js_dependencies do
             'name' => 'Data Management Widgets',
             'slug' => 'script_filesfor_datamanagement_widgets',
             'file' => 'docs/intro/supporting/scripts-general.md',
-            'description' => 'Below are listed the script files for Kendo UI desktop widgets for mobile-ready websites and applications:',
+            'description' => 'Below are listed the script files for the Kendo UI desktop widgets for mobile-ready websites and applications.',
             'include' => /^(grid|listview|pivotgrid|spreadsheet|treelist)$/i
         },
         {
             'name' => 'Editors',
             'slug' => 'script_filesfor_editors_widgets',
             'file' => 'docs/intro/supporting/scripts-editors.md',
-            'description' => 'Below are listed the script files for the Kendo UI Editors widgets:',
+            'description' => 'Below are listed the script files for the Kendo UI Editors widgets.',
             'include' => /^(autocomplete|.*picker|combobox|dropdownlist|editor|.*textbox|multiselect|.*slider|upload|validator)$/i
         },
         {
             'name' => 'Charts',
             'slug' => 'script_filesfor_charts_widgets',
             'file' => 'docs/intro/supporting/scripts-dataviz.md',
-            'description' => 'Below are listed the script files for the Kendo UI Charts widgets:',
+            'description' => 'Below are listed the script files for the Kendo UI Charts widgets.',
             'include' => /^dataviz\.(chart|sparkline|stockchart|treemap)$/i
         },
         {
             'name' => 'Gauges',
             'slug' => 'script_filesfor_gauges_widgets',
             'file' => 'docs/intro/supporting/scripts-gauges.md',
-            'description' => 'Below are listed the script files for the Kendo UI Gauges widgets:',
+            'description' => 'Below are listed the script files for the Kendo UI Gauges widgets.',
             'include' => /^dataviz\.(gauge)$/i
         },
         {
             'name' => 'Barcodes',
             'slug' => 'script_filesfor_barcodes_widgets',
             'file' => 'docs/intro/supporting/scripts-barcodes.md',
-            'description' => 'Below are listed the script files for Kendo UI Barcodes:',
+            'description' => 'Below are listed the script files for Kendo UI Barcodes widgets.',
             'include' => /^dataviz\.(barcode|qrcode)$/i
         },
         {
             'name' => 'Server-Side Wrappers',
             'slug' => 'script_filesfor_serverside_wrappers',
             'file' => 'docs/intro/supporting/scripts-wrappers.md',
-            'description' => 'Below are listed the script files for Kendo UI supplementary scripts for integration with server-side technologies:',
+            'description' => 'Below are listed the script files for the Kendo UI supplementary scripts for integration with server-side technologies.',
             'include' => /^aspnetmvc/i
         },
         {
             'name' => 'Diagrams and Maps',
             'slug' => 'script_filesfor_diagramsandmaps_widgets',
             'file' => 'docs/intro/supporting/scripts-diagrams-maps.md',
-            'description' => 'Below are listed the script files for the Kendo UI Diagrams and Maps:',
+            'description' => 'Below are listed the script files for the Kendo UI Diagrams and Maps widgets.',
             'include' => /^dataviz\.(diagram|map)$/i
         },
         {
-            'name' => 'Scheduling widgets',
+            'name' => 'Scheduling',
             'slug' => 'script_filesfor_scheduling_widgets',
             'file' => 'docs/intro/supporting/scripts-scheduling.md',
-            'description' => 'Below are listed the script files for the Kendo UI Scheduling widgets:',
+            'description' => 'Below are listed the script files for the Kendo UI Scheduling widgets.',
             'include' => /^(scheduler|calendar|gantt)$/i
         },
         {
-            'name' => 'Layout widgets',
+            'name' => 'Layout',
             'slug' => 'script_filesfor_layout_widgets',
             'file' => 'docs/intro/supporting/scripts-layout.md',
-            'description' => 'Below are listed the script files for the Kendo UI Layout widgets:',
+            'description' => 'Below are listed the script files for the Kendo UI Layout widgets.',
             'include' => /^(notification|responsive-panel|splitter|tooltip|window)$/i
         },
         {
             'name' => 'Navigation',
             'slug' => 'script_filesfor_navigation_widgets',
             'file' => 'docs/intro/supporting/scripts-navigation.md',
-            'description' => 'Below are listed the script files for the Kendo UI Navigation widgets:',
+            'description' => 'Below are listed the script files for the Kendo UI Navigation widgets.',
             'include' => /^(button|menu|panelbar|tabstrip|toolbar|treeview)$/i
         },
         {
             'name' => 'Interactivity and UX',
             'slug' => 'script_filesfor_interactivityandux_widgets',
             'file' => 'docs/intro/supporting/scripts-interactivity.md',
-            'description' => 'Below are listed the script files for the Kendo UI Interactivity and UX widgets:',
+            'description' => 'Below are listed the script files for the Kendo UI Interactivity and UX widgets.',
             'include' => /^(draganddrop|fx|progressbar|sortable)$/i
         },
         {
             'name' => 'Hybrid UI',
             'slug' => 'script_filesfor_hybridui_widgets',
             'file' => 'docs/intro/supporting/scripts-hybridui.md',
-            'description' => 'Below are listed the script files for the Kendo UI Hybrid frameworks and widgets:',
+            'description' => 'Below are listed the script files for the Kendo UI Hybrid frameworks and widgets.',
             'include' => /^(mobile\..*)$/i
         },
         {
             'name' => 'Tools, Frameworks and Utilities',
             'slug' => 'script_filesfor_tools_frameworks_utilities',
             'file' => 'docs/intro/supporting/scripts-frameworks.md',
-            'description' => 'Below are listed the script files for the Kendo UI shared components providing behaviors, data access and other services:',
+            'description' => 'Below are listed the script files for the Kendo UI shared components providing behaviors, data access. and other services.',
             'include' => /^(angular.*|mvvm|core|datasource|drawing|pdf-export|router|(mobile\.router|touch|view))$/i
         }
     ]
@@ -183,4 +183,3 @@ task :js_dependencies do
         end
     end
 end
-
