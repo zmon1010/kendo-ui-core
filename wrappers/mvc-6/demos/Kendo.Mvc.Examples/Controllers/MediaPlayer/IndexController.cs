a61c9585e296f0e22656b8f7a1c856a49027815f
Angel Petrov <angel.petrov@telerik.com>
2016-07-04 13:30:03 +0300

    Added the overview demo of the media player

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/MediaPlayer/IndexController.cs
@@ -0,0 +1,32 @@
+﻿using Kendo.Mvc.Examples.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class MediaPlayerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Videos_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            List<Video> videos = new List<Video>();
+            videos.Add(new Video()
+            {
+                 url= "http://www.webestools.com/page/media/videoTag/BigBuckBunny.ogg",
+                 Title = "Act a fool"
+            });
+            videos.Add(new Video()
+            {
+                url = "https://www.youtube.com/watch?v=vr7Xy8POfEM&list=PLhyyDxxabfF9bN4zPBDP1I5tt5oWr4ZtB&index=2",
+                Title = "Няма чакай"
+            });
+            return Json(videos.ToDataSourceResult(request));
+        }
+    }
+}
---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Models/Video.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Kendo.Mvc.Examples.Models
+{
+    public class Video
+    {
+        public string Title { get; set; }
+        public string url { get; set; }
+    }
+}
