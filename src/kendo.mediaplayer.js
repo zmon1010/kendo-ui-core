fe694c0227918d73888ec0ff8436d73c90042bf4
Daniel Peichev <peichev@progress.com>
2016-07-07 16:40:09 +0300

    Various improvements and fixes to the mediaplayer. Added test videos, comments and temp styles to the playground example.

--- src/kendo.mediaplayer.js
+++ src/kendo.mediaplayer.js
@@ -2,8 +2,6 @@
     define(["./kendo.data", "./kendo.slider", "./kendo.toolbar"], f);
 })(function () {
 
-    //todo: destroy, keep proxy instance, remove that if not necessary
-
     var __meta__ = { // jshint ignore:line
         id: "mediaplayer",
         name: "MediaPlayer",
@@ -26,6 +24,9 @@
         MEDIA = "k-mediaplayer-media",
         DOT = ".",
         ui = kendo.ui,
+        ns = ".kendoMediaPlayer",
+        baseTime = new Date(1970,0,1),  
+        timeZoneSec = baseTime.getTimezoneOffset() * 60;                
         Widget = kendo.ui.Widget,
         //extend = $.extend,
         proxy = $.proxy,
@@ -34,43 +35,63 @@
             htmlPlayer: "<video class='" + MEDIA + "'> </video>",
             titleBar: "<div class='" + TITLEBAR + "'><span class='" + TITLE + "'>Video Title</span><a role='button' class='k-icon k-font-icon k-i-playlist-open' style='float: right;''>Open Playlist</a></div>",
             toolBar: "<div class='" + TOOLBAR + "'> </div>",
+            toolBarTime: "<span id='currentTime'>00:00:00</span> / <span id='duration'>00:00:00</span>",
             slider: "<input class='" + SLIDER + "' value='0' />",
             toolTip: "#= kendo.toString(new Date(value), 'HH:mm:ss') #"
         },
-        wrapper, slider, toolBar, titleBar,
-        //rendering = {},
+        rendering = {
+            renderPlaylistButton: function (options) {
+              if (options.playlist && !(options.playlist.button === false)) {
+                  return templates.playlistButton;
+              }
+              return "";
+            },
+
+            renderPlaylistItems: function (data) {
+                var html = "",
+                    i = 0;
+                
+                for (var i = 0; i < data.length; i++) {
+                    var item = data[i];
+                    html += templates.playlistItem({
+                                title: item.title,
+                                poster: item.poster
+                            });
+                }
+
+                return html;
+            }
+        },
         DataSource = kendo.data.DataSource;
 
 
         var MediaPlayer = Widget.extend({
             init: function (element, options) {
-                var that = this;
-
                 wrapper = $(element),
 
                 options = $.isArray(options) ? { dataSource: options } : options;
 
-                Widget.fn.init.call(that, element, options);
+                Widget.fn.init.call(this, element, options);
 
                 wrapper.addClass("k-mediaplayer k-widget");
             
-                options = that.options;
+                options = this.options;
                 
                 that._createHtmlPlayer(options);
 
-                that._createTitlebar();
+                this._createTitlebar(options);
 
-                that._createToolbar();
+                this._createToolbar();
 
-                that._createSlider();
+                this._createSlider();
 
-                that._dataSource();
+                this._dataSource();
 
-                if (that.options.autoBind) {
-                    that.dataSource.fetch();
+                if (this.options.autoBind) {
+                    this.dataSource.fetch();
                 }
 
-                kendo.notify(that);
+                kendo.notify(this);
             },
 
             events: [
@@ -89,8 +110,6 @@
             },
 
             _initData: function (options) {
-                //var that = this;
-
                 if (options.dataSource) {
                 }
             },
@@ -105,9 +124,9 @@
             },
 
             _msToTime: function(ms) {
-                var baseTime = new Date(0);
-                baseTime.setSeconds(ms);
-                return baseTime;
+                var time = new Date(baseTime.getTime());
+                time.setSeconds(ms);
+                return time;
             },
 
             _timeToSec: function(time) {
@@ -115,75 +134,108 @@
                 return curTime / 1000;
             },
 
-            _createTitlebar: function() {
-                var that = this;
+            _createTitlebar: function(options) {
                 titleBar = wrapper.find(DOT + TITLEBAR);
-                
-                if (!titleBar || titleBar.length === 0) { 
-                    wrapper.append(templates.titleBar);
+                if (titleBar.length === 0) {
+                    this._playlistButtonClickHandler = proxy(this._playlistButtonClick, this);
+                    wrapper.append(templates.titleBar(extend(options, rendering)));
+                    wrapper.find(DOT + PLAYLIST_OPEN).on("click" + ns, this._playlistButtonClickHandler);
                 }
+            },
+
+            _playlistButtonClick: function () {
+                wrapper.find(DOT + PLAYLIST).toggle();
             },
 
             _createSlider: function() {
-                var that = this;
                 var sliderElement = wrapper.find(DOT + SLIDER);
-
-                if (sliderElement.length === 0) { 
+                if (sliderElement.length === 0) {
+                    this._sliderDragChangeHandler = proxy(this._sliderDragChange, this);
+                    this._sliderDraggingHandler = proxy(this._sliderDragging, this);
                     wrapper.append(templates.slider);
+                    sliderElement = $(DOT + SLIDER);
+                    slider = new ui.Slider(sliderElement[0], {
+                        smallStep: 1000,
+                        tickPlacement: "none",
+                        showButtons: false,
+                        change: this._sliderDragChangeHandler,
+                        slide: this._sliderDraggingHandler,
+                        tooltip: {
+                            template: templates.toolTip
+                        }
+                    });
                 }
-                sliderElement = $(DOT + SLIDER);
-                sliderElement.kendoSlider({
-                    smallStep: 1000,
-                    tickPlacement: "none",
-                    showButtons: false,
-                    change: function(e) {
-                        that._media.currentTime = that._timeToSec(e.value);                          
-                    },
-                    tooltip: {
-                        template: templates.toolTip
+            },
+
+            _createPlaylist: function (data) {
+                var playlistElement = wrapper.find(DOT + PLAYLIST);
+                if (playlistElement.length === 0) {
+                    this._playlistItemClickHandler = proxy(this._playlistItemClick, this);
+                    wrapper.append(templates.playlist(extend(this.options, { data: data }, rendering)));
+                    wrapper.find(DOT + PLAYLIST + "> ul > li").on("click" + ns, this._playlistItemClickHandler);
+                    if (this.options.playlist.hidden) {
+                        wrapper.find(DOT + PLAYLIST).hide();
                     }
-                });
-                slider = sliderElement.data("kendoSlider");
+                }
+            },
+
+            _resetTime: function(){
+                this._media.currentTime = 0;
+                this._mediaTimeUpdate();
+                $.grep(toolBar.options.items, function(e) { return !!e.template; }).template = templates.toolBarTime; 
+            },
+
+            _playlistItemClick: function (e) {
+                var item = $(e.target).parents("li");
+                var data = this.dataSource.data();
+                var currentItem = data[item.index()];
+                if (currentItem) {
+                    this._updateToolbarTitle(currentItem);
+                    this._resetTime();
+                    wrapper.find(DOT + MEDIA + " > source").remove();
+                    wrapper.find(DOT + MEDIA).attr("src", currentItem.url);
+                    this.play();
+                }
             },
 
             _createToolbar: function () {
-                var that = this;
                 var toolBarElement = wrapper.find(DOT + TOOLBAR);
-
                 if (toolBarElement.length === 0) { 
+                    this._toolbarClickHandler = proxy(this._toolbarClick, this);                    
                     wrapper.append(templates.toolBar);
                     toolBarElement = $(DOT + TOOLBAR);
                     toolBarElement.width($(DOT + MEDIA).width());
-                    toolBarElement.kendoToolBar({
+                    toolBar = new ui.ToolBar(toolBarElement, {
                         resizable: true, 
-                        click: proxy(that._toolbarClick, that),
+                        click: this._toolbarClickHandler,
                         items: [
                             { type: "button", id: "play", spriteCssClass: "k-icon k-font-icon k-i-play" },
                             { type: "separator" },
                             { 
-                                template: "<span id='curSpan'>00:00:00</span> / <span id='maxSpan'>00:00:00</span>"
+                                id: "timeTemplate",
+                                template: templates.toolBarTime
                             }
                         ]
                     });
                     toolBarElement.width("auto");
+                    currentTimeElement = toolBarElement.find("#currentTime");
+                    durationElement = toolBarElement.find("#duration");
                 } 
-                toolBar = toolBarElement.data("kendoToolbar");
             },
 
             _toolbarClick: function (e) {
-                var that = this;
                 var target = $(e.target).children().first();
-                var isPaused = that._media.paused;
+                var isPaused = this._media.paused;
 
                 if (e.id === "play") {
                     if (isPaused) {
-                        that.play();
+                        this.play();
                         target
                             .removeClass(STATE_PLAY)
                             .addClass(STATE_PAUSE);                        
                     } 
                     else {
-                        that.pause();
+                        this.pause();
                         target
                             .removeClass(STATE_PAUSE)
                             .addClass(STATE_PLAY);   
@@ -191,40 +243,57 @@
                 }
             },
 
+            _sliderDragging: function(e) {
+                this._isDragging = true;
+            },
+
+            _sliderDragChange: function(e) {
+                var tzOffset = timeZoneSec * 1000;
+                this._isDragging = false;
+                this._media.currentTime = this._timeToSec(e.value - tzOffset);
+            },
+
             _mediaTimeUpdate: function(e) {
-                var that = this;
-                var currentTime = that._msToTime(that._media.currentTime);
-                $("#curSpan").text(kendo.toString(currentTime, 'HH:mm:ss'));
-                slider.value(that._media.currentTime * 1000);
+                var currentTime = this._msToTime(this._media.currentTime);
+                currentTimeElement.text(kendo.toString(currentTime, 'HH:mm:ss'));
+                if (!this._isDragging) {
+                    slider.value((this._media.currentTime + timeZoneSec) * 1000);
+                }
             },
 
             _mediaDurationChange: function(e) {
-                var that = this;
-                var durationTime = that._msToTime(that._media.duration);
-                $("#maxSpan").text(kendo.toString(durationTime, 'HH:mm:ss'));
+                var durationTime = this._msToTime(this._media.duration);
+                durationElement.text(kendo.toString(durationTime, 'HH:mm:ss'));
                 slider.setOptions({
-                    min: new Date(0).getTime(),
+                    min: baseTime.getTime(),
                     max: durationTime.getTime()
                 });
                 slider._distance = Math.round(slider.options.max - slider.options.min);
                 slider._resize();
+
+                if (!this._isFirstRun) {
+                    this._resetTime();
+                    this._isFirstRun = true;
+                }
             },
 
-            _createHtmlPlayer: function (options) {
-                var that = this;
+             _createHtmlPlayer: function (options) {
+                this._mediaTimeUpdateHandler = proxy(this._mediaTimeUpdate, this);
+                this._mediaDurationChangeHandler = proxy(this._mediaDurationChange, this);
                 wrapper.append(templates.htmlPlayer);
-                that._media = wrapper.find(DOT + MEDIA)[0];
-                $(that._media).css({
-                    width: wrapper.width(),
-                    height: wrapper.height() 
-                });
-
+                this._media = wrapper.find(DOT + MEDIA)[0];
+                $(this._media)
+                    .css({
+                        width: wrapper.width(),
+                        height: wrapper.height() 
+                    });
+					
                 if (options.autoPlay) {
                     $(that._media).attr("autoplay", "");   
-                }
-
-                that._media.ontimeupdate = proxy(that._mediaTimeUpdate, that);
-                that._media.ondurationchange = proxy(that._mediaDurationChange, that);                
+                }		
+				
+                this._media.ontimeupdate = this._mediaTimeUpdateHandler;
+                this._media.ondurationchange = this._mediaDurationChangeHandler; 
             },
 
             setOptions: function (options) {
@@ -235,12 +304,25 @@
             },
 
             destroy: function () {
-                var that = this;
-                Widget.fn.destroy.call(that);
+                Widget.fn.destroy.call(this);
 
-                that._unbindDataSource();
+                this._unbindDataSource();
 
-                kendo.destroy(that.element);
+                this._playlistItemClickHandler = null;
+                this._playlistButtonClickHandler = null;
+
+                this._toolbarClickHandler = null;
+                this._sliderDragChangeHandler = null;
+                this._sliderDraggingHandler = null;
+
+                this.element.off(ns);
+                this.element.find(DOT + PLAYLIST + "> ul > li").off(ns);
+                this.element.find(DOT + PLAYLIST_OPEN).off(ns);
+
+                this._media.ontimeupdate = this._mediaTimeUpdateHandler = null;
+                this._media.ondurationchange = this._mediaDurationChangeHandler = null;
+
+                kendo.destroy(this.element);
             },
 
             play: function () {
@@ -258,34 +340,36 @@
             },
 
             _dataSource: function () {
-                var that = this;
-
-                if (that.dataSource && that._refreshHandler) {
-                    that._unbindDataSource();
+                if (this.dataSource && this._refreshHandler) {
+                    this._unbindDataSource();
                 } else {
-                    that._refreshHandler = proxy(that._refresh, that);
-                    that._progressHandler = proxy(that._progress, that);
-                    that._errorHandler = proxy(that._error, that);
+                    this._refreshHandler = proxy(this._refresh, this);
+                    this._progressHandler = proxy(this._progress, this);
+                    this._errorHandler = proxy(this._error, this);
                 }
 
-                that.dataSource = DataSource.create(that.options.dataSource)
-                                    .bind(CHANGE, that._refreshHandler)
-                                    .bind(PROGRESS, that._progressHandler)
-                                    .bind(ERROR, that._errorHandler);
+                this.dataSource = DataSource.create(this.options.dataSource)
+                                    .bind(CHANGE, this._refreshHandler)
+                                    .bind(PROGRESS, this._progressHandler)
+                                    .bind(ERROR, this._errorHandler);
             },
 
             _unbindDataSource: function () {
-                var that = this;
-
-                that.dataSource.unbind(CHANGE, that._refreshHandler)
-                                .unbind(PROGRESS, that._progressHandler)
-                                .unbind(ERROR, that._errorHandler);
+                this.dataSource.unbind(CHANGE, this._refreshHandler)
+                                .unbind(PROGRESS, this._progressHandler)
+                                .unbind(ERROR, this._errorHandler);
             },
 
             _refresh: function () {
-                var data = this.dataSource.data()
+                var data = this.dataSource.data();
                 if (data && data[0]) {
 
+                    if (this.options.playlist) {
+                        this._createPlaylist(data);
+                    }
+
+                    this._updateToolbarTitle(data[0]);
+                    
                     var sourceElement = document.createElement("source");
                     sourceElement.setAttribute("src", data[0].url);
                     wrapper.find(DOT + MEDIA).append(sourceElement);
