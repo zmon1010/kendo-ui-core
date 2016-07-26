4456d55582edf4b20feaaf2008474e0fe023e302
Bozhidar Ivanchev <Bozhidar.Ivanchev@telerik.com>
2016-07-26 14:50:59 +0300

    Update Spreadsheet services to use session as well as entity

--- wrappers/mvc/demos/Kendo.Mvc.Examples/Models/SpreadsheetProductService.cs
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Models/SpreadsheetProductService.cs
@@ -8,6 +8,7 @@ namespace Kendo.Mvc.Examples.Models
 {
     public class SpreadsheetProductService : IDisposable
     {
+        private static bool UpdateDatabase = false;
         private SampleEntities entities;
 
         public SpreadsheetProductService(SampleEntities entities)
@@ -15,71 +16,127 @@ namespace Kendo.Mvc.Examples.Models
             this.entities = entities;
         }
 
+        public virtual IList<SpreadsheetProductViewModel> GetAll()
+        {
+            var result = HttpContext.Current.Session["SpreadsheetProducts"] as IList<SpreadsheetProductViewModel>;
+
+            if (result == null || UpdateDatabase)
+            {
+                result = entities.Products.Select(product => new SpreadsheetProductViewModel
+                {
+                    ProductID = product.ProductID,
+                    ProductName = product.ProductName,
+                    UnitPrice = product.UnitPrice.HasValue ? product.UnitPrice.Value : default(decimal),
+                    UnitsInStock = product.UnitsInStock.HasValue ? product.UnitsInStock.Value : default(short),
+                    Discontinued = product.Discontinued
+                }).ToList();
+
+                HttpContext.Current.Session["SpreadsheetProducts"] = result;
+            }
+
+            return result;
+        }
+
         public IEnumerable<SpreadsheetProductViewModel> Read()
         {
-            return entities.Products.Select(product => new SpreadsheetProductViewModel
-            {
-                 ProductID = product.ProductID,
-                 ProductName = product.ProductName,
-                 UnitPrice = product.UnitPrice.HasValue ? product.UnitPrice.Value : default(decimal),
-                 UnitsInStock = product.UnitsInStock.HasValue ? product.UnitsInStock.Value : default(short),
-                 Discontinued = product.Discontinued
-            });
+            return GetAll();
         }
 
         public void Create(SpreadsheetProductViewModel product)
         {
-            var entity = new Product();
-
-            entity.ProductName = product.ProductName;
-            entity.UnitPrice = product.UnitPrice;
-            entity.UnitsInStock = (short)product.UnitsInStock;
-            entity.Discontinued = product.Discontinued;
-
-            if (entity.CategoryID == null)
+            if (!UpdateDatabase)
             {
-                entity.CategoryID = 1;
+                var first = GetAll().OrderByDescending(e => e.ProductID).FirstOrDefault();
+                var id = (first != null) ? first.ProductID : 0;
+
+                product.ProductID = id + 1;
+
+                GetAll().Insert(0, product);
             }
+            else
+            {
+                var entity = new Product();
 
-            entities.Products.Add(entity);
-            entities.SaveChanges();
+                entity.ProductName = product.ProductName;
+                entity.UnitPrice = product.UnitPrice;
+                entity.UnitsInStock = (short)product.UnitsInStock;
+                entity.Discontinued = product.Discontinued;
 
-            product.ProductID = entity.ProductID;
+                if (entity.CategoryID == null)
+                {
+                    entity.CategoryID = 1;
+                }
+
+                entities.Products.Add(entity);
+                entities.SaveChanges();
+
+                product.ProductID = entity.ProductID;
+            }
         }
 
         public void Update(SpreadsheetProductViewModel product)
         {
-            var entity = new Product();
+            if (!UpdateDatabase)
+            {
+                var target = One(e => e.ProductID == product.ProductID);
 
-            entity.ProductID = product.ProductID;
-            entity.ProductName = product.ProductName;
-            entity.UnitPrice = product.UnitPrice;
-            entity.UnitsInStock = (short)product.UnitsInStock;
-            entity.Discontinued = product.Discontinued;
+                if (target != null)
+                {
+                    target.ProductName = product.ProductName;
+                    target.UnitPrice = product.UnitPrice;
+                    target.UnitsInStock = product.UnitsInStock;
+                    target.Discontinued = product.Discontinued;
+                }
+            }
+            else
+            {
+                var entity = new Product();
 
-            entities.Products.Attach(entity);
-            entities.Entry(entity).State = EntityState.Modified;
-            entities.SaveChanges();
+                entity.ProductID = product.ProductID;
+                entity.ProductName = product.ProductName;
+                entity.UnitPrice = product.UnitPrice;
+                entity.UnitsInStock = (short)product.UnitsInStock;
+                entity.Discontinued = product.Discontinued;
+
+                entities.Products.Attach(entity);
+                entities.Entry(entity).State = EntityState.Modified;
+                entities.SaveChanges();
+            }
         }
 
         public void Destroy(SpreadsheetProductViewModel product)
         {
-            var entity = new Product();
-
-            entity.ProductID = product.ProductID;
-
-            entities.Products.Attach(entity);
-
-            entities.Products.Remove(entity);
-
-            var orderDetails = entities.Order_Details.Where(pd => pd.ProductID == entity.ProductID);
-
-            foreach (var orderDetail in orderDetails)
+            if (!UpdateDatabase)
             {
-                entities.Order_Details.Remove(orderDetail);
+                var target = GetAll().FirstOrDefault(p => p.ProductID == product.ProductID);
+                if (target != null)
+                {
+                    GetAll().Remove(target);
+                }
             }
+            else
+            {
+                var entity = new Product();
 
-            entities.SaveChanges();
+                entity.ProductID = product.ProductID;
+
+                entities.Products.Attach(entity);
+                entities.Products.Remove(entity);
+
+                var orderDetails = entities.Order_Details.Where(pd => pd.ProductID == entity.ProductID);
+
+                foreach (var orderDetail in orderDetails)
+                {
+                    entities.Order_Details.Remove(orderDetail);
+                }
+
+                entities.SaveChanges();
+            }
+        }
+
+        public SpreadsheetProductViewModel One(Func<SpreadsheetProductViewModel, bool> predicate)
+        {
+            return GetAll().FirstOrDefault(predicate);
         }
 
         public void Dispose()
