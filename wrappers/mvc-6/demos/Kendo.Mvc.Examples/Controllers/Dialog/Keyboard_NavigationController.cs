a364afa50a1ccde570705f9b3c209ab21cd2fb09
Ianko Djemerenov <iankodj@gmail.com>
2016-08-05 17:43:51 +0300

    Add all MVC6 demos
    Related to: #5894

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Dialog/Keyboard_NavigationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class DialogController : Controller
+    {
+        public ActionResult Keyboard_Navigation()
+        {
+            return View();
+        }
+    }
+}
