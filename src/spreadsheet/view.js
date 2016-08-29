a056c836195c84898e9c785c0f00c38e599aca69
Mihai Bazon <mihai.bazon@gmail.com>
2016-08-29 10:03:09 +0300

    [spreadsheet] Prevent JS error when container is hidden
    Ref telerik/kendo-ui-core#1990
    
    This doesn't completely fix the problem -- one has to call
    spreadsheet.refresh() when the container is displayed.  For a Window
    widget, that'd be the "activate" event, as can be seen in the attached
    playground.

--- src/spreadsheet/view.js
+++ src/spreadsheet/view.js
@@ -755,28 +755,32 @@
             } else {
                 pane = this.paneAt(x, y);
 
-                var row = pane._grid.rows.index(y, this.scroller.scrollTop);
-                var column = pane._grid.columns.index(x, this.scroller.scrollLeft);
+                if (!pane) {
+                    object = { type: "outside" };
+                } else {
+                    var row = pane._grid.rows.index(y, this.scroller.scrollTop);
+                    var column = pane._grid.columns.index(x, this.scroller.scrollLeft);
 
-                var type = "cell";
-                var ref = new CellRef(row, column);
-                var selecting = this._sheet.selectionInProgress();
+                    var type = "cell";
+                    var ref = new CellRef(row, column);
+                    var selecting = this._sheet.selectionInProgress();
 
-                if (this.isAutoFill(x, y, pane)) {
-                    type = "autofill";
-                } else if (this.isFilterIcon(x, y, pane, ref)) {
-                    type = "filtericon";
-                } else if (!selecting && x < grid._headerWidth) {
-                    ref = new CellRef(row, -Infinity);
-                    type = this.isRowResizer(y, pane, ref) ? "rowresizehandle" : "rowheader";
-                } else if (!selecting && y < grid._headerHeight) {
-                    ref = new CellRef(-Infinity, column);
-                    type = this.isColumnResizer(x, pane, ref) ? "columnresizehandle" : "columnheader";
-                } else if (this.isEditButton(x, y)) {
-                    type = "editor";
+                    if (this.isAutoFill(x, y, pane)) {
+                        type = "autofill";
+                    } else if (this.isFilterIcon(x, y, pane, ref)) {
+                        type = "filtericon";
+                    } else if (!selecting && x < grid._headerWidth) {
+                        ref = new CellRef(row, -Infinity);
+                        type = this.isRowResizer(y, pane, ref) ? "rowresizehandle" : "rowheader";
+                    } else if (!selecting && y < grid._headerHeight) {
+                        ref = new CellRef(-Infinity, column);
+                        type = this.isColumnResizer(x, pane, ref) ? "columnresizehandle" : "columnheader";
+                    } else if (this.isEditButton(x, y)) {
+                        type = "editor";
+                    }
+
+                    object = { type: type, ref: ref };
                 }
-
-                object = { type: type, ref: ref };
             }
 
             object.pane = pane;
