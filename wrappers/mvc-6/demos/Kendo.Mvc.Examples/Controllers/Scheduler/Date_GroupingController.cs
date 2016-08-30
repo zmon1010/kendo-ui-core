873f4391a78c2829d7908a5fbacc8a7675f0e37c
ivan.danchev <ifujin@gmail.com>
2016-08-31 01:14:34 +0300

    Add Scheduler Date Group demo for MVC5 and MVC6 #6023

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Scheduler/Date_GroupingController.cs
@@ -0,0 +1,51 @@
+﻿using Kendo.Mvc.Examples.Models.Scheduler;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class SchedulerController
+    {
+        [Demo]
+        public IActionResult Date_Grouping()
+        {
+            return View();
+        }
+
+        public virtual JsonResult Date_Grouping_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(meetingService.GetAll().ToDataSourceResult(request));
+        }
+
+        public virtual JsonResult Date_Grouping_Destroy([DataSourceRequest] DataSourceRequest request, MeetingViewModel meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                meetingService.Delete(meeting, ModelState);
+            }
+
+            return Json(new[] { meeting }.ToDataSourceResult(request, ModelState));
+        }
+
+        public virtual JsonResult Date_Grouping_Create([DataSourceRequest] DataSourceRequest request, MeetingViewModel meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                meetingService.Insert(meeting, ModelState);
+            }
+
+            return Json(new[] { meeting }.ToDataSourceResult(request, ModelState));
+        }
+
+        public virtual JsonResult Date_Grouping_Update([DataSourceRequest] DataSourceRequest request, MeetingViewModel meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                meetingService.Update(meeting, ModelState);
+            }
+
+            return Json(new[] { meeting }.ToDataSourceResult(request, ModelState));
+        }
+    }
+}
