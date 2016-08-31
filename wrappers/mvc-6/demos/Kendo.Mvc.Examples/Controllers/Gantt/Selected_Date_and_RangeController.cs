bdb3682eba5193095b7a5e2fc6fbeaaf0f584f4d
veselints <veselintsvetanov@gmail.com>
2016-08-31 16:59:45 +0300

    Gantt Selected date and range demo for MVC Core implemented

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/Gantt/Selected_Date_and_RangeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Kendo.Mvc.Examples.Controllers.Gantt
+{
+    public partial class GanttController : Controller
+    {
+        [Demo]
+        public ActionResult Selected_Date_and_Range()
+        {
+            return View();
+        }
+    }
+}
