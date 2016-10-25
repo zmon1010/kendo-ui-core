1075df4b5c4ca9e3005378c00c5c1390177d70d1
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2016-10-25 16:15:28 +0300

    chore: add generic site upload script

---
+++ build/upload-site-to-kendo-www.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SYNC='rsync -rvcz --delete --inplace'
+DEST='/usr/share/nginx/html/$2'
+USER='nginx'
+declare -a HOSTS=("ordkendowww01.telerik.local" "ordkendowww02.telerik.local")
+
+function log {
+    echo "[$(date +%T)] $1"
+}
+
+for host in "${HOSTS[@]}"
+do
+    log "Uploading site to $host"
+    $SYNC $1/_site/ $USER@$host:$DEST
+done
+
+log "Done"
+
