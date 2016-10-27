1ab567e547f7465b0708c8d225e43266c539b296
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2016-10-27 10:55:09 +0300

    chore: fix core bundle names to not include kendoui

--- build/version.rb
+++ build/version.rb
@@ -20,7 +20,7 @@ CURRENT_COMMIT = `git rev-parse HEAD`.strip
 BETA = !!ENV['BETA']
 
 def prefix_bundle_name(bundle)
-    prefix = (bundle =~ /mvc|php|jsp/) ? 'telerik.ui.for.' : 'telerik.kendoui.'
+    prefix = (bundle =~ /mvc|php|jsp|asp/) ? 'telerik.ui.for.' : 'telerik.kendoui.'
 
     prefix + bundle
 end
