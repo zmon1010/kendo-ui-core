fbb4e722c31aa2f616ac3f1cd75cd0d852eb6cc7
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2016-11-05 09:57:00 +0200

    chore: always execute unit tests

--- build/tests.rb
+++ build/tests.rb
@@ -46,15 +46,10 @@ namespace :tests do
     end
 
     %w[CI Production TZ].each do |env|
-        output = "#{env}-test-results.xml"
-
-        file output => DEPS do |t|
-            gulp_xvfb "ci", "--junit-results=#{output}", "--single-run=true", "--skip-cultures", "--skip-source-maps", scripts_arg, styles_arg
-        end
-
         desc "Run #{env} tests"
-        task env => [:jshint, output, :java] do
-            sh "touch #{output}"
+        task env => [:jshint, :java] do
+            output = "#{env}-test-results.xml"
+            gulp_xvfb "ci", "--junit-results=#{output}", "--single-run=true", "--skip-cultures", "--skip-source-maps", scripts_arg, styles_arg
         end
     end
 end
