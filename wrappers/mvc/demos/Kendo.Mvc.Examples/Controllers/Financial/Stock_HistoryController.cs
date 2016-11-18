9174718dc7034dd713bfa29e67789be031f03b85
Danail Vasilev <danail.vasilev@telerik.com>
2016-11-18 15:05:58 +0200

    feat: Added stock-history mvc demo #6332

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Financial/Stock_HistoryController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class FinancialController : Controller
+    {
+        [Demo]
+        public ActionResult Stock_History()
+        {
+            return View();
+        }
+    }
+}
