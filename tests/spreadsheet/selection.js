zmon1010/kendo-ui-core

commit 437fa1cd38ff995fa032153111b280706474cec8

fix(spreadsheet): regression in sheet.selectedHeaders() after 2c24f6df
The selection can no longer contain infinite ranges.  They'll be cropped
to maximum available area.  /cc @underlog

src/spreadsheet/sheet.js
@@ -861,6 +861,8 @@
             var cols = {};
             var allCols = false;
             var allRows = false;
+            var maxRow = this._grid.rowCount - 1;
+            var maxCol = this._grid.columnCount - 1;
 
             selection.forEach(function(ref) {
                 var i;
@@ -870,8 +872,8 @@
 
                 var bottomRight = ref.bottomRight;
 
-                var rowSelection = bottomRight.col === Infinity;
-                var colSelection = bottomRight.row === Infinity;
+                var rowSelection = bottomRight.col >= maxCol;
+                var colSelection = bottomRight.row >= maxRow;
 
                 if (colSelection) { //column selection
                     allRows = true;

commit c13b3f4287f21c7ba76dbd58acb5ebdad1ed8079

fix(spreadsheet): minor regression after 437fa1cd38
Selecting the last cell in a row triggered "allCols" in selectedHeaders,
making the whole columns header look selected; similar for last cell in
a column.

src/spreadsheet/sheet.js
@@ -871,9 +871,10 @@
                 ref = ref.toRangeRef();
 
                 var bottomRight = ref.bottomRight;
+                var topLeft = ref.topLeft;
 
-                var rowSelection = bottomRight.col >= maxCol;
-                var colSelection = bottomRight.row >= maxRow;
+                var rowSelection = topLeft.col <= 0 && bottomRight.col >= maxCol;
+                var colSelection = topLeft.row <= 0 && bottomRight.row >= maxRow;
 
                 if (colSelection) { //column selection
                     allRows = true;
@@ -886,7 +887,7 @@
                 }
 
                 if (!colSelection) { //column selection
-                    for (i = ref.topLeft.row; i <= bottomRight.row; i++) {
+                    for (i = topLeft.row; i <= bottomRight.row; i++) {
                         if (rows[i] !== "full") {
                             rows[i] = rowState;
                         }
@@ -894,7 +895,7 @@
                 }
 
                 if (!rowSelection) {
-                    for (i = ref.topLeft.col; i <= bottomRight.col; i++) {
+                    for (i = topLeft.col; i <= bottomRight.col; i++) {
                         if (cols[i] !== "full") {
                             cols[i] = colState;
                         }

tests/spreadsheet/selection.js
@@ -184,6 +184,13 @@
         equal(selectedHeaders.allCols, true);
     });
 
+    test("selecting bottom-right cell does not trigger allCols / allRows", function(){
+        sheet.range(999, 99).select();
+        var selectedHeaders = sheet.selectedHeaders();
+        equal(selectedHeaders.allCols, false);
+        equal(selectedHeaders.allRows, false);
+    });
+
     function find(classTestRe, children) {
         for (var i = 0; i < children.length; i++) {
             if (classTestRe.test(children[i].attr.className)) {