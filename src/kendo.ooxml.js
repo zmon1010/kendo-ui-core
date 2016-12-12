zmon1010/kendo-ui-core

commit d65a4f0fad5cb4a3662afac34b575d67eb30882c

fix(spreadsheet): several fixes for defaultCellStyle
- declare Arial 12 as default font, now that it's
  possible (telerik/kendo-ui-core#2505).

- output the default style in OOXML, in <col> styles

- do not consider cells that only have values in defaultCellStyle as
  being non-empty (otherwise we can't insert rows/cols) when we have a
  defaultCellStyle.

- do not output cells that only have values in defaultCellStyle in
  JSON serialization.

Fixes telerik/kendo-ui-core#2417
Fixes telerik/kendo-ui-core#2505

src/kendo.ooxml.js
@@ -141,15 +141,20 @@ var WORKSHEET = kendo.template(
    '<sheetFormatPr x14ac:dyDescent="0.25" defaultRowHeight="#= defaults.rowHeight ? defaults.rowHeight * 0.75 : 15 #" ' +
        '# if (defaults.columnWidth) { # defaultColWidth="#= kendo.ooxml.toWidth(defaults.columnWidth) #" # } #' +
    ' />' +
-   '# if (columns && columns.length > 0) { #' +
+   '# if (defaultCellStyleId != null || (columns && columns.length > 0)) { #' +
    '<cols>' +
+   '# if (!columns || !columns.length) { #' +
+       '<col min="1" max="16384" style="${defaultCellStyleId}" />' +
+   '# } #' +
    '# for (var ci = 0; ci < columns.length; ci++) { #' +
        '# var column = columns[ci]; #' +
        '# var columnIndex = typeof column.index === "number" ? column.index + 1 : (ci + 1); #' +
        '# if (column.width === 0) { #' +
-           '<col min="${columnIndex}" max="${columnIndex}" hidden="1" customWidth="1" />' +
+           '<col #if(defaultCellStyleId!=null){# style="${defaultCellStyleId}" #}#' +
+           'min="${columnIndex}" max="${columnIndex}" hidden="1" customWidth="1" />' +
        '# } else if (column.width) { #' +
-           '<col min="${columnIndex}" max="${columnIndex}" customWidth="1"' +
+           '<col #if(defaultCellStyleId!=null){# style="${defaultCellStyleId}" #}#' +
+           'min="${columnIndex}" max="${columnIndex}" customWidth="1"' +
            '# if (column.autoWidth) { #' +
                ' width="${((column.width*7+5)/7*256)/256}" bestFit="1"' +
            '# } else { #' +
@@ -445,6 +450,11 @@ var Worksheet = kendo.Class.extend({
             }
         }
 
+        var defaultCellStyleId = null;
+        if (this.options.defaultCellStyle) {
+            defaultCellStyleId = this._lookupStyle(this.options.defaultCellStyle);
+        }
+
         var freezePane = this.options.freezePane || {};
         return WORKSHEET({
             frozenColumns: this.options.frozenColumns || freezePane.colSplit,
@@ -457,7 +467,8 @@ var Worksheet = kendo.Class.extend({
             filter: filter,
             showGridLines: this.options.showGridLines,
             hyperlinks: this.options.hyperlinks || [],
-            validations: validations
+            validations: validations,
+            defaultCellStyleId: defaultCellStyleId
         });
     },
     _lookupString: function(value) {
@@ -547,20 +558,33 @@ var Worksheet = kendo.Class.extend({
 
         border = this._lookupBorder(border);
 
-        var style = {
-            bold: data.bold,
-            color: data.color,
-            background: data.background,
-            italic: data.italic,
-            underline: data.underline,
-            fontFamily: data.fontFamily || data.fontName,
-            fontSize: data.fontSize,
-            format: data.format,
-            textAlign: data.textAlign || data.hAlign,
-            verticalAlign: data.verticalAlign || data.vAlign,
-            wrap: data.wrap,
-            borderId: border
-        };
+        var defStyle = this.options.defaultCellStyle || {};
+        var style = { borderId: border };
+
+        (function(add){
+            add("color");
+            add("background");
+            add("bold");
+            add("italic");
+            add("underline");
+            if (!add("fontFamily")) { add("fontName", "fontFamily"); }
+            add("fontSize");
+            add("format");
+            if (!add("textAlign")) { add("hAlign", "textAlign"); }
+            if (!add("verticalAlign")) { add("vAlign", "verticalAlign"); }
+            add("wrap");
+        })(
+            function(prop, target){
+                var val = data[prop];
+                if (val === undefined) {
+                    val = defStyle[prop];
+                }
+                if (val !== undefined) {
+                    style[target || prop] = val;
+                    return true;
+                }
+            }
+        );
 
         var columns = this.options.columns || [];
 

src/kendo.spreadsheet.js
@@ -358,6 +358,10 @@
                     creator   : "Kendo UI PDF Generator v." + kendo.version,
                     // Creation Date; defaults to new Date()
                     date      : null
+                },
+                defaultCellStyle: {
+                    fontFamily: "Arial",
+                    fontSize: "12"
                 }
             },
 
@@ -393,7 +397,7 @@
         kendo.ui.plugin(Spreadsheet);
         $.extend(true, Spreadsheet, { classNames: classNames });
     })(window.kendo);
-    
+
     return window.kendo;
 
 }, typeof define == 'function' && define.amd ? define : function(a1, a2, a3){ (a3 || a2)(); });

src/spreadsheet/range.js
@@ -780,15 +780,28 @@
         },
 
         hasValue: function() {
-            var result = false;
-
-            this.forEachCell(function(row, col, cell) {
-                if (Object.keys(cell).length !== 0) {
-                    result = true;
+            var yesItHas = {};
+            var defStyle = this._sheet._defaultCellStyle;
+            try {
+                this.forEachCell(function(row, col, cell) {
+                    // we must not consider cells that only have same values
+                    // as defaultCellStyle, or otherwise we will forbid
+                    // inserting rows/cols in an empty sheet.
+                    for (var key in cell) {
+                        var val = cell[key];
+                        if (val !== undefined && val !== defStyle[key]) {
+                            throw yesItHas;
+                        }
+                    }
+                });
+            } catch(ex) {
+                if (ex === yesItHas) {
+                    return true;
+                } else {
+                    throw ex;
                 }
-            });
-
-            return result;
+            }
+            return false;
         },
 
         wrap: function(flag) {

src/spreadsheet/sheet.js
@@ -631,16 +631,15 @@
             var topLeft = this._grid.normalize(ref.topLeft);
             var bottomRight = this._grid.normalize(ref.bottomRight);
 
-            for (var ci = topLeft.col; ci <= bottomRight.col; ci ++) {
-                var ri = topLeft.row;
+            function doIt(value) {
+                callback(ri++, ci, value);
+            }
 
+            for (var ci = topLeft.col; ci <= bottomRight.col; ci++) {
+                var ri = topLeft.row;
                 var startCellIndex = this._grid.index(ri, ci);
                 var endCellIndex = this._grid.index(bottomRight.row, ci);
-
-                /* jshint loopfunc: true */
-                this._properties.forEach(startCellIndex, endCellIndex, function(value) {
-                    callback(ri++, ci, value);
-                });
+                this._properties.forEach(startCellIndex, endCellIndex, doIt);
             }
         },
 
@@ -956,8 +955,19 @@
             var columns = this._columns.toJSON("width", {});
             var viewSelection = this._viewSelection;
             var hyperlinks = [];
+            var defaultCellStyle = this._defaultCellStyle || {};
+
+            function clearDefaultStyle(cell) {
+                Object.keys(defaultCellStyle).forEach(function(key){
+                    if (cell[key] === defaultCellStyle[key]) {
+                        delete cell[key];
+                    }
+                });
+            }
 
             this.forEach(kendo.spreadsheet.SHEETREF, function(row, col, cell) {
+                clearDefaultStyle(cell);
+
                 if (Object.keys(cell).length === 0) {
                     return;
                 }
@@ -973,9 +983,7 @@
 
                 if (position === undefined) {
                     position = rows.length;
-
                     rows.push({ index: row });
-
                     positions[row] = position;
                 }
 
@@ -1036,7 +1044,8 @@
                 mergedCells: this._mergedCells.map(function(ref) {
                     return ref.toString();
                 }),
-                hyperlinks: hyperlinks
+                hyperlinks: hyperlinks,
+                defaultCellStyle: defaultCellStyle
             };
 
             if (this._sort) {

commit 4015bd9534e71cbe75ff038cedae896570e4c3c0

fix(spreadsheet): Add default width to <col> in XLSX
This fixes a regression after d65a4f0fad5cb4a3662afac34b575d67eb30882c -
Excel hides all columns unless we pass some width in the <col> tags.

src/kendo.ooxml.js
@@ -144,7 +144,9 @@ var WORKSHEET = kendo.template(
    '# if (defaultCellStyleId != null || (columns && columns.length > 0)) { #' +
    '<cols>' +
    '# if (!columns || !columns.length) { #' +
-       '<col min="1" max="16384" style="${defaultCellStyleId}" />' +
+       '<col min="1" max="16384" style="${defaultCellStyleId}" ' +
+       '# if (defaults.columnWidth) { # width="#= kendo.ooxml.toWidth(defaults.columnWidth) #" # } #' +
+       ' />' +
    '# } #' +
    '# for (var ci = 0; ci < columns.length; ci++) { #' +
        '# var column = columns[ci]; #' +

src/spreadsheet/workbook.js
@@ -402,7 +402,9 @@
                     sheet.revalidate(this._validationContext);
                     return sheet.toJSON();
                 }, this),
-                names: names
+                names: names,
+                columnWidth: this.options.columnWidth,
+                rowHeight: this.options.rowHeight
             };
         },
 