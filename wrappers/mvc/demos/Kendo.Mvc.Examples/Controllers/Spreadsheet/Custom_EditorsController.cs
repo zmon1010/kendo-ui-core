3fe0c3a1f82c341bde624f409a05f2fe50718f05
Peter Milchev <p_mil4ev@hotmail.com>
2017-01-03 18:05:37 +0200

    chore: Add Spreadsheet  Custom Cell Editors demos for ASP.NET MVC and ASP.NET Core

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/Spreadsheet/Custom_EditorsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers.Spreadsheet
+{
+    public partial class SpreadsheetController : Controller
+    {
+        [Demo]
+        public ActionResult Custom_Editors()
+        {
+            return View();
+        }
+    }
+}
