ca57bbd5314aeb7169da79731ad8a0dcc5655224
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2017-01-06 10:50:34 +0200

    fix: leave only legacy upload in Rake

--- build/npm.rb
+++ build/npm.rb
@@ -1,54 +1,50 @@
-task :npm_package do
-    gulp "npm-pro"
-end
+namespace :npm do
+    task :package do
+        gulp "npm-pro"
+    end
 
-task :npm_legacy => 'npm_package' do
-    repo_url = "git@github.com:telerik/npm-kendo-ui.git"
-    repo_path = "dist/npm-kendo-ui"
+    task :upload_legacy => :package do
+        repo_url = "git@github.com:telerik/npm-kendo-ui.git"
+        repo_path = "dist/npm-kendo-ui"
 
-    npmgit = "git --work-tree=#{repo_path} --git-dir=#{repo_path}/.git"
+        npmgit = "git --work-tree=#{repo_path} --git-dir=#{repo_path}/.git"
 
-    SYNC_NPM = <<-SH
-        cd /usr/share/nginx/html;
-        if [ ! -d npm-kendo-ui.git ]; then
-            git clone --bare git@github.com:telerik/npm-kendo-ui.git;
+        SYNC_NPM = <<-SH
+            cd /usr/share/nginx/html;
+            if [ ! -d npm-kendo-ui.git ]; then
+                git clone --bare git@github.com:telerik/npm-kendo-ui.git;
+                cd npm-kendo-ui.git;
+                git config http.receivepack false;
+                git config remote.origin.fetch 'refs/heads/*:refs/heads/*';
+            fi
+
             cd npm-kendo-ui.git;
-            git config http.receivepack false;
-            git config remote.origin.fetch 'refs/heads/*:refs/heads/*';
-        fi
+            git fetch;
+            git fetch --tags --prune origin;
+        SH
 
-        cd npm-kendo-ui.git;
-        git fetch;
-        git fetch --tags --prune origin;
-    SH
+        sh <<-SHELL
+        if [ ! -d #{repo_path} ]; then
+            git clone #{repo_url} #{repo_path};
+        else
+            #{npmgit} fetch;
+            #{npmgit} reset --hard origin/master;
+            #{npmgit} clean -dfx;
+        fi;
 
-    sh <<-SHELL
-    if [ ! -d #{repo_path} ]; then
-        git clone #{repo_url} #{repo_path};
-    else
-        #{npmgit} fetch;
-        #{npmgit} reset --hard origin/master;
-        #{npmgit} clean -dfx;
-    fi;
+        rm -rf #{repo_path}/*;
+        cp -r dist/npm-pro/* #{repo_path};
 
-    rm -rf #{repo_path}/*;
-    cp -r dist/npm/* #{repo_path};
+        #{npmgit} add --all .;
+        #{npmgit} commit --message "Update to #{VERSION}";
+        #{npmgit} tag --force --annotate "#{VERSION}" --message "#{VERSION}";
+        #{npmgit} push --force origin master:master;
+        #{npmgit} push --force origin "#{VERSION}";
 
-    #{npmgit} add --all .;
-    #{npmgit} commit --message "Update to #{VERSION}";
-    #{npmgit} tag --force --annotate "#{VERSION}" --message "#{VERSION}";
-    #{npmgit} push --force origin master:master;
-    #{npmgit} push --force origin "#{VERSION}";
+        ssh nginx@ordkendobower01.telerik.local '#{SYNC_NPM}';
 
-    ssh nginx@ordkendobower01.telerik.local '#{SYNC_NPM}';
+        SHELL
 
-    SHELL
-
+    end
 end
-
-task :npm => 'npm_package' do
-    `cd dist/npm && npm publish`
-end
-
-task 'release_builds:bundles:all' => ['npm', 'npm_legacy']
 
