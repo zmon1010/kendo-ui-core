1d7dd3663fd8bbce4d5f276fcee589545a91b3db
nenchef <nenchev@progress.com>
2017-01-09 16:29:46 +0200

    feat: add new PanelBar demos: binding to local, remote data and templates

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/PanelBar/TemplatesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Examples.Models;
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class PanelBarController : Controller
+    {
+        [Demo]
+        public ActionResult Templates()
+        {
+            return View();
+        }
+
+        public ActionResult Read_TemplateData(string id)
+        {
+            IEnumerable<PanelBarItemViewModel> result;
+            if (string.IsNullOrEmpty(id))
+            {
+                result = PanelBarRepository.GetProjectData().Select(o => o.Clone());
+            }
+            else
+            {
+                result = PanelBarRepository.GetChildren(id);
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
