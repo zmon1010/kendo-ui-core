6e066ea3208be0200e4972699263e13cf646e773
Bozhidar Ruzhenov <ruzhenov@telerik.com>
2017-01-10 14:03:10 +0200

    chore: Wrong descending sort icon in Pivot Grid - header items #6482

--- src/kendo.pivotgrid.js
+++ src/kendo.pivotgrid.js
@@ -3761,7 +3761,7 @@ var __meta__ = { // jshint ignore:line
 
             if (sortable) {
                 icons += '#if (data.sortIcon) {#';
-                icons += '<span class="k-icon ${data.sortIcon} k-i-sort-asc-sm"></span>';
+                icons += '<span class="k-icon ${data.sortIcon}-sm"></span>';
                 icons += '#}#';
             }
 
