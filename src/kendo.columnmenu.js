05a0c954c724a608d724fa4884c536dc0108479b
Alex Gyoshev <alex@gyoshev.net>
2017-01-13 16:33:49 +0200

    fix: move text to title

--- src/kendo.columnmenu.js
+++ src/kendo.columnmenu.js
@@ -76,7 +76,7 @@ var __meta__ = { // jshint ignore:line
             link = element.find(".k-header-column-menu");
 
             if (!link[0]) {
-                link = element.addClass("k-with-icon").prepend('<a class="k-header-column-menu" href="#"><span class="k-icon k-i-more-vertical">' + options.messages.settings + '</span></a>').find(".k-header-column-menu");
+                link = element.addClass("k-with-icon").prepend('<a class="k-header-column-menu" href="#" title="' + options.messages.settings + '"><span class="k-icon k-i-more-vertical"></span></a>').find(".k-header-column-menu");
             }
 
             that.link = link
