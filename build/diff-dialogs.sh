cea013849058ace7e9cb7d0d47393b3220f3cb53
ipeshev <ivan.peshev@progress.com>
2017-01-18 13:04:38 +0200

    fix: BSD does not support grep -P

--- build/diff-dialogs.sh
+++ build/diff-dialogs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-grep -rPo 'dialogs.register\("(.+)"' src/spreadsheet | cut -d'"' -f2 | sort > ./build/dialogs.current
+grep -rEo 'dialogs.register\("(.+)"' src/spreadsheet | cut -d'"' -f2 | sort > ./build/dialogs.current
 diff ./build/dialogs.list ./build/dialogs.current
