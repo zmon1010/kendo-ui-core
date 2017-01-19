4916e9ddf140c353fd203cd86ee94a33e765680f
Alex Gyoshev <alex@gyoshev.net>
2017-01-19 10:20:04 +0200

    fix(aeroviewr): use editor's choice photos, fix favicon

--- demos/mvc/content/spa/aeroviewr/js/aeroviewr.js
+++ demos/mvc/content/spa/aeroviewr/js/aeroviewr.js
@@ -31,7 +31,7 @@ var model,
         27: "Urban Exploration"
     },
     imagePlaceHolder = { user: { userpic_url: "" }, backgroundPhoto: "", image_url: "", calculatedWidth: "400px", noAvatar: true },
-    popularQuery = { url: "/photos", settings: { feature: "popular", exclude: "Nude", page: 1, image_size: 1, rpp: 50 } };
+    popularQuery = { url: "/photos", settings: { feature: "editors", exclude: "Nude", page: 1, image_size: 1, rpp: 50 } };
 
 var photos = new kendo.data.DataSource({
     transport: {
