96165078f57e33136d1784d9e189e20423d01036
Vasil Raev <Vasil.Raev@telerik.com>
2017-03-01 10:03:45 +0200

    docs(mvc-core-demos): correct default culture

--- wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/BaseController.cs
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/BaseController.cs
@@ -1,10 +1,17 @@
 ﻿using Kendo.Mvc.Examples.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 
 namespace Kendo.Mvc.Examples.Controllers
 {
     public abstract class BaseController : Controller
     {
+        public BaseController()
+        {
+            CultureInfo.DefaultThreadCurrentCulture =
+                CultureInfo.DefaultThreadCurrentUICulture =
+                new CultureInfo("en-US");
+        }
         public virtual SampleEntitiesDataContext GetContext()
         {
             return new SampleEntitiesDataContext();
