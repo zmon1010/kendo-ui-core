360b8d4afb9f7843ab0a2ded6426c43f2f3461f6
Angel Petrov <angel.petrov@telerik.com>
2017-03-15 15:30:53 +0200

    feat: add templates and datasource

--- src/kendo.listbox.js
+++ src/kendo.listbox.js
@@ -1,25 +1,37 @@
 /* jshint eqnull: true */
 (function(f, define){
-    define([ "./kendo.draganddrop" ], f);
+    define([ "./kendo.draganddrop", "./kendo.data" ], f);
 })(function(){
 
 var __meta__ = { // jshint ignore:line
     id: "listbox",
     name: "ListBox",
     category: "web",
-    depends: ["draganddrop"]
+    depends: ["draganddrop", "data"]
 };
 
 (function($, undefined) {
     var kendo = window.kendo,
+        proxy = $.proxy,
+        data = kendo.data,
         Widget = kendo.ui.Widget,
+        DataSource = data.DataSource,
         CHANGE = "change";
        
-    var ListBox = Widget.extend({
+    var ListBox = kendo.ui.DataBoundWidget.extend({
         init: function(element, options) {
             var that = this;
-
             Widget.fn.init.call(that, element, options);
+
+            that.wrapper = element = that.element;
+
+            that._dataSource();
+
+            that._templates();
+
+            if(that.options.autoBind) {
+                that.dataSource.fetch();
+            }
         },
 
         events: [
@@ -27,11 +39,69 @@ var __meta__ = { // jshint ignore:line
         ],
 
         options: {
-            name: "ListBox"
+            name: "ListBox",
+            autoBind: true,
+            template: "",
+            dataTextField: null
+        },
+
+        _dataSource: function() {
+            var that = this,
+                options = that.options,
+                dataSource = options.dataSource || {};
+
+            dataSource = $.isArray(dataSource) ? {data: dataSource} : dataSource;
+
+            if (options.dataTextField) {
+                dataSource.fields = [{ field: options.dataTextField }];
+            }
+
+            if (that.dataSource && that._refreshHandler) {
+                that.dataSource.unbind(CHANGE, that._refreshHandler);
+            } else {
+                that._refreshHandler = proxy(that._refresh, that);
+            }
+
+            that.dataSource = DataSource.create(that.options.dataSource)
+                                .bind(CHANGE, that._refreshHandler);
+        },
+
+        _templates: function() {
+            var options = this.options;
+            var template = options.template;
+
+            if (!template) {
+                template = kendo.template('<li class="k-item">${' + kendo.expr(options.dataTextField, "data") + "}</li>", { useWithBlock: false });
+            } else {
+                template = kendo.template(template);
+            }
+
+            this.options.template = template;
+        },
+
+        _refresh: function() {
+            var that = this,
+                view = that.dataSource.view(),
+                template = that.options.template,
+                html = "";
+            
+            html+= "<ul class='k-listBox'>";
+            for (var idx = 0; idx < view.length; idx++) {
+                html += template(view[idx]);
+            }
+            html+= "</ul>";
+            that.element.html(html);
         },
 
         destroy: function() {
-            Widget.fn.destroy.call(this);
+            var that = this;
+
+            DataBoundWidget.fn.destroy.call(that);
+
+            var dataSource = that.dataSource;
+            dataSource.unbind(CHANGE, that._refreshHandler);
+
+            kendo.destroy(that.element);
         }
     });
 
