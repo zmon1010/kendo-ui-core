8590c2d8c6ac961533c4523a2a3f3f5a71adc807
dbalinov <dragomir.balinov@progress.com>
2017-04-12 14:30:26 +0300

    docs(listBox): demo mvc template #6813

---
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/ListBox/TemplatesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class ListBoxController : Controller
+    {
+        [Demo]
+        public ActionResult Templates()
+        {
+            return View();
+        }
+    }
+}
