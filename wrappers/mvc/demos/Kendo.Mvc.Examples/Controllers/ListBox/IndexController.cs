91ac7120677cc7f62864948bd8b0f137dc276a02
dbalinov <dragomir.balinov@progress.com>
2017-04-18 15:39:41 +0300

    docs(listBox): demos mvc navigatable #6813

--- wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/ListBox/IndexController.cs
+++ wrappers/mvc/demos/Kendo.Mvc.Examples/Controllers/ListBox/IndexController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using Kendo.Mvc.Examples.Models;
@@ -43,6 +44,24 @@ namespace Kendo.Mvc.Examples.Controllers
             });
 
             return Json(customers, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetProducts()
+        {
+            var northwind = new SampleEntities();
+
+            var products = northwind.Products.Select(product => new ProductViewModel
+            {
+                ProductID = product.ProductID,
+                ProductName = product.ProductName,
+                UnitPrice = product.UnitPrice ?? 0,
+                UnitsInStock = product.UnitsInStock ?? 0,
+                UnitsOnOrder = product.UnitsOnOrder ?? 0,
+                Discontinued = product.Discontinued,
+                LastSupply = DateTime.Today
+            });
+
+            return Json(products, JsonRequestBehavior.AllowGet);
         }
     }
 }
