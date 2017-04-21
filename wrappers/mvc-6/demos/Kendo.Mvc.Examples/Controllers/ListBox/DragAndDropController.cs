30a7c9bc667e8396eebad47c2f53e1157a380f27
dbalinov <dragomir.balinov@progress.com>
2017-04-21 13:17:27 +0300

    docs(listBox): drag demo mvc6

---
+++ wrappers/mvc-6/demos/Kendo.Mvc.Examples/Controllers/ListBox/DragAndDropController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kendo.Mvc.Examples.Controllers
+{
+    public partial class ListBoxController : BaseController
+    {
+        [Demo]
+        public IActionResult Drag_And_Drop()
+        {
+            return View();
+        }
+    }
+}
