96620aaf2450d3ca94e3525924cbcd108769023a
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2017-05-26 15:00:52 +0300

    chore: add src modules sync script

---
+++ src-modules/README.md
@@ -0,0 +1,10 @@
+# Source Modules
+
+Tooling necessary to sync code that lives in external modules.
+
+## Syncing
+
+1. Run `npm install`
+1. Run `npm start`
+1. Review changes in the root "src" folder and run tests
+1. Commit and push changes
--- src-modules/gulpfile.js
+++ src-modules/gulpfile.js
@@ -15,7 +15,7 @@ const rollupDependencies = require('./transpilers/rollup-dependencies');
 const replaceExports = rollupDependencies.replaceExports;
 const replaceDependencies = rollupDependencies.replaceDependencies;
 
-const SRC_ROOT='../..';
+const SRC_ROOT='./';
 const DRAWING_SRC = path.join(SRC_ROOT, 'kendo-drawing', 'src');
 const CHARTS_SRC = path.join(SRC_ROOT, 'kendo-charts', 'src');
 
