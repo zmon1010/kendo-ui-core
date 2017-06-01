eb1e31445db8c80455462193730bdca23047bd56
Vasil Raev <Vasil.Raev@telerik.com>
2017-06-01 09:32:11 +0300

    feat: Kendo Scaffolder VS15 compatability

--- plugins/KendoScaffolder/KendoScaffolder/KendoScaffolder/UI/Models/ChartConfigurationViewModel.cs
+++ plugins/KendoScaffolder/KendoScaffolder/KendoScaffolder/UI/Models/ChartConfigurationViewModel.cs
@@ -214,7 +214,7 @@ namespace KendoScaffolder.UI.Models
                         SelectedModelFields = EfMetadata.Properties;
                     }
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
                     EfMetadata = null;
                     //throw new InvalidOperationException("Invalid model configuration", ex);
