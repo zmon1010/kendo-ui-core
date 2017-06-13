c0b6060c8cc877e0817ab7a152880dac50241eb2
Mihai Bazon <mihai.bazon@gmail.com>
2017-06-13 10:07:55 +0300

    chore(spreadsheet): add some tests for insert/delete + undo
    Part of telerik/kendo-ui-core#2816

--- tests/spreadsheet/undoredo.js
+++ tests/spreadsheet/undoredo.js
@@ -28,4 +28,101 @@
 
         ok(spreadsheet._workbook.undoRedoStack.canUndo());
     });
+
+    ///// formulas
+
+    test("undo after insert restores formulas", function(){
+        sheet.range("A1").formula("sum(B1:B3)");
+        sheet.range("2:2").select();
+        sheet._workbook.execute({ command: "AddRowCommand", options: { value: "above" } });
+        equal(sheet.range("A1").formula(), "sum(B1:B4)"); // it's updated
+        spreadsheet._workbook.undoRedoStack.undo();
+        equal(sheet.range("A1").formula(), "sum(B1:B3)"); // it's back
+    });
+
+    test("undo after delete restores null references in formulas", function(){
+        sheet.range("A1").formula("sum(B1:B3)");
+        sheet.range("B:B").select();
+        sheet._workbook.execute({ command: "DeleteColumnCommand" });
+        equal(sheet.range("A1").formula(), "sum(#NULL!)"); // becomes null
+        spreadsheet._workbook.undoRedoStack.undo();
+        equal(sheet.range("A1").formula(), "sum(B1:B3)"); // it's back
+    });
+
+    test("undo after insert restores formulas in another sheet", function(){
+        sheet.range("A1").formula("sum(Sheet2!B1:B3)");
+        var sheet2 = spreadsheet.insertSheet({ name: "Sheet2" });
+        spreadsheet.activeSheet(sheet2);
+        sheet2.range("2:2").select();
+        sheet2._workbook.execute({ command: "AddRowCommand", options: { value: "above" } });
+        equal(sheet.range("A1").formula(), "sum(Sheet2!B1:B4)"); // it's updated
+        spreadsheet._workbook.undoRedoStack.undo();
+        equal(sheet.range("A1").formula(), "sum(Sheet2!B1:B3)"); // it's back
+    });
+
+    test("undo after delete restores null references in formulas in another sheet", function(){
+        sheet.range("A1").formula("sum(Sheet2!B1:B3)");
+        var sheet2 = spreadsheet.insertSheet({ name: "Sheet2" });
+        spreadsheet.activeSheet(sheet2);
+        sheet2.range("B:B").select();
+        sheet2._workbook.execute({ command: "DeleteColumnCommand" });
+        equal(sheet.range("A1").formula(), "sum(#NULL!)"); // becomes null
+        spreadsheet._workbook.undoRedoStack.undo();
+        equal(sheet.range("A1").formula(), "sum(Sheet2!B1:B3)"); // it's back
+    });
+
+    ///// validations
+
+    test("undo after insert restores validations", function(){
+        sheet.range("A1").validation({
+            dataType: "list",
+            from: "B1:B3"
+        });
+        sheet.range("2:2").select();
+        sheet._workbook.execute({ command: "AddRowCommand", options: { value: "above" } });
+        equal(sheet.range("A1").validation().from, "B1:B4"); // it's updated
+        spreadsheet._workbook.undoRedoStack.undo();
+        equal(sheet.range("A1").validation().from, "B1:B3"); // it's back
+    });
+
+    test("undo after delete restores null references in validations", function(){
+        sheet.range("A1").validation({
+            dataType: "list",
+            from: "B1:B3"
+        });
+        sheet.range("B:B").select();
+        sheet._workbook.execute({ command: "DeleteColumnCommand" });
+        equal(sheet.range("A1").validation().from, "#NULL!"); // becomes null
+        spreadsheet._workbook.undoRedoStack.undo();
+        equal(sheet.range("A1").validation().from, "B1:B3"); // it's back
+    });
+
+    test("undo after insert restores validations in another sheet", function(){
+        sheet.range("A1").validation({
+            dataType: "list",
+            from: "Sheet2!B1:B3"
+        });
+        var sheet2 = spreadsheet.insertSheet({ name: "Sheet2" });
+        spreadsheet.activeSheet(sheet2);
+        sheet2.range("2:2").select();
+        sheet2._workbook.execute({ command: "AddRowCommand", options: { value: "above" } });
+        equal(sheet.range("A1").validation().from, "Sheet2!B1:B4"); // it's updated
+        spreadsheet._workbook.undoRedoStack.undo();
+        equal(sheet.range("A1").validation().from, "Sheet2!B1:B3"); // it's back
+    });
+
+    test("undo after delete restores null references in validations in another sheet", function(){
+        sheet.range("A1").validation({
+            dataType: "list",
+            from: "Sheet2!B1:B3"
+        });
+        var sheet2 = spreadsheet.insertSheet({ name: "Sheet2" });
+        spreadsheet.activeSheet(sheet2);
+        sheet2.range("B:B").select();
+        sheet2._workbook.execute({ command: "DeleteColumnCommand" });
+        equal(sheet.range("A1").validation().from, "#NULL!"); // becomes null
+        spreadsheet._workbook.undoRedoStack.undo();
+        equal(sheet.range("A1").validation().from, "Sheet2!B1:B3"); // it's back
+    });
+
 })();
