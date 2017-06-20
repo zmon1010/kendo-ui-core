zmon1010/kendo-ui-core

commit 9330ed7490b3b2f8959152d6492a52bff406e538

refactor(spreadsheet): Use only two RangeList-s for borders
Instead of having borderTop, borderRight, borderBottom and borderLeft for
each cell, this introduces only two sets -- hBorders (for top/bottom
borders) and vBorders (for left/right borders).  To accomodate for the last
row/column, hBorders allows columnCount more elements and similarly,
vBorders allows rowCount more elements.

Setting e.g. range("B2").borderLeft(...) now is equivalent to setting
range("A2").borderRight(...), so we don't need additional hacks to collapse
the borders in range.js.

Public API compatibility should be maintained, I'd expect, but the tests are
expected to fail at this point.

Fixes telerik/kendo-ui-core#2224

src/spreadsheet/propertybag.js
@@ -51,16 +51,15 @@
     });
 
     var ValueProperty = Property.extend({
-        init: function(values, formats, validations) {
+        init: function(values, formats) {
             Property.prototype.init.call(this, values);
-
-            this.validations = validations;
             this.formats = formats;
         },
 
         set: function(start, end, value) {
-            //TODO: RESET VALIDATION AS WELL?
             if (value instanceof Date) {
+                // XXX: should we check if existing format is not
+                // already date, in which case not reset it?
                 value = kendo.spreadsheet.dateToNumber(value);
                 this.formats.value(start, end, toExcelFormat(kendo.culture().calendar.patterns.d));
             }
@@ -75,14 +74,12 @@
 
     kendo.spreadsheet.PropertyBag = kendo.Class.extend({
         specs: [
-            { property: ValueProperty, name: "value", value: null, sortable: true, serializable: true, depends: "format" },
             { property: Property, name: "format", value: null, sortable: true, serializable: true },
+            { property: ValueProperty, name: "value", value: null, sortable: true, serializable: true, depends: "format" },
             { property: Property, name: "formula", value: null, sortable: true, serializable: true },
             { property: Property, name: "background", value: null, sortable: true, serializable: true },
-            { property: JsonProperty, name: "borderBottom", value: null, sortable: false, serializable: true },
-            { property: JsonProperty, name: "borderRight", value: null, sortable: false, serializable: true },
-            { property: JsonProperty, name: "borderLeft", value: null, sortable: false, serializable: true },
-            { property: JsonProperty, name: "borderTop", value: null, sortable: false, serializable: true },
+            { property: JsonProperty, name: "vBorders", value: null, sortable: false, serializable: true },
+            { property: JsonProperty, name: "hBorders", value: null, sortable: false, serializable: true },
             { property: Property, name: "color", value: null, sortable: true, serializable: true },
             { property: Property, name: "fontFamily", value: null, sortable: true, serializable: true },
             { property: Property, name: "underline", value: null, sortable: true, serializable: true },
@@ -98,21 +95,28 @@
             { property: Property, name: "editor", value: null, sortable: false, serializable: true }
         ],
 
-        init: function(cellCount, defaultValues) {
+        init: function(rowCount, columnCount, defaultValues) {
             defaultValues = defaultValues || {};
 
+            this.rowCount = rowCount;
+            this.columnCount = columnCount;
             this.properties = {};
-
             this.lists = {};
 
             this.specs.forEach(function(spec) {
-                var value = defaultValues[spec.name] !== undefined ? defaultValues[spec.name] : spec.value;
-
-               this.lists[spec.name] = new kendo.spreadsheet.SparseRangeList(0, cellCount, value);
-            }, this);
-
-            this.specs.forEach(function(spec) {
-                this.properties[spec.name] = new spec.property(this.lists[spec.name], this.lists[spec.depends]);
+                var cellCount = rowCount * columnCount - 1;
+                var name = spec.name;
+                var value = defaultValues[name];
+                if (value === undefined) {
+                    value = spec.value;
+                }
+                if (name == "hBorders") {
+                    cellCount += columnCount;
+                } else if (name == "vBorders") {
+                    cellCount += rowCount;
+                }
+                this.lists[name] = new kendo.spreadsheet.SparseRangeList(0, cellCount, value);
+                this.properties[name] = new spec.property(this.lists[name], this.lists[spec.depends]);
             }, this);
         },
 
@@ -137,10 +141,42 @@
                 return this.lists[name];
             }
 
+            switch (name) {
+              case "borderRight":
+                index += this.rowCount;
+                /* falls through */
+              case "borderLeft":
+                name = "vBorders";
+                break;
+
+              case "borderBottom":
+                index++;
+                /* falls through */
+              case "borderTop":
+                name = "hBorders";
+                break;
+            }
             return this.properties[name].get(index);
         },
 
         set: function(name, start, end, value) {
+            switch (name) {
+              case "borderRight":
+                start += this.rowCount;
+                end += this.rowCount;
+                /* falls through */
+              case "borderLeft":
+                name = "vBorders";
+                break;
+
+              case "borderBottom":
+                start++;
+                end++;
+                /* falls through */
+              case "borderTop":
+                name = "hBorders";
+                break;
+            }
             this.properties[name].set(start, end, value);
         },
 
@@ -175,7 +211,7 @@
 
         iterators: function(start, end) {
             var specs = this.specs.filter(function(spec) {
-                return spec.serializable;
+                return spec.serializable && !/Borders$/.test(spec.name);
             });
 
             return specs.map(function(spec) {
@@ -205,6 +241,12 @@
                         values[iterator.name] = value;
                     }
                 }
+
+                // XXX: this kinda sucks performance-wise, of course.
+                values.borderTop = this.get("borderTop", index);
+                values.borderRight = this.get("borderRight", index);
+                values.borderBottom = this.get("borderBottom", index);
+                values.borderLeft = this.get("borderLeft", index);
 
                 callback(values);
             }

src/spreadsheet/range.js
@@ -16,15 +16,9 @@
     var PROPERTIES = [
         "color", "fontFamily", "underline",
         "italic", "bold", "textAlign",
-        "verticalAlign", "background", "format", "link", "editor"
+        "verticalAlign", "background", "format", "link", "editor",
+        "borderTop", "borderRight", "borderBottom", "borderLeft"
     ];
-
-    var borders = {
-        borderTop: { complement: "borderBottom", direction: { top: -1, bottom: -1 } },
-        borderLeft: { complement: "borderRight", direction: { left: -1, right: -1 } },
-        borderRight: { complement: "borderLeft", direction: { left: 1, right: 1 }  },
-        borderBottom: { complement: "borderTop", direction: { top: 1, bottom: 1 }  }
-    };
 
     var Range = kendo.Class.extend({
         init: function(ref, sheet) {
@@ -114,68 +108,6 @@
             return this._ref.map(function(ref) {
                 return ref.toRangeRef().resize(direction);
             });
-        },
-
-        _border: function(property, value) {
-            var result;
-            var complement = borders[property].complement;
-            var direction = borders[property].direction;
-            var sheet = this._sheet;
-
-            sheet.batch(function() {
-                result = this._property(property, value);
-
-                if (value !== undefined) {
-                    this._resizedRef(direction).forEach(function(ref) {
-                        if (ref !== kendo.spreadsheet.NULLREF) {
-                            new Range(ref, sheet)._property(complement, null);
-                        }
-                    });
-                }
-            }.bind(this), {});
-
-            return result;
-        },
-
-        _collapsedBorder: function(property) {
-            var result = this._property(property);
-            var complement = borders[property].complement;
-            var direction = borders[property].direction;
-
-            this._resizedRef(direction).forEach(function(ref) {
-                if (!result && ref !== kendo.spreadsheet.NULLREF) {
-                    var range = new Range(ref, this._sheet);
-                    result = range._property(complement);
-                }
-            }.bind(this));
-
-            return result;
-        },
-
-        borderTop: function(value) {
-            return this._border("borderTop", value);
-        },
-        borderRight: function(value) {
-            return this._border("borderRight", value);
-        },
-        borderBottom: function(value) {
-            return this._border("borderBottom", value);
-        },
-        borderLeft: function(value) {
-            return this._border("borderLeft", value);
-        },
-
-        collapsedBorderTop: function() {
-            return this._collapsedBorder("borderTop");
-        },
-        collapsedBorderRight: function() {
-            return this._collapsedBorder("borderRight");
-        },
-        collapsedBorderBottom: function() {
-            return this._collapsedBorder("borderBottom");
-        },
-        collapsedBorderLeft: function() {
-            return this._collapsedBorder("borderLeft");
         },
 
         input: function(value) {

src/spreadsheet/sheet.js
@@ -120,8 +120,6 @@
         ],
 
         _reinit: function(rowCount, columnCount, rowHeight, columnWidth, headerHeight, headerWidth, defaultCellStyle) {
-            var cellCount = rowCount * columnCount - 1;
-
             defaultCellStyle = defaultCellStyle || {};
 
             this._defaultCellStyle = {
@@ -146,7 +144,7 @@
             this._gridLinesColor = null;
             this._grid = new kendo.spreadsheet.Grid(this._rows, this._columns, rowCount, columnCount, headerHeight, headerWidth);
             this._sheetRef = this._grid.normalize(kendo.spreadsheet.SHEETREF);
-            this._properties = new kendo.spreadsheet.PropertyBag(cellCount, this._defaultCellStyle);
+            this._properties = new kendo.spreadsheet.PropertyBag(rowCount, columnCount, this._defaultCellStyle);
             this._sorter = new kendo.spreadsheet.Sorter(this._grid, this._properties.sortable());
 
             this._viewSelection = new Selection(this);

commit 383eb8ea464ce13a3e6ddf02e24bf13f84a9b989

fix(spreadsheet): defaultCellStyle broken after 9330ed7490
Ref telerik/kendo-ui-core#3309

src/spreadsheet/propertybag.js
@@ -112,7 +112,8 @@
                     value = spec.value;
                 }
                 this.lists[name] = new kendo.spreadsheet.SparseRangeList(0, cellCount, value);
-                this.properties[name] = new spec.property(this.lists[name], this.lists[spec.depends]);
+                var prop = this.properties[name] = new spec.property(this.lists[name], this.lists[spec.depends]);
+                prop.spec = spec;
             }, this);
 
             // XXX: this is a hack but I have no better ideas at this
@@ -218,7 +219,7 @@
                 return index > cellCount ? null : prop.parse(at.call(iter, index));
             };
             iter.name = name;
-            iter.value = prop.list.range.value;
+            iter.value = prop.spec.value;
             return iter;
         },
 