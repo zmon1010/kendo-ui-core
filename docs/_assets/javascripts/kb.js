3036d0764a6805f7e9e33749cffa081c179c2009
PROGRESS\tmitev <todor.mitev@telerik.com>
2017-06-29 10:09:41 +0300

    ViewModel and initial filter

--- docs/_assets/javascripts/kb.js
+++ docs/_assets/javascripts/kb.js
@@ -1,72 +1,21 @@
-(function () {
-    var cx = '011354416605164361800:zp-phug0_0o';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-})();
+var KB_DEFAULT_QUERY = 'knowledge-base';
 
-window.__gcse = {
-    callback: gcseLatestUpdatedCallback
-};
+$(document).ready(function () {
+    var footer = $("#feedback-section").height() + $("footer").height() + 40
+    var windowHeight = $(window).height();
+    $("#page-article").css("min-height", windowHeight - 80 - footer)
+})
 
-function gcseLatestUpdatedCallback() {
-    if (document.readyState != 'complete') {
-        return google.setOnLoadCallback(gcseLatestUpdatedCallback, true);
-    } else {
-        attachToEvents();
+function searchInternal() {
+    viewModel.docs = false;
+    viewModel.kb = true;
+    viewModel.updateLabel();
 
-        google.search.cse.element.render({
-            gname: 'hidden-gsearch',
-            div: 'updated-articles-results',
-            tag: 'searchresults-only',
-            attributes: {
-                // webSearchQueryAddition: 'kb', // The kb meta tag filter will be here
-                // resultsUrl: 'http://docs.telerik.com/kendo-ui/knowledge-base/'
-                enableOrderBy: 'false', // This should be false in order the 'Sort by' options to be invisible!
-                sort_by: 'date',
-                sort: 'date',
-                webSearchExtendedRestricts: {
-                    sort_by: 'date',
-                    sort: 'date'
-                }
-            }
-        });
-        var element = google.search.cse.element.getElement('hidden-gsearch');
-        var orderByOptions = [{ key: 'date', label: 'Date' }];
-        element['uiOptions']['orderByOptions'] = orderByOptions;
-        element.execute('knowledge-base');
-    }
-};
+    var element = google.search.cse.element.getElement('google-search');
+    var orderByOptions = [{ key: 'date', label: 'Date' }];
+    element['uiOptions']['orderByOptions'] = orderByOptions;
 
-function hideLatestUpdatedResults() {
-    $('#last-updated-articles').hide();
-}
+    element.execute(KB_DEFAULT_QUERY);
 
-function filter(el) {
-    if (el) {
-        var filterPrefix = "more:pagemap:restype:";// "metatags-restype:";
-        var resourceType = $('#resource-type-options option:selected').val();
-        var filter = filterPrefix + resourceType;
-
-        // alert(filter);
-        el.webSearchQueryAddition = filter;
-    }
-}
-
-function search() {
-    hideLatestUpdatedResults();
-    filter(google.search.cse.element.getElement('google-search'));
-}
-
-function attachToEvents() {
-    $('.gsc-input').keydown(function (e) {
-        if (e.keyCode == 13) {
-            search();
-        }
-    })
-
-    $('.gsc-search-button').click(search);
+    $("#page-search .gsc-input-box .gsc-input").val("");
 }
